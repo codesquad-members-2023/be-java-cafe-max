cbd5a122b1e9f03e46f0208a30b43add4b989f2f
ALbertIM0427 <guang7226961@naver.com>
2023-03-27 19:30:49 +0900

    feat: PostForm 객체 추가

---
+++ src/main/java/kr/codesqaud/cafe/post/PostForm.java
@@ -0,0 +1,34 @@
+package kr.codesqaud.cafe.post;
+
+public class PostForm {
+
+	private String nickname;
+
+	private String title;
+
+	private String textContent;
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getTextContent() {
+		return textContent;
+	}
+
+	public void setTextContent(String textContent) {
+		this.textContent = textContent;
+	}
+}
