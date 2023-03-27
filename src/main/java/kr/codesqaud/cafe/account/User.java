1cd5abbe7b93bea6097da7d448955571232c7546
ALbertIM0427 <guang7226961@naver.com>
2023-03-27 11:29:36 +0900

    feat: 사용자 객체 추가

---
+++ src/main/java/kr/codesqaud/cafe/account/User.java
@@ -0,0 +1,37 @@
+package kr.codesqaud.cafe.account;
+
+public class User {
+	private String nickName;
+	private String email;
+	private String password;
+
+	public User(String nickName, String email, String password) {
+		this.nickName = nickName;
+		this.email = email;
+		this.password = password;
+	}
+
+	public String getNickName() {
+		return nickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
