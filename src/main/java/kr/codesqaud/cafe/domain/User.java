1198fbdc14eb3bcf2484a1b43b1f87c5370845d4
yeon <cerealnum@gmail.com>
2023-03-27 18:25:26 +0900

    User Domain 생성
    아이디, 비밀번호, 이름, 이메일을 갖는 User Domain을 생성합니다.
    ---
    getter와 setter를 추가합니다.

---
+++ src/main/java/kr/codesqaud/cafe/domain/User.java
@@ -0,0 +1,51 @@
+package kr.codesqaud.cafe.domain;
+
+public class User {
+    private String userId;
+    private String password;
+    private String name;
+    private String email;
+
+    public User() {
+
+    }
+
+    public User(String userId, String password, String name, String email) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getId() {
+        return userId;
+    }
+
+    public void setId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
