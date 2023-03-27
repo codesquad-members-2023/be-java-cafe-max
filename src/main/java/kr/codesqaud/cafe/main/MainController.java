b9386d9debf7f7fb7704ef039aafad6428ce7575
ALbertIM0427 <guang7226961@naver.com>
2023-03-27 20:33:13 +0900

    fix: Main 페이지에서 posts를 보여주는 기능을 구현

---
+++ src/main/java/kr/codesqaud/cafe/main/MainController.java
@@ -0,0 +1,28 @@
+package kr.codesqaud.cafe.main;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import kr.codesqaud.cafe.post.Post;
+import kr.codesqaud.cafe.post.PostsRepository;
+
+@Controller
+public class MainController {
+
+	private final PostsRepository postsRepository;
+
+	public MainController(PostsRepository postsRepository) {
+		this.postsRepository = postsRepository;
+	}
+
+	@GetMapping("/")
+	public String showMainPage(Model model) {
+		List<Post> posts = postsRepository.getAllPosts();
+		model.addAttribute("posts",posts);
+		return "index";
+	}
+
+}
