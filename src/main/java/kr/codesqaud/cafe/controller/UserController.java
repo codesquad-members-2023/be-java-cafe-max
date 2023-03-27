a28d9213a7504200c37868b227d64d09ccfd0eec
yeon <cerealnum@gmail.com>
2023-03-27 21:53:01 +0900

    User Controller에 회원 목록을 처리하는 Get Mapping 추가
    Model에 모든 회원 목록을 추가합니다.
    ---
    /users : 모든 회원 목록 조회 페이지입니다.

--- src/main/java/kr/codesqaud/cafe/controller/UserController.java
+++ src/main/java/kr/codesqaud/cafe/controller/UserController.java
@@ -4,10 +4,13 @@ import kr.codesqaud.cafe.domain.User;
 import kr.codesqaud.cafe.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/users")
@@ -21,7 +24,9 @@ public class UserController {
     }
 
     @GetMapping
-    public String getUsersList() {
+    public String getUsersList(Model model) {
+        List<User> users = userRepository.findAll();
+        model.addAttribute("users", users);
         return "user/list";
     }
 
