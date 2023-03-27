336ba321559745998dec0a753fad612fd76c1d61
ALbertIM0427 <guang7226961@naver.com>
2023-03-27 19:29:03 +0900

    feat: post 객체 추가

---
+++ src/main/java/kr/codesqaud/cafe/post/Post.java
@@ -0,0 +1,50 @@
+package kr.codesqaud.cafe.post;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class Post {
+	private static final AtomicLong postId = new AtomicLong();
+	private final Long id;
+
+	private String nickname;
+
+	private String title;
+
+	private String textContent;
+
+	public Post(Long id) {
+		this.id = id;
+	}
+
+	public static long createNewId() {
+		return postId.getAndIncrement();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getTextContent() {
+		return textContent;
+	}
+
+	public void setTextContent(String textContent) {
+		this.textContent = textContent;
+	}
+}
