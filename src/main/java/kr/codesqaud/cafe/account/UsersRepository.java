843a7034dbe6e2e7f72f049e0f765a7737ec4606
ALbertIM0427 <guang7226961@naver.com>
2023-03-27 11:36:50 +0900

    feat: 사용자 저장소 추가

---
+++ src/main/java/kr/codesqaud/cafe/account/UsersRepository.java
@@ -0,0 +1,21 @@
+package kr.codesqaud.cafe.account;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsersRepository {
+
+	private final List<User> usersRepository;
+
+	public UsersRepository() {
+		this.usersRepository = new ArrayList<>();
+	}
+
+	public boolean add(User user) {
+		return usersRepository.add(user);
+	}
+
+	public boolean remove(User user) {
+		return usersRepository.remove(user);
+	}
+}
