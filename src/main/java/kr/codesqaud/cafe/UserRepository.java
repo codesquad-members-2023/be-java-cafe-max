19909f2084df3fa1b4520594758e3cf00f767af3
n-aa-g <wlsdnjs0124@gmail.com>
2023-03-28 15:40:35 +0900

    Feat($UserRepository): UserRepository 클래스 생성
    유저를 관리하기 위한 저장소를 만들었습니다.

---
+++ src/main/java/kr/codesqaud/cafe/UserRepository.java
@@ -0,0 +1,16 @@
+package kr.codesqaud.cafe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserRepository {
+    private List<User> repository = new ArrayList<>();
+
+    public void save(User user) {
+        repository.add(user);
+    }
+
+    public List<User> getRepository() {
+        return repository;
+    }
+}
