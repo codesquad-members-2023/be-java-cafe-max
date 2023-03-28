562fd190ecd5a8e3b45c95cfb2c46e4505a389f0
yhpark95 <98851575+yhpark95@users.noreply.github.com>
2023-03-28 23:13:13 +0900

    refactor: User service 추가

--- src/main/java/kr/codesqaud/cafe/Controller/UserController.java
+++ src/main/java/kr/codesqaud/cafe/Controller/UserController.java
@@ -1,27 +1,32 @@
 package kr.codesqaud.cafe.Controller;
 
 import kr.codesqaud.cafe.domain.User;
+import kr.codesqaud.cafe.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
 public class UserController {
-    private List<User> users = new ArrayList<>();
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @PostMapping("/user/create")
     public String createUser(UserForm userForm){
-        users.add(new User(userForm.getUserId(), userForm.getPassword(), userForm.getName(), userForm.getEmail()));
+        userService.join(new User(userForm.getUserId(), userForm.getPassword(), userForm.getName(), userForm.getEmail()));
         return "redirect:/users";
     }
 
     @GetMapping("/users")
     public String listUsers(Model model) {
-        model.addAttribute("users", users);
+        model.addAttribute("users", userService.checkUsers());
         return "user/list";
     }
 
---
+++ src/main/java/kr/codesqaud/cafe/service/UserService.java
@@ -0,0 +1,40 @@
+package kr.codesqaud.cafe.service;
+
+import kr.codesqaud.cafe.repository.MemoryUserRepository;
+import kr.codesqaud.cafe.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class UserService {
+
+
+    private final MemoryUserRepository userRepository;
+
+    @Autowired
+    public UserService(MemoryUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    //회원가입
+    public String join(User user) {
+        validateDuplicateMember(user); //중복 회원 검증
+        userRepository.save(user);
+        return user.getUserId();
+    }
+    private void validateDuplicateMember(User user) {
+        userRepository.findByID(user.getUserId())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+
+    //회원목록 조회
+    public List<User> checkUsers() {
+        return userRepository.findAll();
+    }
+
+}
