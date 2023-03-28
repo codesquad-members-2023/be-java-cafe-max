ecba96670ed4894325d07cebde5b9a37c55e2789
crtEvent <crtevn@gmail.com>
2023-03-28 23:34:54 +0900

    [1단계-회원가입-04] 회원정보를 저장하기 위한 UserRepository.save() 추가

---
+++ src/main/java/kr/codesqaud/cafe/repository/UserRepository.java
@@ -0,0 +1,21 @@
+package kr.codesqaud.cafe.repository;
+
+import kr.codesqaud.cafe.domain.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UserRepository {
+    private List<User> users;
+
+    public UserRepository() {
+        users = new ArrayList<>();
+    }
+
+    public void save(User user) {
+        users.add(user);
+    }
+
+}
