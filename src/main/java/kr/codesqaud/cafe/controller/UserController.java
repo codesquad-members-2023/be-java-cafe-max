c7d4bcf61ec19046712f429e1ada64d8106ebab4
Hana <dokkisan.wang@gmail.com>
2023-03-28 14:41:25 +0900

    style: 코드 정렬

--- src/main/java/kr/codesqaud/cafe/controller/UserController.java
+++ src/main/java/kr/codesqaud/cafe/controller/UserController.java
@@ -1,10 +1,16 @@
 package kr.codesqaud.cafe.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+    @GetMapping(value = "/form")
+    public String form() {
+        return "user/form";
+    }
 }
