ae4e1b013a273f09c750c9f719d627014b25f2d9
crtEvent <crtevn@gmail.com>
2023-03-28 23:44:49 +0900

    [1단계-회원가입-04] UserController에서 회원가입 메서드 mapping

--- src/main/java/kr/codesqaud/cafe/controller/UserController.java
+++ src/main/java/kr/codesqaud/cafe/controller/UserController.java
@@ -1,16 +1,33 @@
 package kr.codesqaud.cafe.controller;
 
+import kr.codesqaud.cafe.domain.User;
+import kr.codesqaud.cafe.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
 
+    private final UserService service;
+
+    @Autowired
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
     @GetMapping("/signup")
     public String signup() {
         return "/user/form";
+    }
+
+    @PostMapping("")
+    public String userAdd(User user) {
+        service.addUser(user);
+        return "redirect:/users";
     }
 
 }
