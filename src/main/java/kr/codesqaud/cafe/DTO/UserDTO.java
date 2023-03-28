e57323a671e8be62429e41ee20f66565f40689b0
jinny-l <jinny.l.dev@gmail.com>
2023-03-28 16:09:20 +0900

    ✨feat: UserDTO 및 User 객체 구현
    * User.java : 회원을 의미하는 객체

---
+++ src/main/java/kr/codesqaud/cafe/DTO/UserDTO.java
@@ -0,0 +1,44 @@
+package kr.codesqaud.cafe.DTO;
+
+/**
+ * 회원 DTO 객체
+ */
+public class UserDTO {
+
+    private String userId;
+    private String password;
+    private String name;
+    private String email;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
