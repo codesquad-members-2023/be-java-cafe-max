ae11d7c9d758883c3c105de384f613420cd1bc7a
codemojae <116280283+codemojae@users.noreply.github.com>
2023-03-28 17:43:43 +0900

    페이지 접속시 기본화면을 index.html로 설정
    templates 폴더를 이용하기 위해 thymeleaf를 gradle에 추가했다.
    index.html 페이지에서 로고를 누르면 "/index.html"로 이동한다. 그래서 / , /index , /index.html의 경우로 다중매핑을 해서 index.html을 불러오게 했다.

---
+++ src/main/java/kr/codesqaud/cafe/controller/HomeController.java
@@ -0,0 +1,13 @@
+package kr.codesqaud.cafe.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping({"/", "/index", "/index.html"})
+    public String home() {
+        return "index";
+    }
+}
