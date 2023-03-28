1d022f8b7564bea0b712b40f95224134dc9be226
yhpark95 <98851575+yhpark95@users.noreply.github.com>
2023-03-28 14:57:06 +0900

    feat: UserController생성 -> 회원가입 요청시 유저 객체 생성

---
+++ src/main/java/kr/codesqaud/cafe/Controller/UserController.java
@@ -0,0 +1,23 @@
+package kr.codesqaud.cafe.Controller;
+
+import kr.codesqaud.cafe.domain.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class UserController {
+    private List<User> users = new ArrayList<>();
+
+    @PostMapping("/user/create")
+    public String createUser(UserForm userForm){
+        users.add(new User(userForm.getUserId(), userForm.getPassword(), userForm.getName(), userForm.getEmail()));
+        return "redirect:/";
+    }
+
+
+
+}
