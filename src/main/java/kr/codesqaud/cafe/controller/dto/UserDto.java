a6c039438e07715750e9cd067d8a37915a3d3671
Jeongyong Park <1239824@naver.com>
2023-03-28 11:39:22 +0900

    refactor : 유저의 프로필 정보를 조회할 때 UserListDto 클래스를 재활용할 수 있어 이름을 수정

--- src/main/java/kr/codesqaud/cafe/controller/dto/UserDto.java
+++ src/main/java/kr/codesqaud/cafe/controller/dto/UserDto.java
@@ -2,21 +2,21 @@ package kr.codesqaud.cafe.controller.dto;
 
 import kr.codesqaud.cafe.domain.user.User;
 
-public class UserListDto {
+public class UserDto {
 
 	private final Long rowIndex;
 	private final String userId;
 	private final String name;
 	private final String email;
 
-	private UserListDto(Long rowIndex, String userId, String name, String email) {
+	private UserDto(Long rowIndex, String userId, String name, String email) {
 		this.rowIndex = rowIndex;
 		this.userId = userId;
 		this.name = name;
 		this.email = email;
 	}
 
-	public static UserListDto from(User user, final long rowIndex) {
-		return new UserListDto(rowIndex, user.getUserId(), user.getName(), user.getEmail());
+	public static UserDto from(User user, final long rowIndex) {
+		return new UserDto(rowIndex, user.getUserId(), user.getName(), user.getEmail());
 	}
 }
