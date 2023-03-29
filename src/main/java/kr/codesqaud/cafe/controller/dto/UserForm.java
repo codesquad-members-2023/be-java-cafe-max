256a5e21b9c28c5c7e5af131aec7e4fd9861bc1c
Sully <2dcoder@naver.com>
2023-03-29 23:51:20 +0900

    fix: get 방식에서 post 방식으로 변경

---
+++ src/main/java/kr/codesqaud/cafe/controller/dto/UserForm.java
@@ -0,0 +1,40 @@
+package kr.codesqaud.cafe.controller.dto;
+
+public class UserForm {
+    private String userId;
+    private String name;
+    private String password;
+    private String email;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
