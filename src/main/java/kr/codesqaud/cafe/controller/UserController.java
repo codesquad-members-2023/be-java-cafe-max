62b79c264d2f4ee7c34a877dd30e44dcc14c9715
Sully <2dcoder@naver.com>
2023-03-29 23:58:34 +0900

    feat: UserService 객체를 관리하도록 생성자 추가

--- src/main/java/kr/codesqaud/cafe/controller/UserController.java
+++ src/main/java/kr/codesqaud/cafe/controller/UserController.java
@@ -1,12 +1,26 @@
 package kr.codesqaud.cafe.controller;
 
+import kr.codesqaud.cafe.controller.dto.UserForm;
+import kr.codesqaud.cafe.domain.User;
+import kr.codesqaud.cafe.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/user/join")
     public String joinForm() {
         return "user/form";
     }
+
 }
