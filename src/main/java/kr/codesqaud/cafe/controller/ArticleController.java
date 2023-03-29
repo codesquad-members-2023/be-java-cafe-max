99019982422ef7bae188995b5c50436a9d633d3e
DOEKYONG <apochmn@naver.com>
2023-03-30 01:52:14 +0900

    feat : 게시글 상세보기 컨트롤러 작성

--- src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
+++ src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
@@ -7,7 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class ArticleController {
@@ -24,10 +27,17 @@ public class ArticleController {
     }
     @PostMapping("/article/question")
     public String postQuestion(ArticleFormDto articleFormDto){
-
         articleService.questionWrite(articleFormDto);
         return "redirect:/";
     }
+
+    @GetMapping("/article/show/{index}")
+    public String getShow(@PathVariable("index")int index,Model model){
+        List<ArticleDto> list = articleService.getArticleList();
+        model.addAttribute("article",list.get(index));
+        return "/qna/show";
+    }
+
 
     @GetMapping("/")
     public String getIndex(Model model){
