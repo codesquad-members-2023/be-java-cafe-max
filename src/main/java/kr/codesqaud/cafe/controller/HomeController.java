50db39691fdcb66cfa9f08dff664d4a454ef4ba4
he2joo <hottestej6@gamil.com>
2023-03-29 16:34:33 +0900

    feat: HomeController 와 UserController 추가
    html파일에 따라 작동하는 메서드를 가진 클래스를 추가했습니다.

--- src/main/java/kr/codesqaud/cafe/controller/HomeController.java
+++ src/main/java/kr/codesqaud/cafe/controller/HomeController.java
@@ -1,4 +1,13 @@
 package kr.codesqaud.cafe.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
 }
--- src/main/java/kr/codesqaud/cafe/controller/UserController.java
+++ src/main/java/kr/codesqaud/cafe/controller/UserController.java
@@ -1,4 +1,44 @@
 package kr.codesqaud.cafe.controller;
 
+import kr.codesqaud.cafe.domain.User;
+import kr.codesqaud.cafe.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
 public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users/new")
+    public String createForm() {
+        return "users/createUserForm";
+    }
+
+    @PostMapping("/users/new")
+    public String create(UserForm form) {
+        User user = new User(form.getId(), form.getPassword(), form.getName(), form.getEmail());
+
+        userService.join(user);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/users")
+    public String list(Model model) {
+        List<User> users = userService.findMembers();
+        model.addAttribute("users", users);
+        return "users/userList";
+    }
+
 }
