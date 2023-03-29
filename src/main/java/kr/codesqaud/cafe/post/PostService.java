7a7d91ae8c804b2da10d4122c5d674c95c459180
ALbertIM0427 <guang7226961@naver.com>
2023-03-29 15:37:11 +0900

    feat: PostService 추가

---
+++ src/main/java/kr/codesqaud/cafe/post/PostService.java
@@ -0,0 +1,25 @@
+package kr.codesqaud.cafe.post;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+
+	private final PostsRepository postsRepository;
+
+	public PostService(PostsRepository postsRepository) {
+		this.postsRepository = postsRepository;
+	}
+
+	public void createNewPost(PostForm postForm) {
+		long newId = Post.createNewId();
+		Post post = new Post(newId);
+		post.setNickname(postForm.getNickname());
+		post.setTitle(postForm.getTitle());
+		post.setTextContent(postForm.getTextContent());
+		post.setCreatedDateTime(LocalDateTime.now());
+		postsRepository.add(post);
+	}
+}
