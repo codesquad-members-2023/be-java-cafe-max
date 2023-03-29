161df8dea4e1ddfc8742bb5df243ce040f90c223
O_jeegu <lumirite0@gmail.com>
2023-03-29 15:20:13 +0900

    feat : 사용자 정보를 저장하고 관리하는 User 객체 생성
    getter, setter 메서드 추가

---
+++ src/main/java/kr/codesqaud/cafe/domain/User.java
@@ -0,0 +1,49 @@
+package kr.codesqaud.cafe.domain;
+
+public class User {
+    private long id;
+    private String name;
+    private String userId;
+    private String password;
+    private String email;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
