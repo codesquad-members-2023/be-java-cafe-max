4f2eb1db858b92af4232dd98b1cbe28fd80e2acc
he2joo <hottestej6@gamil.com>
2023-03-29 16:28:15 +0900

    feat : UserRepository의 구현클래스 MemoryUserRepository 생성
    private변수로 User를 저장하는 ConcurrentHashMap을 가진 클래스로, UserRepository를 구현한 클래스를 만들었습니다.

--- src/main/java/kr/codesqaud/cafe/repository/MemoryUserRepository.java
+++ src/main/java/kr/codesqaud/cafe/repository/MemoryUserRepository.java
@@ -1,4 +1,44 @@
 package kr.codesqaud.cafe.repository;
 
-public class MemoryUserRepository {
+import kr.codesqaud.cafe.domain.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Repository
+public class MemoryUserRepository implements UserRepository {
+
+    private static Map<Long, User> store = new ConcurrentHashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public User save(User user) {
+        store.put(user.getId(), user);
+        return user;
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<User> findByName(String name) {
+        return store.values().stream()
+                .filter(user -> user.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<User> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
 }
