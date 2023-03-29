c9ab3c96f38ee7be671d1346edb255003870b2a2
ALbertIM0427 <guang7226961@naver.com>
2023-03-29 20:43:29 +0900

    refactor: PostController 에 @Valid 로직 추가

--- src/main/java/kr/codesqaud/cafe/post/PostController.java
+++ src/main/java/kr/codesqaud/cafe/post/PostController.java
@@ -2,11 +2,17 @@ package kr.codesqaud.cafe.post;
 
 import java.util.Optional;
 
+import javax.validation.Valid;
+
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import kr.codesqaud.cafe.post.form.PostForm;
 
@@ -22,21 +28,27 @@ public class PostController {
 	}
 
 	@GetMapping("/post")
-	public String showPostPage(Model model) {
+	public String showPostPage(Model model, @RequestParam @Nullable boolean errors) {
 		model.addAttribute(new PostForm());
+		model.addAttribute("errors", errors);
 		return "/post/form";
 	}
 
 	@PostMapping("/post")
-	public String addPost(PostForm postForm) {
-		postService.createNewPost(postForm);
-		return "redirect:/";
+	public String addPost(@Valid PostForm postForm, Errors errors, RedirectAttributes redirectAttributes, Model model) {
+		if (errors.hasErrors()) {
+			redirectAttributes.addAttribute("errors", true);
+			return "redirect:/post";
+		}
+		Post post = postService.createNewPost(postForm);
+		model.addAttribute(post);
+		return "/post/postDetail";
 	}
 
 	@GetMapping("/post/{postId}")
 	public String showPostPage(Model model, @PathVariable Long postId) {
 		Optional<Post> optionalPost = postsRepository.findById(postId);
-		if (optionalPost.isEmpty()){
+		if (optionalPost.isEmpty()) {
 			return "redirect:/";
 		}
 		Post post = optionalPost.get();
