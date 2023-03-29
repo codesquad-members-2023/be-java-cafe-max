51461fd1c7cb979c61dd9c2abb1f0eaa53be6e5f
he2joo <hottestej6@gamil.com>
2023-03-29 16:24:27 +0900

    feat: 사용자 정보 저장하는 User클래스 생성

--- src/main/java/kr/codesqaud/cafe/domain/User.java
+++ src/main/java/kr/codesqaud/cafe/domain/User.java
@@ -1,4 +1,33 @@
 package kr.codesqaud.cafe.domain;
 
 public class User {
+
+    private Long id;
+    private String password;
+    private String name;
+    private String email;
+
+    public User(Long id, String password, String name, String email) {
+        this.id = id;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
 }
