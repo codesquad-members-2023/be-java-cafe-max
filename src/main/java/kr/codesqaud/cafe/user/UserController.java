ae09ccc02b9b89eeefa44c24268aa12cc29ad3cd
n-aa-g <wlsdnjs0124@gmail.com>
2023-03-29 14:48:52 +0900

    Feat($User): UserController 클래스 생성
    유저 서비스의 컨트롤러인 UserController 클래스를 생성했습니다.

---
+++ src/main/java/kr/codesqaud/cafe/user/UserController.java
@@ -0,0 +1,25 @@
+package kr.codesqaud.cafe.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    private final UserService userService = new UserService();
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "user/form";
+    }
+
+    @PostMapping("/signup")
+    public String signup(UserForm userForm) {
+        userService.saveUser(userForm);
+
+        return "redirect:/";
+    }
+
+}
