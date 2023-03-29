b37d62e4ef43ad6a832d2ec5dad8b021aa7fea64
othertkfka <57559288+othertkfka@users.noreply.github.com>
2023-03-29 09:27:34 +0900

    feat: 회원가입할 수 있는 addUser 구현
    User - userId,password,userName,email을 저장할 수 있는 DTO를 구현했습니다.
    UserController - 회원 가입 기능에 대한 요청을 구현했습니다.
    get으로 요청이 오면 form.mustache를 보여줍니다.
    post로 요청이 오면 User 객체로 데이터를 담아서 List에 추가하도록 구현했습니다.

---
+++ src/main/java/kr/codesqaud/cafe/user/controller/UserController.java
@@ -0,0 +1,31 @@
+package kr.codesqaud.cafe.user.controller;
+
+import kr.codesqaud.cafe.user.controller.domain.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    List<User> users = new ArrayList<>();
+
+    @GetMapping("/add")
+    public String addUser() {
+        return "user/form";
+    }
+
+    @PostMapping("/add")
+    public String addUser(@ModelAttribute User user) {
+        users.add(user);
+
+        return "user/list";
+    }
+}
