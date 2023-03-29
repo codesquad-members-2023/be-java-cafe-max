425e04561f204bb335436add4348841ccbe125af
ALbertIM0427 <guang7226961@naver.com>
2023-03-29 14:59:32 +0900

    feat: 맴버리스트 출력용 UsersForm 추가

---
+++ src/main/java/kr/codesqaud/cafe/account/form/UsersForm.java
@@ -0,0 +1,31 @@
+package kr.codesqaud.cafe.account.form;
+
+public class UsersForm {
+	private Long id;
+	private String nickname;
+	private String email;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
