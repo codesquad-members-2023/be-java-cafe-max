1c1547c0ee2db0729e788a4b6bf3c0c33aacfaa5
Sully <2dcoder@naver.com>
2023-03-29 17:58:34 +0900

    feat: save 메서드의 반환값을 Optional<>로 변경
    값이 안 들어올 수 있음

--- src/main/java/kr/codesqaud/cafe/repository/UserRepository.java
+++ src/main/java/kr/codesqaud/cafe/repository/UserRepository.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository {
-    User save(User user);
+    Optional<User> save(User user);
 
     Optional<User> findByUserId(String userId);
 
