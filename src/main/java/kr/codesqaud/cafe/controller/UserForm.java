6ab287cd96cf7f563e730965e5c752f39b57825d
he2joo <hottestej6@gamil.com>
2023-03-29 16:25:04 +0900

    feat : 사용자 입력 저장하는 UserForm클래스 생성

--- src/main/java/kr/codesqaud/cafe/controller/UserForm.java
+++ src/main/java/kr/codesqaud/cafe/controller/UserForm.java
@@ -1,2 +1,41 @@
-package kr.codesqaud.cafe.controller;public class UserForm {
+package kr.codesqaud.cafe.controller;
+
+public class UserForm {
+
+    private Long id;
+    private String password;
+    private String name;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
