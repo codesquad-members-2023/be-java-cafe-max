7a641e15719eed95dd938f4e537e38a88db41d00
he2joo <hottestej6@gamil.com>
2023-03-29 16:26:25 +0900

    feat : UserRepository 인터페이스 생성
    user정보를 저장하고, 검색하는 기능을 가진 인터페이스를 만들었습니다.

--- src/main/java/kr/codesqaud/cafe/repository/UserRepository.java
+++ src/main/java/kr/codesqaud/cafe/repository/UserRepository.java
@@ -1,2 +1,14 @@
-package kr.codesqaud.cafe.repository;public interface UserRepository {
+package kr.codesqaud.cafe.repository;
+
+import kr.codesqaud.cafe.domain.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository {
+
+    User save(User user);
+    Optional<User> findById(Long id);
+    Optional<User> findByName(String name);
+    List<User> findAll();
 }
