9cd46b530e7cfdd18eb571a2c8e67140496659c1
jinny-l <jinny.l.dev@gmail.com>
2023-03-29 11:15:47 +0900

    ✨feat: 회원 가입 기능 구현
    * 구현 기능:
      - 가입하기 페이지에서 회원 가입 폼을 표시하는 기능
      - 회원 가입 완료 후 확인 버튼을 누르면 회원 목록 조회 페이지로 이동하는 기능

--- src/main/java/kr/codesqaud/cafe/controller/UserController.java
+++ src/main/java/kr/codesqaud/cafe/controller/UserController.java
@@ -1,11 +1,12 @@
 package kr.codesqaud.cafe.controller;
 
+import kr.codesqaud.cafe.DTO.UserDTO;
 import kr.codesqaud.cafe.domain.User;
 import kr.codesqaud.cafe.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
@@ -17,31 +18,20 @@ public class UserController {
         this.userService = userService;
     }
 
-//    @GetMapping("/user/create")
-//    public String signUpPage() {
-//        return "/user/form";
-//    }
-
-    /* 문제:
-     * 1. 이 코드 그대로 실행하면 매핑 에러 발생 - 위에 /user/create로 매핑해줬던 템플릿이 없어서인 것으로 추측
-     * 2. 임시 방편으로 다음 과정을 거쳤더니 되긴 함, 근데 실제로 사용되지 않는 Location을 매핑하는 것이 맞는지 모르겠음.
-     *    - 위에 메서드 활성화
-     *    - create 메서드 매핑 주소를 안쓰는 location으로 생성해서 html과 연결
-     * 3. 위에 메서드를 활성화 하고 create를 PathVariable 받는 형식으로 하면 URI에 파라미터가 생성되지만, 리다이렉팅이 안됨
-     */
     @GetMapping("/user/create")
-    public String create(
-            @RequestParam String userId,
-            @RequestParam String password,
-            @RequestParam String name,
-            @RequestParam String email) {
-        User user = new User();
-        user.setUserId(userId);
-        user.setPassword(password);
-        user.setName(name);
-        user.setEmail(email);
+    public String signUpPage() {
+        return "/user/form";
+    }
+
+    @PostMapping("/user/create")
+    public String create(UserDTO userDTO) {
+        User user = new User(); // TODO: 생성자 생성할 때 파람 안받고 setter로 해주는 이유를 모르겠다.
+        user.setUserId(userDTO.getUserId()); // TODO: 파람이 많을 때 가독성 좋게 넣어줄 수 있는 방법 찾아서 리팩터링 필요
+        user.setPassword(userDTO.getPassword());
+        user.setName(userDTO.getName());
+        user.setEmail(userDTO.getEmail());
         userService.join(user);
-        return "redirect:/user/list";
+        return "redirect:/user/list"; // TODO: 어떤 상황에 템플릿 or 리다이렉팅 해주는지 이해 못했다.
     }
 
     @GetMapping("/user/list")
