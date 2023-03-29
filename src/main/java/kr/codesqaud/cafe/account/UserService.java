48fb7bdf15202c58ee616f9a5d2880de25801e51
ALbertIM0427 <guang7226961@naver.com>
2023-03-29 13:02:52 +0900

    feat: UserService 객체 추가
    UserController에서 로직을 분리하는 목적으로 추가 하였습니다.

---
+++ src/main/java/kr/codesqaud/cafe/account/UserService.java
@@ -0,0 +1,55 @@
+package kr.codesqaud.cafe.account;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+	private final UsersRepository usersRepository;
+
+	public UserService(UsersRepository usersRepository) {
+		this.usersRepository = usersRepository;
+	}
+
+	public User createNewUser(UserForm userForm) {
+		User user = new User(User.createNewId());
+		user.setNickname(userForm.getNickname());
+		user.setEmail(userForm.getEmail());
+		user.setPassword(userForm.getPassword());
+		usersRepository.add(user);
+		return user;
+	}
+
+	public Optional<User> findByEmail(String email) {
+		return usersRepository.findByEmail(email);
+	}
+
+	public List<UserForm> getAllMembers() {
+		List<User> allMembers = usersRepository.getAllMembers();
+		List<UserForm> userForms = allMembers.stream()
+			.map(user -> user.mappingUserForm())
+			.collect(Collectors.toList());
+		return userForms;
+	}
+
+	public Optional<UserForm> findById(Long userId) {
+		Optional<User> userOptional = usersRepository.findById(userId);
+		return Optional.of(userOptional.get().mappingUserForm());
+	}
+
+	public void update(UserForm userForm, Long userId) {
+		User user = usersRepository.findById(userId).get();
+		user.setEmail(userForm.getEmail());
+		user.setNickname(userForm.getNickname());
+	}
+
+	public boolean checkPassword(UserForm userForm, Long userId) {
+		Optional<User> optionalUser = usersRepository.findById(userId);
+		User user = optionalUser.get();
+		return user.getPassword().equals(userForm.getPassword());
+	}
+}
