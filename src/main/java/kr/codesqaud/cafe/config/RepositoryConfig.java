1e95b53fcb9975d64426c10d4f656fdcbbd4d074
김수민(Sumin-Kim) <sumin.kim.develop@gmail.com>
2023-03-30 01:35:54 +0900

    서식 통일
    - 클래스 body 첫 줄을 공백으로 통일

--- src/main/java/kr/codesqaud/cafe/config/RepositoryConfig.java
+++ src/main/java/kr/codesqaud/cafe/config/RepositoryConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RepositoryConfig {
+
     @Bean
     public UserRepository userRepository() {
         return new MemoryUserRepository();
--- src/main/java/kr/codesqaud/cafe/controller/IndexController.java
+++ src/main/java/kr/codesqaud/cafe/controller/IndexController.java
@@ -5,7 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-    @GetMapping("")
+
+    @GetMapping("/")
     public String main() {
         return "index";
     }
