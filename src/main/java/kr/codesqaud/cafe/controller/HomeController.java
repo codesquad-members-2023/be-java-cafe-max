631971fcd98b7ed1733529f41e8065529edaf046
he2joo <hottestej6@gamil.com>
2023-03-29 21:08:00 +0900

    feat : HomeController에 GetMapping메서드 추가

--- src/main/java/kr/codesqaud/cafe/controller/HomeController.java
+++ src/main/java/kr/codesqaud/cafe/controller/HomeController.java
@@ -6,8 +6,38 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping(value = {"/", "/index","/index.html"})
     public String home() {
-        return "/index";
+        return "index";
+    }
+
+    @GetMapping("user/login.html")
+    public String loginForm() {
+        return "user/login";
+    }
+
+    @GetMapping("user/form.html")
+    public String joinForm() {
+        return "user/form";
+    }
+
+    @GetMapping("user/list.html")
+    public String listUser() {
+        return "user/list";
+    }
+
+    @GetMapping("user/profile.html")
+    public String profile() {
+        return "user/profile";
+    }
+
+    @GetMapping("qna/form.html")
+    public String makeQna() {
+        return "qna/form";
+    }
+
+    @GetMapping("qna/show.html")
+    public String showQna() {
+        return "qna/show";
     }
 }
