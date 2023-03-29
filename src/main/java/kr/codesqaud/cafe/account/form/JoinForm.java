2fa7397236d44ba1b96366d256018383c95eb4f7
ALbertIM0427 <guang7226961@naver.com>
2023-03-29 14:50:03 +0900

    feat: 가입 폼 추가

---
+++ src/main/java/kr/codesqaud/cafe/account/form/JoinForm.java
@@ -0,0 +1,31 @@
+package kr.codesqaud.cafe.account.form;
+
+public class JoinForm {
+	private String nickname;
+	private String email;
+	private String password;
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
