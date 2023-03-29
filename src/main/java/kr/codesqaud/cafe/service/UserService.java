9d999f2150df7e4c2d1a568a445ce817d539fc5b
Sully <2dcoder@naver.com>
2023-03-29 23:55:23 +0900

    feat: UserRepository 객체를 관리하는 Service 생성
    UserRepository 객체를 생성자로 받아와서 관리한다.
    userId가 중복되는지 판별하고, 저장해주는 회원가입 기능을 갖고 있다.
    user 전체를 찾는 기능도 갖고 있다.

---
+++ src/main/java/kr/codesqaud/cafe/service/UserService.java
@@ -0,0 +1,32 @@
+package kr.codesqaud.cafe.service;
+
+import kr.codesqaud.cafe.domain.User;
+import kr.codesqaud.cafe.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void join(User user) {
+        validateDuplicateUser(user);
+        userRepository.save(user);
+    }
+
+    private void validateDuplicateUser(User user) {
+        userRepository.findByUserId(user.getUserId())
+                .ifPresent(u -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    public List<User> findUsers() {
+        return userRepository.findAll();
+    }
+}
