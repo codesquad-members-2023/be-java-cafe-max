8258176417c99b8b1b502ec27cd7c97e5da3c2ce
othertkfka <57559288+othertkfka@users.noreply.github.com>
2023-03-29 16:34:50 +0900

    feat: UserRepository를 사용하여 User 데이터를 관리하도록 변경
    UserRepository - (id,User) 구조의  Map으로 저장하는 repository를 구현했습니다.
    add(User user) - Map에 User를 넣어줍니다.
    getUser(String userId) - userId로 User를 리턴해줍니다.
    getUserList() - values를 List로 만들어서 리턴합니다.
    
    UserController도 userRepository를 이용해서 데이터를 저장하고 가져오도록 수정했습니다.

--- src/main/java/kr/codesqaud/cafe/user/controller/UserController.java
+++ src/main/java/kr/codesqaud/cafe/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package kr.codesqaud.cafe.user.controller;
 
 import kr.codesqaud.cafe.user.controller.domain.User;
+import kr.codesqaud.cafe.user.controller.repository.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,15 +9,16 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
-import java.util.List;
-
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
-    List<User> users = new ArrayList<>();
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @GetMapping("/add")
     public String addUser() {
@@ -25,14 +27,14 @@ public class UserController {
 
     @PostMapping("/add")
     public String addUser(@ModelAttribute User user) {
-        users.add(user);
+        userRepository.add(user);
         return "redirect:/user/list";
     }
 
     @GetMapping("/list")
     public String list(Model model) {
-        model.addAttribute("users", users);
-
+        model.addAttribute("users", userRepository.getUserList());
         return "user/list";
     }
+
 }
---
+++ src/main/java/kr/codesqaud/cafe/user/controller/repository/UserRepository.java
@@ -0,0 +1,28 @@
+package kr.codesqaud.cafe.user.controller.repository;
+
+import kr.codesqaud.cafe.user.controller.domain.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Repository
+public class UserRepository {
+
+    private Map<String, User> users = new HashMap<>();
+
+    public void add(User user) {
+        users.put(user.getUserId(), user);
+    }
+
+    public User getUser(String userId) {
+        return users.get(userId);
+    }
+
+    public List<User> getUserList() {
+        return users.values().stream().collect(Collectors.toList());
+    }
+
+}
