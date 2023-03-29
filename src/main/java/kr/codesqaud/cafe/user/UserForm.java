18cc3483efe772f4cf54d829035ca6f96fa7bafd
n-aa-g <wlsdnjs0124@gmail.com>
2023-03-29 14:50:06 +0900

    Feat($user): UserForm 클래스 생성
    html에서 받는 값을 전달하기 위한 객체인 userForm을 생성하는 클래스를 만들었습니다.

---
+++ src/main/java/kr/codesqaud/cafe/user/UserForm.java
@@ -0,0 +1,40 @@
+package kr.codesqaud.cafe.user;
+
+public class UserForm {
+    private String userId;
+    private String password;
+    private String userName;
+    private String email;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
