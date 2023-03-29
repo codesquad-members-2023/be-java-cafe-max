505ede86d8c5ab5d4be6998abdd0795d496a8174
crtEvent <crtevn@gmail.com>
2023-03-29 14:53:22 +0900

    [1단계-회원목록-개선-01] 회원 목록 입/출력을 구분하기 위해 DTO 추가

---
+++ src/main/java/kr/codesqaud/cafe/dto/SignUpDTO.java
@@ -0,0 +1,47 @@
+package kr.codesqaud.cafe.dto;
+
+import kr.codesqaud.cafe.domain.User;
+
+public class SignUpDTO {
+    private String userId;
+    private String password;
+    private String name;
+    private String email;
+
+    public SignUpDTO(String userId, String password, String name, String email) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public User convertToUser(int idx) {
+        return new User(idx, userId, password, name, email);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId='" + userId + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
