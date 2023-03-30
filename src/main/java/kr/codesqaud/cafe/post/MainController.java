a8c230f8d8e07e9cb8dddbc9352c91b39e5bdbc5
ALbertIM0427 <guang7226961@naver.com>
2023-03-30 11:02:49 +0900

    fix: 메인 컨트롤러에서 posts 전체정보에서 simple 정보만 보내도록 수정

--- src/main/java/kr/codesqaud/cafe/post/MainController.java
+++ src/main/java/kr/codesqaud/cafe/post/MainController.java
@@ -1,4 +1,4 @@
-package kr.codesqaud.cafe.main;
+package kr.codesqaud.cafe.post;
 
 import java.util.List;
 
@@ -7,21 +7,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import kr.codesqaud.cafe.post.Post;
+import kr.codesqaud.cafe.post.PostService;
 import kr.codesqaud.cafe.post.PostsRepository;
+import kr.codesqaud.cafe.post.form.SimplePostForm;
 
 @Controller
 public class MainController {
 
 	private final PostsRepository postsRepository;
+	private final PostService postService;
 
-	public MainController(PostsRepository postsRepository) {
+	public MainController(PostsRepository postsRepository, PostService postService) {
 		this.postsRepository = postsRepository;
+		this.postService = postService;
 	}
 
 	@GetMapping("/")
 	public String showMainPage(Model model) {
 		List<Post> posts = postsRepository.getAllPosts();
-		model.addAttribute("posts", posts);
+		List<SimplePostForm> simpleForms = postService.mappingSimpleForm(posts);
+		model.addAttribute("simpleForms", simpleForms);
 		return "index";
 	}
 
