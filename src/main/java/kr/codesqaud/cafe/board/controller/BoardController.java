bed8efedf792b4a3f37d7bc97bf687b463e6db13
othertkfka <57559288+othertkfka@users.noreply.github.com>
2023-03-30 12:14:47 +0900

    feat: 글쓰기 기능 구현
    post로 "/board/write"로 요청을 하면 실행되는 writePost()를 구현했습니다.
    boardRepository를 이용해서 게시글 데이터를 저장하고 "/"로 리다이렉트 합니다.

--- src/main/java/kr/codesqaud/cafe/board/controller/BoardController.java
+++ src/main/java/kr/codesqaud/cafe/board/controller/BoardController.java
@@ -1,16 +1,32 @@
 package kr.codesqaud.cafe.board.controller;
 
+import kr.codesqaud.cafe.board.domain.BoardPost;
+import kr.codesqaud.cafe.board.repository.BoardRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 
+    private final BoardRepository boardRepository;
+
+    public BoardController(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
+
     @GetMapping("/write")
     public String writePost() {
         return "/board/write";
+    }
+
+    @PostMapping("/write")
+    public String writePost(@ModelAttribute BoardPost boardPost) {
+        boardRepository.add(boardPost);
+        return "redirect:/";
     }
 
 }
