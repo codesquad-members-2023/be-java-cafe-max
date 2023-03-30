bc1f0f177f230170a7c369a323cf310a43bfcd23
othertkfka <57559288+othertkfka@users.noreply.github.com>
2023-03-30 12:11:13 +0900

    feat: 게시글 데이터를 관리하는 BoardRepository 구현
    key는 boardId, value는 BoardPost를 가지는 HashMap에 게시글 데이터를 저장합니다.
    BoardPost를 저장해주는 add(), boardId로 BoardPost를 꺼내주는 getPost()
    BoardPost 전체 리스트를 반환해주는 getPostList()를 구현했습니다.

---
+++ src/main/java/kr/codesqaud/cafe/board/repository/BoardRepository.java
@@ -0,0 +1,26 @@
+package kr.codesqaud.cafe.board.repository;
+
+import kr.codesqaud.cafe.board.domain.BoardPost;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Repository
+public class BoardRepository {
+    Map<Long, BoardPost> boardData = new HashMap<>();
+
+    public void add(BoardPost boardPost) {
+        boardData.put(boardPost.getBoardId(), boardPost);
+    }
+
+    public BoardPost getPost(Long boardId) {
+        return boardData.get(boardId);
+    }
+
+    public List<BoardPost> getPostList() {
+        return boardData.values().stream().collect(Collectors.toList());
+    }
+}
