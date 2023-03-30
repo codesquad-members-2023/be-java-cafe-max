cdeec33c07904069c3ba770aa6bd9da19c9dee43
CDBchan <kdjin1215@naver.com>
2023-03-31 06:50:52 +0900

    feat: profileEditDto추가
    이전패스워드와 변경할 패스워드를 가진 DTO가 필요해 profileEditDto를 생성했다.

---
+++ src/main/java/kr/codesqaud/cafe/dto/ProfileEditDto.java
@@ -0,0 +1,61 @@
+package kr.codesqaud.cafe.dto;
+
+public class ProfileEditDto {
+    private String nickName;
+    private String email;
+    private String newPassword;
+    private String oriPassword;
+    private Integer id;
+
+    public ProfileEditDto(String nickName, String email, String newPassword, String oriPassword, Integer id) {
+        this.nickName = nickName;
+        this.email = email;
+        this.newPassword = newPassword;
+        this.oriPassword = oriPassword;
+        this.id = id;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+
+    public String getOriPassword() {
+        return oriPassword;
+    }
+
+    public void setOriPassword(String oriPassword) {
+        this.oriPassword = oriPassword;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public UserDto toUserDto(){
+        return new UserDto(nickName,email,newPassword,id);
+    }
+}
