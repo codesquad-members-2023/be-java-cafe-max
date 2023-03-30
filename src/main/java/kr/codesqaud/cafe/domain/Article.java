c97087ee2065e5ac872203a20a1d48b0efefe30c
yeonise <cerealnum@gmail.com>
2023-03-30 13:32:41 +0900

    Article Domain 생성
    인덱스, 제목, 작성자, 내용, 등록일을 갖는 Article Domain을 생성합니다.
    ---
    getter와 setter를 추가합니다.

---
+++ src/main/java/kr/codesqaud/cafe/domain/Article.java
@@ -0,0 +1,58 @@
+package kr.codesqaud.cafe.domain;
+
+import java.time.LocalDateTime;
+
+public class Article {
+    private Long id;
+    private String title;
+    private String writer;
+    private String contents;
+    private final LocalDateTime regTime;
+
+    public Article() {
+        this.regTime = LocalDateTime.now();
+    }
+
+    public Article(String title, String writer, String contents) {
+        this.title = title;
+        this.writer = writer;
+        this.contents = contents;
+        this.regTime = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public LocalDateTime getRegTime() {
+        return regTime;
+    }
+}
