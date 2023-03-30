eb4d9eb58389e1aa517943c23f82f912b93cf8e5
code-tree <dltjdqls1200@naver.com>
2023-03-30 10:58:21 +0900

    [STEP-1] feat: 회원 가입 필드 검증 오류시 다시 입력 받는 기능 추가

--- src/main/java/kr/codesqaud/cafe/controller/UserController.java
+++ src/main/java/kr/codesqaud/cafe/controller/UserController.java
@@ -5,10 +5,13 @@ import kr.codesqaud.cafe.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @RequestMapping("/users")
 @Controller
@@ -22,13 +25,18 @@ public class UserController {
 
     @GetMapping
     public String list(Model model) {
+        System.out.println("check");
         model.addAttribute("users", userService.findAll());
 
         return "user/list";
     }
 
     @PostMapping
-    public String create(@ModelAttribute UserJoinDto userJoinDto) {
+    public String create(@Valid @ModelAttribute("user") UserJoinDto userJoinDto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "user/form";
+        }
+
         userService.join(userJoinDto);
 
         return "redirect:/users";
--- src/main/java/kr/codesqaud/cafe/controller/dto/UserJoinDto.java
+++ src/main/java/kr/codesqaud/cafe/controller/dto/UserJoinDto.java
@@ -2,16 +2,34 @@ package kr.codesqaud.cafe.controller.dto;
 
 import kr.codesqaud.cafe.domain.User;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class UserJoinDto {
+    @NotBlank(message = "아이디를 입력하세요.")
+    @Size(min = 5, max = 20, message = "아이디는 {min} ~ {max} 길이로 입력하세요.")
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "아이디는 알파벳, 숫자만 가능합니다.")
     private String userId;
-    private String name;
+
+    @NotBlank(message = "비밀번호를 입력하세요.")
+    @Size(min = 8, max = 20, message = "비밀번호는 {min} ~ {max} 길이로 입력하세요.")
+    @Pattern(regexp = "[a-zA-Z0-9!@#$%^&*()_+|~=`{}\\[\\]:\";'\\\\<>,.?/\\-]+", message = "비밀번호는 알파벳, 숫자, 특수문자만 가능합니다.")
     private String password;
+
+    @NotBlank(message = "이름을 입력하세요.")
+    @Size(min = 2, max = 4, message = "유효한 이름을 입력하세요.")
+    @Pattern(regexp = "^[가-힣]{2,4}$", message = "이름 형식에 맞게 입력하세요.")
+    private String name;
+
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
     private String email;
 
-    public UserJoinDto(String userId, String name, String password, String email) {
+    public UserJoinDto(String userId, String password, String name, String email) {
         this.userId = userId;
-        this.name = name;
         this.password = password;
+        this.name = name;
         this.email = email;
     }
 
@@ -26,20 +44,20 @@ public class UserJoinDto {
         this.userId = userId;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getPassword() {
         return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getEmail() {
