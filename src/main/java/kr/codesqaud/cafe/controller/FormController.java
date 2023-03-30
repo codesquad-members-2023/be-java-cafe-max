87a6039f2ad36858df5d9efcf3fbb3bc106e35b7
sudago <sudago22@gmail.com>
2023-03-30 18:48:12 +0900

    feat: 연습을 위한 컨트롤러 추가

--- src/main/java/kr/codesqaud/cafe/controller/FormController.java
+++ src/main/java/kr/codesqaud/cafe/controller/FormController.java
@@ -1,12 +1,14 @@
 package kr.codesqaud.cafe.controller;
 
+import org.springframework.context.annotation.Conditional;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class HomeController {
-    @GetMapping("/")
+@Controller()
+public class FormController {
+
+    @GetMapping("/users")
     public String home(){
-        return "index";
+        return "createUser";
     }
 }
