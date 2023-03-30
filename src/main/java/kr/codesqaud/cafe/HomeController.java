1671a57220975311838a45890be6d6409a71eb76
othertkfka <57559288+othertkfka@users.noreply.github.com>
2023-03-30 13:44:41 +0900

    refactor: "/"로 요청했을 경우 redirect하도록 수정
    첫 진입 페이지를 글 목록을 조회하여 보여주도록 redirect로 변경했습니다.

--- src/main/java/kr/codesqaud/cafe/HomeController.java
+++ src/main/java/kr/codesqaud/cafe/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/")
     public String home() {
-        return "index";
+        return "redirect:/board/list";
     }
 }
