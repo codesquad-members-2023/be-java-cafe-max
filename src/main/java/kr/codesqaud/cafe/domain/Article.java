bb2b3726ccf46d2690ef03be9c2ae730ffb3228f
Sully <2dcoder@naver.com>
2023-03-30 22:15:05 +0900

    feat: 글쓴이, 제목, 내용 정보를 담고 있는 Article 클래스 생성

---
+++ src/main/java/kr/codesqaud/cafe/domain/Article.java
@@ -0,0 +1,40 @@
+package kr.codesqaud.cafe.domain;
+
+public class Article {
+    private String name;
+    private String title;
+    private String content;
+
+    public Article() {
+    }
+
+    public Article(String name, String title, String content) {
+        this.name = name;
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
