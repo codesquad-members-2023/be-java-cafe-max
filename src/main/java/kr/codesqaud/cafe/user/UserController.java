3aabe82d7a4ca40a8dab0af92cfb14552d6729a3
n-aa-g <wlsdnjs0124@gmail.com>
2023-03-30 14:09:32 +0900

    Feat($user): UserController에 회원 목록을 띄우는 기능 추가
    회원 목록을 띄우는 메서드를 만들고 "/list"로 매핑했습니다.

--- src/main/java/kr/codesqaud/cafe/user/UserController.java
+++ src/main/java/kr/codesqaud/cafe/user/UserController.java
@@ -1,9 +1,12 @@
 package kr.codesqaud.cafe.user;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/user")
@@ -19,7 +22,15 @@ public class UserController {
     public String signup(UserForm userForm) {
         userService.saveUser(userForm);
 
-        return "redirect:/user/signup";
+        return "redirect:/user/list";
+    }
+
+    @GetMapping("/list")
+    public String showList(Model model) {
+        List<User> users = userService.getUserList();
+        model.addAttribute("users", users);
+
+        return "user/list";
     }
 
 }
