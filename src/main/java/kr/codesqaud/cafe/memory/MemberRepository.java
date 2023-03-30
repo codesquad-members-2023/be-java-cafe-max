12f686e8264cc87f084228e850cad4189dada0e0
codemojae <116280283+codemojae@users.noreply.github.com>
2023-03-30 22:37:38 +0900

    Repository 단에서 필요할 것 같은 검색메소드 틀 추가
    이후에 중복되지 않게 할 LoginID, email, 그리고 전체검색 함수를 만들 예정입니다.

--- src/main/java/kr/codesqaud/cafe/memory/MemberRepository.java
+++ src/main/java/kr/codesqaud/cafe/memory/MemberRepository.java
@@ -2,7 +2,13 @@ package kr.codesqaud.cafe.memory;
 
 import kr.codesqaud.cafe.domain.Member;
 
+import java.util.List;
+
 public interface MemberRepository {
     Member save(Member member);
     Member findMemberByUid(Long id);
+
+    Member findMemberByLoginId(String loginId);
+    Member findMemberByEmail(String email);
+    List<Member> findAllMember();
 }
--- src/main/java/kr/codesqaud/cafe/memory/MemoryMemberRepository.java
+++ src/main/java/kr/codesqaud/cafe/memory/MemoryMemberRepository.java
@@ -2,6 +2,7 @@ package kr.codesqaud.cafe.memory;
 
 import kr.codesqaud.cafe.domain.Member;
 
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class MemoryMemberRepository implements MemberRepository {
@@ -17,7 +18,23 @@ public class MemoryMemberRepository implements MemberRepository {
         return member;
     }
 
+    @Override
     public Member findMemberByUid(Long uid) {
         return repository.get(uid);
+    }
+
+    @Override
+    public Member findMemberByLoginId(String loginId) {
+        return null;
+    }
+
+    @Override
+    public Member findMemberByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public List<Member> findAllMember() {
+        return null;
     }
 }
