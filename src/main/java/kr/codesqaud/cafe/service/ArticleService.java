8059bb254b5207d23135687f1b2750dca27bd02f
Sully <2dcoder@naver.com>
2023-03-31 09:37:45 +0900

    feat: ArticleRepository를 객체로 갖고 관리하는 서비스 생성

---
+++ src/main/java/kr/codesqaud/cafe/service/ArticleService.java
@@ -0,0 +1,28 @@
+package kr.codesqaud.cafe.service;
+
+import kr.codesqaud.cafe.domain.Article;
+import kr.codesqaud.cafe.repository.ArticleRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ArticleService {
+    private final ArticleRepository articleRepository;
+
+    public ArticleService(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
+    public void add(Article article) {
+        articleRepository.save(article);
+    }
+
+    public List<Article> findArticles() {
+        return articleRepository.findAll();
+    }
+
+    public Article findByName(String name) {
+        return articleRepository.findByName(name);
+    }
+}
