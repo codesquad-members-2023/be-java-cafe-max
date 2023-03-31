651b71bfa15b781cfb78d79dad98fff0903a3300
Hana <dokkisan.wang@gmail.com>
2023-03-31 15:45:09 +0900

    feat: nav fragment 링크 연결(index, 로그인, 회원가입 페이지)

--- src/main/java/kr/codesqaud/cafe/controller/HomeController.java
+++ src/main/java/kr/codesqaud/cafe/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping(value = {"/", "/index"})
     public String home() {
         return "index";
     }
