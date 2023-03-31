f71df14c3aaec48616a78051f52fd17bc07f45b1
JJONSOO <pjs970616@naver.com>
2023-03-31 17:43:35 +0900

    [feat] User를 저장한 후 users로 redirect 하는 메서드 구현

--- src/main/java/kr/codesqaud/cafe/domain/user/controller/UserController.java
+++ src/main/java/kr/codesqaud/cafe/domain/user/controller/UserController.java
@@ -1,7 +1,9 @@
 package kr.codesqaud.cafe.domain.user.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import kr.codesqaud.cafe.domain.user.dto.UserSaveRequestDto;
 import kr.codesqaud.cafe.domain.user.repository.UserRepository;
 
 @Controller
@@ -11,5 +13,11 @@ public class UserController {
 
 	UserController(UserRepository userRepository) {
 		this.userRepository = userRepository;
+	}
+
+	@PostMapping("/user/join")
+	public String signIn(UserSaveRequestDto userSaveRequestDto) {
+		userRepository.save(userSaveRequestDto.toEntity());
+		return "redirect:/users";
 	}
 }
