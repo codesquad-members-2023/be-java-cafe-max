263b21d1f0da9ae4cc3848bb3da65f738b253715
JJONSOO <pjs970616@naver.com>
2023-03-31 17:28:07 +0900

    [feat] 초기 화면에서 글 목록을 보여주기 위한 메서드 구현

--- src/main/java/kr/codesqaud/cafe/domain/article/controller/ArticleController.java
+++ src/main/java/kr/codesqaud/cafe/domain/article/controller/ArticleController.java
@@ -1,6 +1,8 @@
 package kr.codesqaud.cafe.domain.article.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import kr.codesqaud.cafe.domain.article.repository.ArticleRepository;
 
@@ -11,5 +13,11 @@ public class ArticleController {
 
 	public ArticleController(ArticleRepository articleRepository) {
 		this.articleRepository = articleRepository;
+	}
+
+	@GetMapping("/")
+	public String main(Model model) {
+		model.addAttribute("articles", articleRepository.findAll());
+		return "index";
 	}
 }
