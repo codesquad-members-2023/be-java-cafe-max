838223b139522c8ac2dc24757496289a87470abd
sejeong00 <buttermelt@daum.net>
2023-03-31 10:13:01 +0900

    feat 게시글 저장하는 기능 구현

--- src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
+++ src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
@@ -1,14 +1,49 @@
 package kr.codesqaud.cafe.controller;
 
+import kr.codesqaud.cafe.domain.Article;
+import kr.codesqaud.cafe.repository.ArticleMemoryRepository;
+import kr.codesqaud.cafe.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ArticleController {
+
+    private final ArticleRepository articleRepository;
+
+    @Autowired
+    public ArticleController(ArticleMemoryRepository articleMemoryRepository) {
+        this.articleRepository = articleMemoryRepository;
+    }
 
     @GetMapping("/qna/write")
     public String write(){
         return "qna/form";
     }
+
+    @PostMapping("/qna/create")
+    public String create(
+            @RequestParam("writer") String writer,
+            @RequestParam("title") String title,
+            @RequestParam("contents") String contents,
+            Model model
+    ){
+        Article article = new Article();
+
+        article.setWriter(writer);
+        article.setTitle(title);
+        article.setContents(contents);
+
+        articleRepository.save(article);
+
+        model.addAttribute("article", article);
+
+        return "redirect:/";
+    }
+
+
 }
