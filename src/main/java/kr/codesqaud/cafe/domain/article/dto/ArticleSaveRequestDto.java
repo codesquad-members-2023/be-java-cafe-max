b59eb3c93063c67627cf6e9804099924acd5a8e9
JJONSOO <pjs970616@naver.com>
2023-03-31 17:25:40 +0900

    [feat] Client에서 Article를 등록할 때 필요한 Dto 추가

---
+++ src/main/java/kr/codesqaud/cafe/domain/article/dto/ArticleSaveRequestDto.java
@@ -0,0 +1,25 @@
+package kr.codesqaud.cafe.domain.article.dto;
+
+import kr.codesqaud.cafe.domain.article.entity.Article;
+
+public class ArticleSaveRequestDto {
+	String title;
+	String content;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public ArticleSaveRequestDto(String title, String content) {
+		this.title = title;
+		this.content = content;
+	}
+
+	public Article toEntity() {
+		return new Article(title, content);
+	}
+}
