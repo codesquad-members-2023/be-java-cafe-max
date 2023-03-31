1d4d372d26a55200617cbdd9d3423966e14e205c
sudago <sudago22@gmail.com>
2023-03-31 16:34:32 +0900

    feat: 회원 가입 기능 추가
    User 객체와 MemoryUserRepository에 저장

--- src/main/java/kr/codesqaud/cafe/controller/UserController.java
+++ src/main/java/kr/codesqaud/cafe/controller/UserController.java
@@ -4,8 +4,12 @@ import kr.codesqaud.cafe.domain.User;
 import kr.codesqaud.cafe.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -23,10 +27,25 @@ public class UserController {
     @PostMapping("/user/create")
     public String create(UserForm form){
         User user = new User();
-        user.setName(form.getUserId());
+        user.setUserId(form.getUserId());
+        user.setPassword(form.getPassword());
+        user.setName(form.getName());
+        user.setEmail(form.getEmail());
 
         userService.join(user);
-        return "redirect:/";
+        return "redirect:/users";
+    }
+
+    @GetMapping("/users")
+    public String list(Model model){
+        List<User> users = userService.findUsers();
+        model.addAttribute("users", users);
+        return "/user/list";
+    }
+
+    @GetMapping("/users/profile") // ("/users/{{userId}}")
+    public String profile(){
+        return "/user/profile";
     }
 
 }
