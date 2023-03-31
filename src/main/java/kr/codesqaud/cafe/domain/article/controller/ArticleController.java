6cf0ea1469916e171492ec286e29195545bf965e
JJONSOO <pjs970616@naver.com>
2023-03-31 17:28:46 +0900

    [feat] Client가 글 등록을 할 수 있는 메서드 구현

--- src/main/java/kr/codesqaud/cafe/domain/article/controller/ArticleController.java
+++ src/main/java/kr/codesqaud/cafe/domain/article/controller/ArticleController.java
@@ -3,7 +3,9 @@ package kr.codesqaud.cafe.domain.article.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import kr.codesqaud.cafe.domain.article.dto.ArticleSaveRequestDto;
 import kr.codesqaud.cafe.domain.article.repository.ArticleRepository;
 
 @Controller
@@ -19,5 +21,11 @@ public class ArticleController {
 	public String main(Model model) {
 		model.addAttribute("articles", articleRepository.findAll());
 		return "index";
+	}
+
+	@PostMapping("/questions")
+	public String askQuestions(ArticleSaveRequestDto articleSaveRequestDto) {
+		articleRepository.save(articleSaveRequestDto.toEntity());
+		return "redirect:/";
 	}
 }
