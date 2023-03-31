fa5f21881decb0dce6cd910719cfa87034b58cd0
sejeong00 <buttermelt@daum.net>
2023-03-31 11:55:45 +0900

    feat 인덱스에서 게시글 제목을 누르면 게시글 상세페이지로 이동해서 게시글 상세정보를 조회하는 기능 구현

--- src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
+++ src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -52,4 +53,10 @@ public class ArticleController {
         return "index";
     }
 
+    @GetMapping("/articles/{index}")
+    public String showDetail(@PathVariable("index") long index, Model model){
+        model.addAttribute("article", articleRepository.findByIndex(index));
+
+        return "qna/show";
+    }
 }
