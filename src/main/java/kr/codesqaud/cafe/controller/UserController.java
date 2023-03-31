ad92a8de4065c11d6c6510e6d89ffa2cfa24d65f
pinetree <eodnjs568@gmail.com>
2023-03-31 09:46:49 +0900

    refactor "UserController 내 GetMapping, PostMapping 메서드 수정 및 template 내 HTML 경로 변경"
    GetMapping 및 PostMapping 적용오류 현상 발생.
    페어리뷰 중 도움을 받아서 메서드의 return 값등 변경.

--- src/main/java/kr/codesqaud/cafe/controller/UserController.java
+++ src/main/java/kr/codesqaud/cafe/controller/UserController.java
@@ -1,10 +1,14 @@
 package kr.codesqaud.cafe.controller;
 
+import kr.codesqaud.cafe.domain.User;
 import kr.codesqaud.cafe.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping
 @Controller
 public class UserController {
 
@@ -17,9 +21,22 @@ public class UserController {
 
     @GetMapping("user/form")
     public String loginPage() {
-        return "form.html";
+        return "user/form";
+    }
+    // 정보를 받아오는 느낌
+
+    @PostMapping("user/form")
+    public String register(User user) {
+        userService.join(user);
+        return "redirect:/user/list";
+    }
+    @GetMapping("user/list")
+    public String getUserList() {
+        return "user/list";
     }
 
+    // ID, PW를 보내면
+    // 글쓰기 같은 것?
 
 
 }
