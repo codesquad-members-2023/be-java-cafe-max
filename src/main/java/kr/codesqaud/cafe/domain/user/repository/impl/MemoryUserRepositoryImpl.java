a0e7453ba0069344e21b2bbdc49b520881ea4e4b
JJONSOO <pjs970616@naver.com>
2023-03-31 17:36:02 +0900

    [feat] Java Map에 User Entity를 추가하는 메서드 구현

--- src/main/java/kr/codesqaud/cafe/domain/user/repository/impl/MemoryUserRepositoryImpl.java
+++ src/main/java/kr/codesqaud/cafe/domain/user/repository/impl/MemoryUserRepositoryImpl.java
@@ -6,9 +6,16 @@ import java.util.concurrent.ConcurrentHashMap;
 import org.springframework.stereotype.Repository;
 
 import kr.codesqaud.cafe.domain.user.entity.User;
+import kr.codesqaud.cafe.domain.user.repository.UserRepository;
 
 @Repository
-public class MemoryUserRepositoryImpl {
+public class MemoryUserRepositoryImpl implements UserRepository {
 
 	private Map<Integer, User> users = new ConcurrentHashMap<>();
+	private static Integer autoIncrement = 0;
+
+	public void save(User user) {
+		user.setId(++autoIncrement);
+		users.put(autoIncrement, user);
+	}
 }
