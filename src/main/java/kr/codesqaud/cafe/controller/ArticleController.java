d33649e6c851389d83c29cc3e1f5abb00435daef
he2joo <hottestej6@gamil.com>
2023-03-31 12:13:57 +0900

    feat : 작성 글 목록 보기 기능 구현

--- src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
+++ src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
@@ -5,7 +5,12 @@ import kr.codesqaud.cafe.dto.ArticleForm;
 import kr.codesqaud.cafe.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Controller
 public class ArticleController {
@@ -17,12 +22,21 @@ public class ArticleController {
         this.articleService = articleService;
     }
 
-    @PostMapping("/questions")
+    @PostMapping("/qna/form")
     public String write(ArticleForm form) {
 
         Article article = new Article(form.getWriter(), form.getTitle(), form.getContents());
         articleService.write(article);
 
         return "redirect:/";
+    }
+
+    //todo : "/"을 담당하는 컨트롤러에서 이 기능 구현?
+    @GetMapping(value = {"/", "/index"})
+    public String list(Model model) {
+        List<Article> articles = articleService.findArticles();
+        model.addAttribute("articles", articles);
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "/index";
     }
 }
--- src/main/java/kr/codesqaud/cafe/controller/HomeController.java
+++ src/main/java/kr/codesqaud/cafe/controller/HomeController.java
@@ -1,17 +1,15 @@
 package kr.codesqaud.cafe.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
 
-    @GetMapping(value = {"/", "/index"})
-    public String home() {
-        return "/index";
-    }
+//    @GetMapping(value = {"/", "/index"})
+//    public String home() {
+//        return "/index";
+//    }
 
     @GetMapping("user/login")
     public String loginForm() {
