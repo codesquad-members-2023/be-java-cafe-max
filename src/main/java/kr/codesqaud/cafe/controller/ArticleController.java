12fb66dee616ac2fc707936e0bd8dfad04dd4536
Sully <2dcoder@naver.com>
2023-03-31 12:30:39 +0900

    feat: show.html에 articleIndex를 넘겨주도록 구현
    이를 위해 findByArticleIndex 메서드를 만들었음

--- src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
+++ src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -44,6 +45,13 @@ public class ArticleController {
         List<Article> articles = articleService.findArticles();
         model.addAttribute("articles", articles);
         return "index";
+    }
+
+    @GetMapping("/questions/{articleIndex}")
+    public String articleQna(@PathVariable("articleIndex") Long articleIndex, Model model) {
+        Article article = articleService.findByArticleIndex(articleIndex);
+        model.addAttribute("article", article);
+        return "qna/show";
     }
 
     @GetMapping("/questions/show")
