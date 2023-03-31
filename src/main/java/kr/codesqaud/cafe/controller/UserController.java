c751ad812c57df10b0bc0a0aa26cbe448099c998
O_jeegu <lumirite0@gmail.com>
2023-03-31 09:09:26 +0900

    feat : 회원 정보 페이지로 이동하는 기능 추가
     회원의 id를 주소로 받아  회원 정보 페이지로 이동

--- src/main/java/kr/codesqaud/cafe/controller/UserController.java
+++ src/main/java/kr/codesqaud/cafe/controller/UserController.java
@@ -1,12 +1,14 @@
 package kr.codesqaud.cafe.controller;
 
-import kr.codesqaud.cafe.domain.User;
+import kr.codesqaud.cafe.controller.dto.UserJoinDTO;
+import kr.codesqaud.cafe.controller.dto.UserReadDTO;
 import kr.codesqaud.cafe.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -27,15 +29,22 @@ public class UserController {
     }
 
     @PostMapping("/user/join")
-    public String signUp(@ModelAttribute User user) { // todo : 회원가입용 dto 만드는게 좋음(검증 필요)
-        userService.signUp(user);
+    public String signUp(@ModelAttribute UserJoinDTO userJoinDTO) {
+        userService.signUp(userJoinDTO);
         return "redirect:/users";
     }
 
     @GetMapping("/users")
     public String list(Model model) {
-        List<User> users = userService.findUsers();
+        List<UserReadDTO> users = userService.findUsers();
         model.addAttribute("users", users);
         return "user/list";
+    }
+
+    @GetMapping("/users/{id}")
+    public String profileFrom(@PathVariable Long id, Model model) {
+        UserReadDTO wantedUser = userService.findOne(id);
+        model.addAttribute("wantedUser", wantedUser);
+        return "user/profile";
     }
 }
