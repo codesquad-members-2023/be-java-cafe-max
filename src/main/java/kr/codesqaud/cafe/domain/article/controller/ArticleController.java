23a6041d20872de1668520dadeae572380efc896
JJONSOO <pjs970616@naver.com>
2023-03-31 17:29:22 +0900

    [feat] id로 특정 글을 볼 수 있는 메서드 구현

--- src/main/java/kr/codesqaud/cafe/domain/article/controller/ArticleController.java
+++ src/main/java/kr/codesqaud/cafe/domain/article/controller/ArticleController.java
@@ -3,6 +3,7 @@ package kr.codesqaud.cafe.domain.article.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import kr.codesqaud.cafe.domain.article.dto.ArticleSaveRequestDto;
@@ -27,5 +28,11 @@ public class ArticleController {
 	public String askQuestions(ArticleSaveRequestDto articleSaveRequestDto) {
 		articleRepository.save(articleSaveRequestDto.toEntity());
 		return "redirect:/";
+	}
+	
+	@GetMapping("/questions/{id}")
+	public String viewArticle(@PathVariable("id") Integer id, Model model) {
+		model.addAttribute("article", articleRepository.findById(id));
+		return "/post/show";
 	}
 }
