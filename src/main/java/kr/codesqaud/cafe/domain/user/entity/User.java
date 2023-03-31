8fdd502724cdfc8d6d9f194097756a6ec8bdc730
JJONSOO <pjs970616@naver.com>
2023-03-31 17:31:38 +0900

    [feat] User Entity 클래스 추가

---
+++ src/main/java/kr/codesqaud/cafe/domain/user/entity/User.java
@@ -0,0 +1,41 @@
+package kr.codesqaud.cafe.domain.user.entity;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class User {
+
+	private String password;
+	private String name;
+	private String email;
+	private Integer id;
+	String dateTime;
+
+	public User(String name, String password, String email) {
+		this.password = password;
+		this.name = name;
+		this.email = email;
+		this.dateTime = setDateTime();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String setDateTime() {
+		LocalDateTime now = LocalDateTime.now();
+		return now.format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일(E) HH:mm:ss"));
+	}
+}
