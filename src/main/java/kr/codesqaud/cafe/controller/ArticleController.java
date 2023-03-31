f2ebfa871eaed49669ecc3d1b6be0795dc00e9c5
Sully <2dcoder@naver.com>
2023-03-31 09:41:20 +0900

    feat: 글 쓴 정보를 메인 페이지로 리다이렉트 하도록 구현
    MvcConfig를 사용하려 했지만 잘 되지 않아 TODO로 남겨두었다.
    글 썼던 정보는 form으로 다 잘 넘어온다.

--- src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
+++ src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
@@ -1,15 +1,47 @@
 package kr.codesqaud.cafe.controller;
 
+import kr.codesqaud.cafe.config.MvcConfig;
+import kr.codesqaud.cafe.controller.dto.ArticleForm;
+import kr.codesqaud.cafe.domain.Article;
+import kr.codesqaud.cafe.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
-    // TODO: WebMVCConfigurer 적용하기
-    @GetMapping("/questions/form")
-    public String postForm() {
+    private final ArticleService articleService;
+    // TODO: mvcConfig 이용해 보기
+    private final MvcConfig mvcConfig;
+
+    @Autowired
+    public ArticleController(ArticleService articleService, MvcConfig mvcConfig) {
+        this.articleService = articleService;
+        this.mvcConfig = mvcConfig;
+    }
+
+    @GetMapping("/questions/add")
+    public String addForm() {
         return "qna/form";
     }
+
+    @PostMapping("/questions/post")
+    public String postArticle(ArticleForm form) {
+        Article article = new Article(form.getWriter(), form.getTitle(), form.getContents());
+        article.setWriter(article.getWriter());
+
+        articleService.add(article);
+
+        return "redirect:/";
+    }
+
+//    @GetMapping
+//    public String articleList(Model model) {
+//        List<Article> articles = articleService.findArticles();
+//        model.addAttribute("articles", articles);
+//        return "index";
+//    }
 
     @GetMapping("/questions/show")
     public String showForm() {
