7eb9c55b03a20a637f340e3d311be5c8b73b8d40
JJONSOO <pjs970616@naver.com>
2023-03-31 17:41:46 +0900

    [feat] User를 수정할 때 필요한 dto 추가

---
+++ src/main/java/kr/codesqaud/cafe/domain/user/dto/UserUpdateDto.java
@@ -0,0 +1,27 @@
+package kr.codesqaud.cafe.domain.user.dto;
+
+import kr.codesqaud.cafe.domain.user.entity.User;
+
+public class UserUpdateDto {
+
+	private String name;
+	private String email;
+	private String password;
+
+	public UserUpdateDto(String name, String email, String password) {
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public User toEntity(Integer id) {
+		User convertedUser = new User(name, password, email);
+		convertedUser.setId(id);
+		return convertedUser;
+	}
+
+}
