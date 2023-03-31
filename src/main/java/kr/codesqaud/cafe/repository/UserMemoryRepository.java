7d48a454208e591941cd4bb08be08b6f49ea6bf1
sejeong00 <buttermelt@daum.net>
2023-03-31 12:01:21 +0900

    style 쓸데없는 주석 제거

--- src/main/java/kr/codesqaud/cafe/repository/UserMemoryRepository.java
+++ src/main/java/kr/codesqaud/cafe/repository/UserMemoryRepository.java
@@ -11,10 +11,8 @@ import java.util.concurrent.ConcurrentHashMap;
 @Repository
 public class UserMemoryRepository implements UserRepository {
 
-    //디버깅하다가 nonstatic으로 바꾸긴 했는데 강의에서 왜 static 이었을까?? -> 싱글톤으로 사용하기 위해
     private static final Map<String, User> userList = new ConcurrentHashMap<>();
 
-    //TODO: 왜 제 개발자모드에서는 GET, POST 가 안나오는 건가여
     @Override
     public User save(User user) {
         userList.put(user.getUserId(), user);
