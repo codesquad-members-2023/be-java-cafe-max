3969e3a537ce386d779b73a6d371151e86911393
chacha <peony@bluehole.net>
2023-04-02 00:42:53 +0900

    feat MemberController에 findAll 기능 추가

---
+++ src/main/java/kr/codesqaud/cafe/controller/MemberController.java
@@ -0,0 +1,36 @@
+package kr.codesqaud.cafe.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.NoSuchElementException;
+
+import kr.codesqaud.cafe.dto.ProfileEditRequestDto;
+import kr.codesqaud.cafe.dto.SignUpRequestDto;
+import kr.codesqaud.cafe.service.MemberService;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+    private final MemberService memberService;
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping
+    public String findAll(Model model){
+        model.addAttribute("memberResponsesDto",memberService.findAll());
+        return "/members";
+    }
+
+}
