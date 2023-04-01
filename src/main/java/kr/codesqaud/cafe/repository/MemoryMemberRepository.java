86e574c13806e162fdb4dcb945a3442e891398c9
chacha <peony@bluehole.net>
2023-04-02 00:44:06 +0900

    feat MemoryMemberRepository에 findAll 기능 추가

---
+++ src/main/java/kr/codesqaud/cafe/repository/MemoryMemberRepository.java
@@ -0,0 +1,31 @@
+package kr.codesqaud.cafe.repository;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+import kr.codesqaud.cafe.domain.Member;
+
+@Repository
+public class MemoryMemberRepository implements MemberRepository {
+    private final Map<String, Member> store;
+
+    public MemoryMemberRepository() {
+        this.store = new ConcurrentHashMap<>();
+    }
+
+
+    @Override
+    public List<Member> findAll() {
+        return store.values()
+                .stream()
+                .sorted(Comparator.comparing(Member::getCreateTime).reversed())
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+}
