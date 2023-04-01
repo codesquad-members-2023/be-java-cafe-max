6567ab812cf4419ce8077ddde3d5333d4e5f76a2
CDBchan <kdjin1215@naver.com>
2023-04-02 02:33:56 +0900

    refactor: WebConfigurerAdapter -> WebMvcConfigurer
    registry.setOrder도 필요어 삭제했습니다.

--- src/main/java/kr/codesqaud/cafe/MvcConfig.java
+++ src/main/java/kr/codesqaud/cafe/MvcConfig.java
@@ -1,16 +1,13 @@
 package kr.codesqaud.cafe;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class MvcConfig extends WebMvcConfigurerAdapter {
+public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
-
         //user
         registry.addViewController("/user/login").setViewName("/user/login");
         registry.addViewController("/user/login_success").setViewName("/user/login_success");
