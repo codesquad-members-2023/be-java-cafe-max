7c71dd2c901b169aa538356b6eb1d4b42927c74c
CDBchan <kdjin1215@naver.com>
2023-04-02 01:47:38 +0900

    refactor: UserService bean등록

--- src/main/java/kr/codesqaud/cafe/service/UserService.java
+++ src/main/java/kr/codesqaud/cafe/service/UserService.java
@@ -3,9 +3,11 @@ package kr.codesqaud.cafe.service;
 import kr.codesqaud.cafe.controller.dto.ProfileEditDTO;
 import kr.codesqaud.cafe.domain.User;
 import kr.codesqaud.cafe.repository.UserRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class UserService {
 
     private UserRepository userRepository;
