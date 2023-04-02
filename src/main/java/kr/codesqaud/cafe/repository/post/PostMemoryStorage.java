e257d5280213d10fc649bed8ca31cb5ed1f095be
chacha <peony@bluehole.net>
2023-04-02 10:52:46 +0900

    feat repository에 save 기능 추가

---
+++ src/main/java/kr/codesqaud/cafe/repository/post/PostMemoryStorage.java
@@ -0,0 +1,31 @@
+package kr.codesqaud.cafe.repository.post;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import kr.codesqaud.cafe.domain.Post;
+
+@Repository
+public class PostMemoryStorage implements PostRepository{
+    private final Map<UUID, Post> postMap;
+    private final UUID postId;
+
+    public PostMemoryStorage() {
+        this.postMap = new HashMap<>();
+        this.postId = UUID.randomUUID();
+    }
+
+    @Override
+    public UUID save(Post post) {
+        postMap.put(postId,post.of(post.getPostId()));
+        return postId;
+    }
+
+}
---
+++ src/main/java/kr/codesqaud/cafe/repository/post/PostRepository.java
@@ -0,0 +1,12 @@
+package kr.codesqaud.cafe.repository.post;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import kr.codesqaud.cafe.domain.Post;
+
+public interface PostRepository {
+    UUID save(Post post);
+
+}
