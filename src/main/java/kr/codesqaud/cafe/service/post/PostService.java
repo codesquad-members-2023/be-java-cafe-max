01779b33157170121dc3fbff2782feb46ffee82e
chacha <peony@bluehole.net>
2023-04-02 10:55:44 +0900

    feat service에 findById 기능 추가

--- src/main/java/kr/codesqaud/cafe/service/post/PostService.java
+++ src/main/java/kr/codesqaud/cafe/service/post/PostService.java
@@ -30,4 +30,17 @@ public class PostService {
         return postRepository.save(postWriteRequest.toEntity(postId));
     }
 
+    public PostResponseDto findById(UUID postId) {
+        Post post = postRepository.findById(postId).orElseThrow(() -> new NoSuchElementException("해당 id를 가진 post가 없습니다."));
+        return PostResponseDto.of(post, writerInfo(post.getWriteId()));
+    }
+
+    public MemberResponseDto writerInfo(String writerId) {
+        if (writerId == null) {
+            return null;
+        }
+        Optional<Member> optionalMember = memberRepository.findById(writerId);
+        Member member = optionalMember.orElseThrow(() -> new NoSuchElementException("해당 id를 가진 회원이 없습니다."));
+        return MemberResponseDto.of(member);
+    }
 }
