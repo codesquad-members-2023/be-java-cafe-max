5248986a8506a628505dcb3a0f046fa25c327868
Sully <2dcoder@naver.com>
2023-04-02 16:37:33 +0900

    delete: Article 클래스의 불필요한 세터 삭제

--- src/main/java/kr/codesqaud/cafe/domain/Article.java
+++ src/main/java/kr/codesqaud/cafe/domain/Article.java
@@ -27,23 +27,11 @@ public class Article {
         this.articleIndex = articleIndex;
     }
 
-    public void setWriter(String writer) {
-        this.writer = writer;
-    }
-
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getContents() {
         return contents;
-    }
-
-    public void setContents(String contents) {
-        this.contents = contents;
     }
 }
