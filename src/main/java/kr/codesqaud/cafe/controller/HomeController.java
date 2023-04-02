3482bf5b83c0acc5af5452690751a034e38aca05
crtEvent <crtevn@gmail.com>
2023-04-03 00:43:07 +0900

    [PR-01-개선-05] HomeController - 시작 페이지의 trailing slash 제거

--- src/main/java/kr/codesqaud/cafe/controller/HomeController.java
+++ src/main/java/kr/codesqaud/cafe/controller/HomeController.java
@@ -1,10 +1,12 @@
 package kr.codesqaud.cafe.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping
     public String home(){
         return "index";
     }
