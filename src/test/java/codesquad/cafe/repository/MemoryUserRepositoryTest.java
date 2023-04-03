fff6ed407ae587091b425c263972b1753df207a3
chung <chunghye1998>
2023-04-03 17:30:19 +0900

    test: Repository 사용자 목록 조회 테스트

--- src/test/java/codesquad/cafe/repository/MemoryUserRepositoryTest.java
+++ src/test/java/codesquad/cafe/repository/MemoryUserRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
@@ -43,11 +44,18 @@ class MemoryUserRepositoryTest {
     }
 
     @Test
+    @DisplayName("사용자 목록 조회 테스트")
     void findAll() {
         // given
+        User user1 = new User("sio", "1234", "sio","sio@gmail.com");
+        User user2 = new User("시오", "1234", "시오","siooo@gmail.com");
+        memoryUserRepository.save(user1);
+        memoryUserRepository.save(user2);
 
         // when
+        List<User> users = memoryUserRepository.findAll();
 
         // then
+        assertThat(users.size()).isEqualTo(2);
     }
 }
