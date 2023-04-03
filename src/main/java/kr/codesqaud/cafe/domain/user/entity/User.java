2eb1eef981807ebb9973f1e1a03726df692ea248
JJONSOO <pjs970616@naver.com>
2023-04-03 11:24:24 +0900

    [fix] id의 타입을 Integer 가 아닌 Long 으로 수정

--- src/main/java/kr/codesqaud/cafe/domain/user/entity/User.java
+++ src/main/java/kr/codesqaud/cafe/domain/user/entity/User.java
@@ -8,7 +8,7 @@ public class User {
 	private String password;
 	private String name;
 	private String email;
-	private Integer id;
+	private Long id;
 	String dateTime;
 
 	public User(String name, String password, String email) {
@@ -26,11 +26,11 @@ public class User {
 		this.name = name;
 	}
 
-	public Integer getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
