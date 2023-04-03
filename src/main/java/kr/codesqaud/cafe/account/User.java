e9306ff0584245b321e59e76325abec7bd2f4ade
ALbertIM0427 <guang7226961@naver.com>
2023-04-03 15:16:32 +0900

    feat: User에 Builder 추가

--- src/main/java/kr/codesqaud/cafe/account/User.java
+++ src/main/java/kr/codesqaud/cafe/account/User.java
@@ -64,4 +64,42 @@ public class User {
 		profileSettingForm.setNickname(getNickname());
 		return profileSettingForm;
 	}
+
+	private User(Builder builder) {
+		this.id = builder.id;
+		this.email = builder.email;
+		this.password = builder.password;
+		this.nickname = builder.nickname;
+	}
+
+	public static class Builder {
+		private Long id;
+		private String nickname;
+		private String email;
+		private String password;
+
+		public Builder(Long id) {
+			this.id = id;
+		}
+
+		public Builder nickname(String nickname) {
+			this.nickname = nickname;
+			return this;
+		}
+
+		public Builder email(String email) {
+			this.email = email;
+			return this;
+		}
+
+		public Builder password(String password) {
+			this.password = password;
+			return this;
+		}
+
+		public User build() {
+			return new User(this);
+		}
+	}
+
 }
