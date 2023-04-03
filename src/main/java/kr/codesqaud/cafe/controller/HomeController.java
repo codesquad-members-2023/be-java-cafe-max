af4fbf4ff06d6a152dff5986861532ab4d791f75
sudago <sudago22@gmail.com>
2023-04-03 17:12:55 +0900

    style: @GetMapping("/") 제거
    url이 "/" 일 때 스프링 부트는 기본으로 index를 찾아준다.
    그러므로 불필요한 메소드 제거.

--- src/main/java/kr/codesqaud/cafe/controller/HomeController.java
+++ src/main/java/kr/codesqaud/cafe/controller/HomeController.java
@@ -5,11 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
-    public String home(){
-        return "/index";
-    }
-
     @GetMapping("/index")
     public String index(){
         return "/index";
