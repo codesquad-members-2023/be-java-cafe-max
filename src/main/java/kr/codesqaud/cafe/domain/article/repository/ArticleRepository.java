d53a6625072c812e41384b927966dcc753ed8fe8
JJONSOO <pjs970616@naver.com>
2023-04-03 11:11:34 +0900

    [fix] findById 메서드가 게시글이 존재하지 않을 때에도 수용할 수 있는  Optional 로 변경

--- src/main/java/kr/codesqaud/cafe/domain/article/repository/ArticleRepository.java
+++ src/main/java/kr/codesqaud/cafe/domain/article/repository/ArticleRepository.java
@@ -1,6 +1,7 @@
 package kr.codesqaud.cafe.domain.article.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import kr.codesqaud.cafe.domain.article.entity.Article;
 
@@ -8,7 +9,7 @@ public interface ArticleRepository {
 
 	void save(Article article);
 
-	Article findById(Integer id);
+	Optional<Article> findById(Integer id);
 
 	List<Article> findAll();
 
