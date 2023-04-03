847bb077ca56bf4f0adcf3d5ce4f16f94c6f82e4
yeonise <cerealnum@gmail.com>
2023-04-03 22:15:25 +0900

    feat : ArticleRepository에 게시글의 존재 여부를 반환하는 메서드 추가
    - 불필요한 메서드도 제거합니다.

--- src/main/java/kr/codesqaud/cafe/repository/ArticleRepository.java
+++ src/main/java/kr/codesqaud/cafe/repository/ArticleRepository.java
@@ -8,6 +8,5 @@ public interface ArticleRepository {
     Article save(Article article);
     Article findById(Long id);
     List<Article> findAll();
-    void update(Long id, Article article);
-    void delete(Long id);
+    boolean isExists(Long id);
 }
