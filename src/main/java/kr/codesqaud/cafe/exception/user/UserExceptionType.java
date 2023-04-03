a1701a524d395713cd960503f7d3cf91e5e76443
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-04-03 11:13:34 +0900

    fix: 기존 분산되어 있는 enum들을 UserExceptionType enum으로 통합
    * 같은 BaseExceptionType 인터페이스를 구현하는 다음 ExceptionType을 UserExceptionType enum으로 통합하였습니다.
    - UserDuplicatedExceptionType
    - UserValidFormatExceptionType
    - UserNotFoundExceptionType

--- src/main/java/kr/codesqaud/cafe/exception/user/UserExceptionType.java
+++ src/main/java/kr/codesqaud/cafe/exception/user/UserExceptionType.java
@@ -3,15 +3,17 @@ package kr.codesqaud.cafe.exception.user;
 import kr.codesqaud.cafe.exception.BaseExceptionType;
 import org.springframework.http.HttpStatus;
 
-public enum UserDuplicatedExceptionType implements BaseExceptionType {
-    // 회원가입 또는 로그인시 발생할 수 있는 예외
+public enum UserExceptionType implements BaseExceptionType {
     ALREADY_EXIST_USERID(600, HttpStatus.OK, "이미 존재하는 아이디입니다."),
-    ALREADY_EXIST_EMAIL(601, HttpStatus.OK, "이미 존재하는 이메일입니다.");
+    ALREADY_EXIST_EMAIL(601, HttpStatus.OK, "이미 존재하는 이메일입니다."),
+    INVALID_USER_FORMAT(700, HttpStatus.OK, "올바르지 않은 입력 형식입니다."),
+    NOT_FOUND_USER(800, HttpStatus.OK, "회원을 찾을 수 없습니다.");
+
     private final int errorCode;
     private final HttpStatus httpStatus;
     private final String errorMessage;
 
-    UserDuplicatedExceptionType(int errorCode, HttpStatus httpStatus, String errorMessage) {
+    UserExceptionType(int errorCode, HttpStatus httpStatus, String errorMessage) {
         this.errorCode = errorCode;
         this.httpStatus = httpStatus;
         this.errorMessage = errorMessage;
