6ac563bc0bbaa1b03f904a91fe51abc2a9f1fa28
JJONSOO <pjs970616@naver.com>
2023-04-03 12:07:15 +0900

    [fix] dateTime 타입을 LocalDateTime으로 수정

--- src/main/java/kr/codesqaud/cafe/domain/article/entity/Article.java
+++ src/main/java/kr/codesqaud/cafe/domain/article/entity/Article.java
@@ -1,23 +1,17 @@
 package kr.codesqaud.cafe.domain.article.entity;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 public class Article {
 	Long id;
 	String content;
 	String title;
-	String dateTime;
+	LocalDateTime dateTime;
 
 	public Article(String title, String content) {
 		this.title = title;
 		this.content = content;
-		this.dateTime = setDateTime();
-	}
-
-	public String setDateTime() {
-		LocalDateTime now = LocalDateTime.now();
-		return now.format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일(E) HH:mm:ss"));
+		this.dateTime = LocalDateTime.now();
 	}
 
 	public String getContent() {
@@ -42,6 +36,10 @@ public class Article {
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public LocalDateTime getDateTime() {
+		return dateTime;
 	}
 }
 
