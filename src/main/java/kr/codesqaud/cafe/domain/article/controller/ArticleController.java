78cda73f1a5f781ecb94b5247017a20b890a3a40
JJONSOO <pjs970616@naver.com>
2023-04-03 12:08:04 +0900

    [fix] View layer에 Entity를 반환하지 않고 ResponseDto를 반환하도록 수정

--- src/main/java/kr/codesqaud/cafe/domain/article/controller/ArticleController.java
+++ src/main/java/kr/codesqaud/cafe/domain/article/controller/ArticleController.java
@@ -1,5 +1,7 @@
 package kr.codesqaud.cafe.domain.article.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 import org.springframework.stereotype.Controller;
@@ -8,7 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import kr.codesqaud.cafe.domain.article.dto.ArticleSaveRequestDto;
+import kr.codesqaud.cafe.domain.article.dto.request.ArticleSaveRequestDto;
+import kr.codesqaud.cafe.domain.article.dto.response.ArticleDetailResponseDto;
+import kr.codesqaud.cafe.domain.article.entity.Article;
 import kr.codesqaud.cafe.domain.article.repository.ArticleRepository;
 
 @Controller
@@ -22,7 +26,10 @@ public class ArticleController {
 
 	@GetMapping("/")
 	public String main(Model model) {
-		model.addAttribute("articles", articleRepository.findAll());
+		List<Article> articles = articleRepository.findAll();
+		List<ArticleDetailResponseDto> articleDetailResponseDtos = new ArrayList<>();
+		articles.forEach(article -> articleDetailResponseDtos.add(new ArticleDetailResponseDto(article)));
+		model.addAttribute("articles", articleDetailResponseDtos);
 		return "index";
 	}
 
@@ -34,7 +41,8 @@ public class ArticleController {
 
 	@GetMapping("/questions/{id}")
 	public String viewArticle(@PathVariable("id") Long id, Model model) {
-		model.addAttribute("article", articleRepository.findById(id).orElseThrow(NoSuchElementException::new));
+		Article article = articleRepository.findById(id).orElseThrow(NoSuchElementException::new);
+		model.addAttribute("article", new ArticleDetailResponseDto(article));
 		return "/post/show";
 	}
 }
