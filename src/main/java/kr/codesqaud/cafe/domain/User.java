bef8f863aee3f80b821cdd06e8a05ea97008f677
pinetree <eodnjs568@gmail.com>
2023-04-03 15:04:48 +0900

    refactor "불필요 주석 삭제"

--- src/main/java/kr/codesqaud/cafe/domain/User.java
+++ src/main/java/kr/codesqaud/cafe/domain/User.java
@@ -12,27 +12,6 @@ public class User {
         this.name = name;
         this.email = email;
     }
-
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    public void setId(String id) {
-//        this.id = id;
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//
     public String getId() {
         return id;
     }
@@ -40,7 +19,6 @@ public class User {
     public String getEmail() {
         return email;
     }
-//
     public String getName() {
         return name;
     }
