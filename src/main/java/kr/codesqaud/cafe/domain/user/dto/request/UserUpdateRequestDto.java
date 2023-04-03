c475922cd4ede4920d029e54813c5bcb6ef39272
JJONSOO <pjs970616@naver.com>
2023-04-03 12:12:01 +0900

    [fix] request package로 이동

--- src/main/java/kr/codesqaud/cafe/domain/user/dto/request/UserUpdateRequestDto.java
+++ src/main/java/kr/codesqaud/cafe/domain/user/dto/request/UserUpdateRequestDto.java
@@ -1,14 +1,14 @@
-package kr.codesqaud.cafe.domain.user.dto;
+package kr.codesqaud.cafe.domain.user.dto.request;
 
 import kr.codesqaud.cafe.domain.user.entity.User;
 
-public class UserUpdateDto {
+public class UserUpdateRequestDto {
 
 	private String name;
 	private String email;
 	private String password;
 
-	public UserUpdateDto(String name, String email, String password) {
+	public UserUpdateRequestDto(String name, String email, String password) {
 		this.name = name;
 		this.email = email;
 		this.password = password;
