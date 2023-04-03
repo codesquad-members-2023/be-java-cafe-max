84b9906355e0916d6df9efd899b97cdcad40d196
chung <chunghye1998>
2023-04-03 18:12:38 +0900

    test: Service 사용자 회원가입 시 목록에 정상적으로 들어가는지 테스트

--- src/test/java/codesquad/cafe/service/UserServiceTest.java
+++ src/test/java/codesquad/cafe/service/UserServiceTest.java
@@ -2,10 +2,14 @@ package codesquad.cafe.service;
 
 import codesquad.cafe.domain.User;
 import codesquad.cafe.dto.UserRequestDto;
+import codesquad.cafe.dto.UserResponseDto;
 import codesquad.cafe.repository.MemoryUserRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -44,12 +48,23 @@ class UserServiceTest {
     }
 
     @Test
+    @DisplayName("사용자 회원가입 시 목록에 정상적으로 들어가는지 테스트")
     void showUsers() {
         // given
+        UserRequestDto userRequestDto1 = new UserRequestDto("sio", "1234", "sio", "sio@gmail.com");
+        UserRequestDto userRequestDto2 = new UserRequestDto("시오", "1111", "시오", "sioooo@gmail.com");
+        userService.join(userRequestDto1);
+        userService.join(userRequestDto2);
+
+        List<UserResponseDto> userResponse = new ArrayList<>();
+        userResponse.add(new UserResponseDto("sio", "sio", "sio@gmail.com"));
+        userResponse.add(new UserResponseDto("시오", "시오", "sioooo@gmail.com"));
 
         // when
+        List<UserResponseDto> userResponseDtos = userService.showUsers();
 
         // then
+        assertThat(userResponseDtos.size()).isEqualTo(userResponse.size());
     }
 
     @Test
