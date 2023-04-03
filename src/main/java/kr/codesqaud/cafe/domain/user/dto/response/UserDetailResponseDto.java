829925830b420711d3b0be44022edc58b15a946a
JJONSOO <pjs970616@naver.com>
2023-04-03 12:13:17 +0900

    [feat] User의 자세한 내용을 담고있는 ResponseDto 클래스 구현

---
+++ src/main/java/kr/codesqaud/cafe/domain/user/dto/response/UserDetailResponseDto.java
@@ -0,0 +1,41 @@
+package kr.codesqaud.cafe.domain.user.dto.response;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import kr.codesqaud.cafe.domain.user.entity.User;
+
+public class UserDetailResponseDto {
+
+	private String name;
+	private String email;
+	private String id;
+	private String dateTime;
+
+	public UserDetailResponseDto(User user) {
+		this.name = user.getName();
+		this.email = user.getEmail();
+		this.id = String.valueOf(user.getId());
+		this.dateTime = setDateTime(user.getDateTime());
+	}
+
+	private String setDateTime(LocalDateTime localDateTime) {
+		return localDateTime.format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일(E) HH:mm:ss"));
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getDateTime() {
+		return dateTime;
+	}
+}
