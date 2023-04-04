40b335332d0933a50494ed497e1fb95ac841dbe3
sudago <sudago22@gmail.com>
2023-04-04 18:53:03 +0900

    feat: 글쓰기 기능 구현

---
+++ src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
@@ -0,0 +1,38 @@
+package kr.codesqaud.cafe.controller;
+
+import kr.codesqaud.cafe.domain.Article;
+import kr.codesqaud.cafe.service.ArticleService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Controller
+public class ArticleController {
+    private final ArticleService articleService;
+    public ArticleController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+    @GetMapping("/questions")
+    public String createForm(){
+        return "qna/form";
+    }
+
+    @PostMapping("/questions")
+    public String create(ArticleForm form){
+        Article article = new Article();
+        article.setWriter(form.getWriter());
+        article.setTitle(form.getTitle());
+        article.setContents(form.getContents());
+        article.setCreatedAt(LocalDateTime.now());
+        article.setPoints(1L);
+
+        articleService.post(article);
+        return "redirect:/";
+    }
+
+}
