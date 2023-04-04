26d42113005b9caaca51251d8b00fffcf49f3a51
sudago <sudago22@gmail.com>
2023-04-04 16:51:32 +0900

    test: User 불변 리스트 추가, 변경, 삭제 시 예외 테스트 작성

--- src/test/java/kr/codesqaud/cafe/repository/UserRepositoryTest.java
+++ src/test/java/kr/codesqaud/cafe/repository/UserRepositoryTest.java
@@ -2,11 +2,13 @@ package kr.codesqaud.cafe.repository;
 
 import kr.codesqaud.cafe.domain.User;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UserRepositoryTest {
     UserRepository repository = new MemoryUserRepository();
@@ -71,12 +73,35 @@ public class UserRepositoryTest {
         repository.save(user1);
 
         User user2 = new User();
-        user2.setName("springName1");
+        user2.setName("springName2");
         user2.setUserId("springId2");
         repository.save(user2);
 
         List<User> result = repository.findAll();
 
         assertThat(result.size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("예외 테스트: 불변 User 리스트에는 User가 추가, 변경, 삭제 되지 않는다.")
+    public void findAllAddTest(){
+        // given
+        User user1 = new User();
+        user1.setName("springName1");
+        user1.setUserId("springId1");
+        repository.save(user1);
+
+        List<User> result = repository.findAll();
+
+        // when
+        User user2 = new User();
+        user2.setName("springName2");
+        user2.setUserId("springId2");
+
+        // then
+        assertThrows(UnsupportedOperationException.class, () -> result.add(user2));
+        assertThrows(UnsupportedOperationException.class, () -> result.set(0, user2));
+        assertThrows(UnsupportedOperationException.class, () -> result.remove(0));
+        assertThrows(UnsupportedOperationException.class, result::clear);
     }
 }
