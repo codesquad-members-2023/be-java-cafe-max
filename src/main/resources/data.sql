8f8d9ac5bcf4aefe22e70722ef9352ef6719f575
ALbertIM0427 <guang7226961@naver.com>
2023-04-04 14:01:55 +0900

    feat: schema.sql 및 data.sql 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into users (nickname, email, password)
+values ('jack', 'jack@mail.com', '123456789a');
+
+insert into post (user_id, nickname, title, text_content,createDataTime)
+values (1, 'spring', 'spring study', 'Spring is comming!!','2023-04-04 01.58.59');
+
