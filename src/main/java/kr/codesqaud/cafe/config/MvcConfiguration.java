365f1d5690aad55481313c6697ce42e8fe641dc6
pinetree <eodnjs568@gmail.com>
2023-04-04 15:14:46 +0900

    feat "WebMVCConfigurer 적용"
    Lucas 조건에 따라 WebMVCConfiguer을 적용하였습니다.
    아직 해당 개념에 대해서 학습이 부족한 관계로 문제 조건과 같이 구현은 했으나
    추후 공부로 보강해보도록 하겠습니다.

--- src/main/java/kr/codesqaud/cafe/config/MvcConfiguration.java
+++ src/main/java/kr/codesqaud/cafe/config/MvcConfiguration.java
@@ -1,8 +1,10 @@
 package kr.codesqaud.cafe.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.concurrent.TimeUnit;
@@ -10,11 +12,11 @@ import java.util.concurrent.TimeUnit;
 @Configuration
 public class MvcConfiguration implements WebMvcConfigurer {
     @Override
-    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
-                .addResourceLocations("classPath:/templates/", "classPath:/static/", "classPath:/resources/")
-                .setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES));
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+        registry.addViewController("/users/form").setViewName("user/form");
+        registry.addViewController("/users/login").setViewName("user/login");
+        registry.addViewController("questions/form").setViewName("qna/form");
     }
-
-
 }
