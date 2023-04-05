9f1ded745484ccc4ce8a80dcd624522f4d174cbd
yeonise <cerealnum@gmail.com>
2023-04-05 11:59:21 +0900

    feat : h2 데이터베이스 연동
    - application.properties를 YAML 파일로 변경합니다.
    - h2 database / jdbc 의존성을 추가합니다.
    - 프로그램 실행 시 schema의 sql문을 수행합니다.

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS Users (
+  userId    VARCHAR(50) PRIMARY KEY,
+  password  VARCHAR(50) NOT NULL,
+  name      VARCHAR(50) NOT NULL,
+  email     VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Articles (
+  id        BIGINT AUTO_INCREMENT PRIMARY KEY,
+  title     VARCHAR(50) NOT NULL,
+  writer    VARCHAR(50) NOT NULL,
+  contents  TEXT NOT NULL,
+  createdAt DATETIME NOT NULL
+);
