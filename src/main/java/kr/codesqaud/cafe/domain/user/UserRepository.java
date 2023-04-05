3858d537b068a1716d8d289946378ec935840c37
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-04-05 16:06:26 +0900

    feat: 회원 정보 수정, 로그인, 로그아웃, 회원 비밀번호 확인 기능 추가
    * 회원 url 설계에 따른 회원 등록번호(id) 멤버를 추가하였습니다.
    * 회원 저장소의 키(key)값이 회원 아이디(userId)에서 회원 등록번호(id)로 변경되었습니다.
    * 여러 스레드가 deleteAll() 또는 nextId() 호출시 쓰레드 세이프하기 위해서 synchronized 키워드를 추가하였습니다.
    * UserService에 구현된 메소드 이름들이 좀더 비즈니스 로직에 맞는 이름으로 변경되었습니다.
    * UserService에 기존 존재하던 검증 기능들을 UserValidator라는 빈으로 분리하였습니다.

--- src/main/java/kr/codesqaud/cafe/domain/user/UserRepository.java
+++ src/main/java/kr/codesqaud/cafe/domain/user/UserRepository.java
@@ -1,24 +1,31 @@
 package kr.codesqaud.cafe.domain.user;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserRepository {
 
-    // key: 사용자 아이디(userId), value: User 객체
-    private final Map<String, User> store = new HashMap<>();
+    // key: 사용자 등록번호(Long), value: User 객체
+    private final Map<Long, User> store = new ConcurrentHashMap<>();
+    private static long sequence = 0;
 
     public List<User> findAll() {
         return new ArrayList<>(store.values());
     }
 
+    public Optional<User> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
     public Optional<User> findByUserId(String userId) {
-        return Optional.ofNullable(store.get(userId));
+        return store.values().stream()
+            .filter(user -> user.getUserId().equals(userId))
+            .findFirst();
     }
 
     public Optional<User> findByEmail(String email) {
@@ -28,13 +35,18 @@ public class UserRepository {
     }
 
     public User save(User user) {
-        store.put(user.getUserId(), user);
+        store.put(user.getId(), user);
         return user;
     }
 
-    public int deleteAll() {
+    public synchronized int deleteAll() {
         int deleteUserCount = store.keySet().size();
         store.clear();
+        sequence = 0;
         return deleteUserCount;
+    }
+
+    public synchronized Long nextId() {
+        return ++sequence;
     }
 }
