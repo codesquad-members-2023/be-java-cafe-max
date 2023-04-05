fd7e36f4cc9494e3bc8ac40862dec14f8bb8cd4c
crtEvent <crtevn@gmail.com>
2023-04-06 08:49:13 +0900

    [2단계-FIX] Q&A 게시판 페이징 처리시 페이지에 비정상 값이 입력된 경우 처리 및 테스트 추가

--- src/main/java/kr/codesqaud/cafe/domain/PageHandler.java
+++ src/main/java/kr/codesqaud/cafe/domain/PageHandler.java
@@ -5,30 +5,28 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class PageHandler {
-	private int totalPostsCount; // 총 개시글 수
-	private int pageSize = 15; // 한 페이지에 보여줄 게시글 수
-	private int pagingBarSize = 5; // 페이징 바에 보여줄 페이지 수
-	private final int firstPage = 1; // 1 페이지
-	private int lastPage; // 마지막 페이지
-	private int currentPage; // 현재 페이지
-	private int beginPageInPagingBar; // 현재 페이징 바의 첫 번째 페이지
-	private int endPageInPagingBar; // 현재 페이징 바의 마지막 페이지
-	private int postOffset; // 현재 페이지의 첫 번째 게시글의 변위
-	private boolean previousBlock; // 페이징 바에서 이전 블록이 존재하는가?
-	private boolean nextBlock; // 페이징 바에서 다음 블록이 존재하는가?
-	private int previousPage; // 이전 블록으로 넘어갈 때 페이지 번호
-	private int nextPage; // 다음 블록으로 넘어갈 때 페이지 번호
-	private List<Integer> pagingBar; // 페이징 바
+	private final int totalPostsCount; // 총 개시글 수
+	private static final int PAGE_SIZE = 15; // 한 페이지에 보여줄 게시글 수
+	private static final int PAGING_BAR_SIZE = 5; // 페이징 바에 보여줄 페이지 수
+	private static final int FIRST_PAGE = 1; // 1 페이지
+	private final int lastPage; // 마지막 페이지
+	private final int currentPage; // 현재 페이지
+	private final int beginPageInPagingBar; // 현재 페이징 바의 첫 번째 페이지
+	private final int endPageInPagingBar; // 현재 페이징 바의 마지막 페이지
+	private final int postOffset; // 현재 페이지의 첫 번째 게시글의 변위
+	private final boolean previousBlock; // 페이징 바에서 이전 블록이 존재하는가?
+	private final boolean nextBlock; // 페이징 바에서 다음 블록이 존재하는가?
+	private final int previousPage; // 이전 블록으로 넘어갈 때 페이지 번호
+	private final int nextPage; // 다음 블록으로 넘어갈 때 페이지 번호
+	private final List<Integer> pagingBar; // 페이징 바
 
 	public PageHandler(int totalPostsCount, int currentPage) {
 		this.totalPostsCount = totalPostsCount;
-		this.currentPage = currentPage;
 		this.lastPage = (getTotalPostsCount() - 1) / getPageSize() + 1;
+		this.currentPage = (currentPage > 0) ? (Math.min(currentPage, getLastPage())) : 1;
 		this.postOffset = (getCurrentPage() - 1) * getPageSize();
 		this.beginPageInPagingBar = ((getCurrentPage() - 1) / getPagingBarSize() * getPagingBarSize()) + 1;
-		this.endPageInPagingBar = getBeginPageInPagingBar() + getPagingBarSize() - 1;
-		if (getEndPageInPagingBar() > getLastPage())
-			this.endPageInPagingBar = getLastPage();
+		this.endPageInPagingBar = Math.min(getBeginPageInPagingBar() + getPagingBarSize() - 1, getLastPage());
 		this.previousBlock = getCurrentPage() > getPagingBarSize();
 		this.nextBlock = getCurrentPage() <= getLastPage() - getPagingBarSize() + 1;
 		this.previousPage = (getCurrentPage() - getPagingBarSize());
@@ -43,15 +41,15 @@ public class PageHandler {
 	}
 
 	public int getPageSize() {
-		return pageSize;
+		return PAGE_SIZE;
 	}
 
 	public int getPagingBarSize() {
-		return pagingBarSize;
+		return PAGING_BAR_SIZE;
 	}
 
 	public int getFirstPage() {
-		return firstPage;
+		return FIRST_PAGE;
 	}
 
 	public int getLastPage() {
---
+++ src/test/java/kr/codesqaud/cafe/domain/PageHandlerTest.java
@@ -0,0 +1,106 @@
+package kr.codesqaud.cafe.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class PageHandlerTest {
+
+	private PageHandler handler;
+	private int totalPostCount;
+	private int currentPage;
+
+	/**
+	 * 이 테스트는 아래의 두 조건을 만족해야 정상적으로 동작합니다
+	 * 1. PageHandler의 int pageSize == 15일때 (한 페이지에 보여줄 게시글 수 == 15)
+	 * 2. PageHandler의 int pagingBarSize == 5일때 (페이징 바에 보여줄 페이지 수 == 5)
+	 */
+	@Nested
+	@DisplayName("전체 게시글수 가 500개 일 때")
+	class PagingTest {
+
+		@BeforeEach
+		void initialTest() {
+			totalPostCount = 500;
+			currentPage = 1;
+		}
+
+		@Test
+		@DisplayName("마지막 페이지는 34")
+		void testLastPage() {
+			handler = new PageHandler(totalPostCount, currentPage);
+
+			assertThat(handler.getLastPage()).isEqualTo(34);
+		}
+
+		@Test
+		@DisplayName("페이지에 음수값이 들어오면 페이지를 1로 변경")
+		void testNegativePage() {
+			currentPage = -1;
+			handler = new PageHandler(totalPostCount, currentPage);
+
+			assertThat(handler.getCurrentPage()).isEqualTo(1);
+		}
+
+		@Test
+		@DisplayName("페이지에 마지막 페이지보다 큰 수가 들어오면 페이지를 마지막 페이지로 변경")
+		void testOverflowPage() {
+			currentPage = Integer.MAX_VALUE;
+			handler = new PageHandler(totalPostCount, currentPage);
+
+			assertThat(handler.getCurrentPage()).isEqualTo(handler.getLastPage());
+		}
+
+		@Test
+		@DisplayName("각 블록의 첫 페이지는 1, 6, 11, 16, 21, 26, 31")
+		void testBeginPageInPagingBar() {
+			int[] beginPageBundle = {1, 6, 11, 16, 21, 26, 31};
+
+			SoftAssertions softAssertions = new SoftAssertions();
+			for (int beginPage : beginPageBundle) {
+				handler = new PageHandler(totalPostCount, currentPage);
+				softAssertions.assertThat(handler.getBeginPageInPagingBar())
+					.isEqualTo(beginPage);
+				currentPage += 5;
+			}
+			softAssertions.assertAll();
+		}
+
+		@Test
+		@DisplayName("각 블록의 마지막 페이지는 5, 10, 15, 20, 25, 30, 34")
+		void testEndPageInPagingBar() {
+			int[] endPageBundle = {5, 10, 15, 20, 25, 30, 34};
+
+			SoftAssertions softAssertions = new SoftAssertions();
+			for (int endPage : endPageBundle) {
+				handler = new PageHandler(totalPostCount, currentPage);
+				softAssertions.assertThat(handler.getEndPageInPagingBar())
+					.isEqualTo(endPage);
+				currentPage += 5;
+			}
+			softAssertions.assertAll();
+		}
+
+		@Test
+		@DisplayName("첫 번째 블록에선 이전 블록은 존재하지 않는다.")
+		void testPreviousBlock() {
+			handler = new PageHandler(totalPostCount, currentPage);
+
+			assertThat(handler.isPreviousBlock()).isFalse();
+		}
+
+		@Test
+		@DisplayName("마지막 블록에선 다음 블록은 존재하지 않는다.")
+		void testNextBlock() {
+			currentPage = 34;
+			handler = new PageHandler(totalPostCount, currentPage);
+
+			assertThat(handler.isNextBlock()).isFalse();
+		}
+	}
+
+}
