d9f550619d346a85a3821dfa774c8014a7f8d3d8
Sully <2dcoder@naver.com>
2023-04-05 15:34:43 +0900

    feat: 회원 수정을 위한 dto 생성
    userId 필드를 뺀 상태로 만들었습니다.
    이제야 dto에 대한 감이 잡히는 것 같습니다.

---
+++ src/main/java/kr/codesqaud/cafe/controller/dto/UserUpdateForm.java
@@ -0,0 +1,45 @@
+package kr.codesqaud.cafe.controller.dto;
+
+import javax.validation.constraints.NotBlank;
+
+public class UserUpdateForm {
+    private Long id;
+    @NotBlank(message = "이름을 입력해주세요.")
+    private String name;
+    @NotBlank(message = "패스워드를 입력해주세요.")
+    private String password;
+    @NotBlank(message = "이메일을 입력해주세요.")
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
