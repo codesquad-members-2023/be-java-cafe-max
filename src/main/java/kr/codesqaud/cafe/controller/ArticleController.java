1df36dbfae2710bdf056b399533e1bd86e0372f4
n-aa-g <wlsdnjs0124@gmail.com>
2023-04-05 17:35:03 +0900

    Feat(ArticleController): showIndex 메서드 생성
    메인페이지를 관리하는 메서드를 만들었습니다.
    글을 쓴 뒤 메인페이지에서 쓴 글의 목록을 볼 수 있습니다.
    또한 index.html이 showIndex가 작동하게 변경했습니다.

--- src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
+++ src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
@@ -1,19 +1,38 @@
 package kr.codesqaud.cafe.controller;
 
+import kr.codesqaud.cafe.domain.Article;
 import kr.codesqaud.cafe.domain.ArticleForm;
 import kr.codesqaud.cafe.service.ArticleService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping
 public class ArticleController {
     ArticleService articleService = new ArticleService();
 
+    @GetMapping
+    public String showIndex(Model model) {
+        List<Article> articles = articleService.findAll();
+        model.addAttribute("articles", articles);
+
+        return "index";
+    }
     @GetMapping("/write")
     public String showWrite (ArticleForm articleForm){
         return "qna/form";
+    }
+
+    @PostMapping("/write")
+    public String write (ArticleForm articleForm) {
+        articleService.saveArticle(articleForm);
+
+        return "redirect:/";
     }
 
 }
