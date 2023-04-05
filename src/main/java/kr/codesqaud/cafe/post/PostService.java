cd46a20f7686b0856227ad7865f0661450cfa70a
ALbertIM0427 <guang7226961@naver.com>
2023-04-06 07:55:17 +0900

    feat: logger 추가

--- src/main/java/kr/codesqaud/cafe/post/PostService.java
+++ src/main/java/kr/codesqaud/cafe/post/PostService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import kr.codesqaud.cafe.exception.ErrorCode;
@@ -15,6 +17,7 @@ import kr.codesqaud.cafe.post.form.SimplePostForm;
 public class PostService {
 
 	private final PostRepository postRepository;
+	private static final Logger logger = LoggerFactory.getLogger(PostService.class);
 
 	public PostService(PostRepository postRepository) {
 		this.postRepository = postRepository;
@@ -36,6 +39,7 @@ public class PostService {
 
 		Optional<Post> optionalPost = postRepository.findById(postId);
 		if (optionalPost.isEmpty()) {
+			logger.info(ErrorCode.INVALID_POST_ID_CODE.getMessage());
 			throw new InvalidPostIdFailedException(ErrorCode.INVALID_POST_ID_CODE);
 		}
 		return optionalPost.get();
