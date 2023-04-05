8faf6cce947629ce812a95cbf24bb3f7c48e9bcd
ALbertIM0427 <guang7226961@naver.com>
2023-04-06 06:17:38 +0900

    refactor: schema.sql User와의 관계를 해제
    연결된 비지로직 없어서 일단 연결을 끊고 나중에 추가 할 예정입니다.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE users
 (
     user_id  INT       NOT NULl AUTO_INCREMENT,
     nickname CHAR(256) NOT NULL,
-    email    CHAR(256) NOT NULL unique ,
+    email    CHAR(256) NOT NULL unique,
     password CHAR(256) NOT NULL,
     PRIMARY KEY (user_id)
 );
@@ -12,11 +12,9 @@ CREATE TABLE users
 CREATE TABLE post
 (
     post_id        INT       NOT NULL AUTO_INCREMENT,
-    user_id        INT       NOT NULL,
     nickname       CHAR(256) NOT NULL,
     title          CHAR(256) NOT NULL,
     text_content   BLOB      NOT NULL,
-    createDataTime DATETIME  NOT NULL,
-    PRIMARY KEY (post_id),
-    FOREIGN KEY (user_id) REFERENCES USERS (user_id)
+    create_DateTime DATETIME  NOT NULL,
+    PRIMARY KEY (post_id)
 )
