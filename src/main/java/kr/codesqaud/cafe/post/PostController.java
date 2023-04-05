431980aa2a167e4ce8b29c09f44a95ba3d5421b3
ALbertIM0427 <guang7226961@naver.com>
2023-04-05 22:30:30 +0900

    refactor: WebConfig 삭제 GetMapping 추가
    Postform으로 값을 받지 못하여 수정

--- src/main/java/kr/codesqaud/cafe/post/PostController.java
+++ src/main/java/kr/codesqaud/cafe/post/PostController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -22,6 +23,10 @@ public class PostController {
 		this.postService = postService;
 	}
 
+	@GetMapping(value = {"/posts/new", "/posts/form"})
+	public String showNewPage(@ModelAttribute PostForm postForm) {
+		return "/post/formInvalid";
+	}
 
 	@PostMapping("/posts")
 	public String addPost(@Valid PostForm postForm, Errors errors, Model model) {
