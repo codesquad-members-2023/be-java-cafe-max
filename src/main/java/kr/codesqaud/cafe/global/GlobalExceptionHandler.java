1ec32871f1703f248932355877700d8113c91d5f
CDBchan <kdjin1215@naver.com>
2023-04-07 00:19:21 +0900

    feat: UserNotFoundException 처리
    UserNotFoundException발생시 error 페이지로 이동하도록 했습니다.

--- src/main/java/kr/codesqaud/cafe/global/GlobalExceptionHandler.java
+++ src/main/java/kr/codesqaud/cafe/global/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package kr.codesqaud.cafe.global;
 
 
 import kr.codesqaud.cafe.exception.IdDuplicatedException;
+import kr.codesqaud.cafe.exception.InvalidPasswordException;
+import kr.codesqaud.cafe.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,11 +12,23 @@ import org.springframework.web.servlet.ModelAndView;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(IdDuplicatedException.class)
-    public ModelAndView handleIdDuplicatedException(IdDuplicatedException e) {
+    private ModelAndView createErrorResponseModelAndView(String viewName, Exception e) {
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage());
-        ModelAndView mav = new ModelAndView("/user/form");
+        ModelAndView mav = new ModelAndView(viewName);
         mav.addObject("error", errorResponse);
         return mav;
     }
+
+    @ExceptionHandler(IdDuplicatedException.class)
+    public ModelAndView handleIdDuplicatedException(IdDuplicatedException e) {
+        return createErrorResponseModelAndView("/user/form", e);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ModelAndView handleUserNotFoundException(UserNotFoundException e) {
+        return createErrorResponseModelAndView("/error/error", e);
+    }
+
+
+
 }
