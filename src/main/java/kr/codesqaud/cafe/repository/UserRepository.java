d58210ea1f6ad0cd685ca7b23e16c8731977dbd9
takealook <takealook97@naver.com>
2023-04-06 13:49:29 +0900

    UserRepository 인터페이스 수정

--- src/main/java/kr/codesqaud/cafe/repository/UserRepository.java
+++ src/main/java/kr/codesqaud/cafe/repository/UserRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 public interface UserRepository {
     User save(User user);
 
-    Optional<User> findById(Long id);
+    Optional<User> findByUserId(String userId);
 
     Optional<User> findByName(String name);
 
