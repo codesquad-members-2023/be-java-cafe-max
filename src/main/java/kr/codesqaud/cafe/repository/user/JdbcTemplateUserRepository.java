aa1e4643de09659fd3a153f29bbffa27276b5352
Sully <2dcoder@naver.com>
2023-04-06 11:10:57 +0900

    refactor(User): JdbcTemplate 사용 클래스를 NamedParameterJdbcTemplate와 SimpleJdbcInsert를 사용하도록 리팩토링

--- src/main/java/kr/codesqaud/cafe/repository/user/JdbcTemplateUserRepository.java
+++ src/main/java/kr/codesqaud/cafe/repository/user/JdbcTemplateUserRepository.java
@@ -1,79 +1,129 @@
 package kr.codesqaud.cafe.repository.user;
 
 import kr.codesqaud.cafe.domain.User;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
-import java.sql.PreparedStatement;
+import javax.sql.DataSource;
 import java.util.List;
-import java.util.Objects;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
 public class JdbcTemplateUserRepository implements UserRepository {
-    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate template;
+    private final SimpleJdbcInsert simpleJdbcInsert;
 
-    public JdbcTemplateUserRepository(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public JdbcTemplateUserRepository(DataSource dataSource) {
+        this.template = new NamedParameterJdbcTemplate(dataSource);
+        this.simpleJdbcInsert = new SimpleJdbcInsert(dataSource)
+                .withTableName("users")
+                .usingGeneratedKeyColumns("id");
     }
 
     @Override
     public void save(User user) {
-        String sql = "insert into users (userId, password, name, email) values (?, ?, ?, ?)";
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-        jdbcTemplate.update(con -> {
-            // 자동 증가 키
-            PreparedStatement ps = con.prepareStatement(sql, new String[]{"id"});
-            ps.setString(1, user.getUserId());
-            ps.setString(2, user.getPassword());
-            ps.setString(3, user.getName());
-            ps.setString(4, user.getEmail());
-            return ps;
-        }, keyHolder);
+        SqlParameterSource param = new BeanPropertySqlParameterSource(user);
+        Number key = simpleJdbcInsert.executeAndReturnKey(param);
+        user.setId(key.longValue());
 
-        Long key = Objects.requireNonNull(keyHolder.getKey()).longValue();
-        user.setId(key);
+//        String sql = "insert into users (userId, password, name, email) values (?, ?, ?, ?)";
+//        KeyHolder keyHolder = new GeneratedKeyHolder();
+//        jdbcTemplate.update(con -> {
+//            // 자동 증가 키
+//            PreparedStatement ps = con.prepareStatement(sql, new String[]{"id"});
+//            ps.setString(1, user.getUserId());
+//            ps.setString(2, user.getPassword());
+//            ps.setString(3, user.getName());
+//            ps.setString(4, user.getEmail());
+//            return ps;
+//        }, keyHolder);
+//
+//        Long key = Objects.requireNonNull(keyHolder.getKey()).longValue();
+//        user.setId(key);
     }
 
     @Override
     public Optional<User> findById(Long id) {
-        List<User> result = jdbcTemplate.query("select * from users where id = ?", userRowMapper(), id);
-        return result.stream().findAny();
+        String sql = "select * from users where id = :id";
+
+        try {
+            Map<String, Object> param = Map.of("id", id);
+            User user = template.queryForObject(sql, param, userRowMapper());
+            assert user != null;
+            return Optional.of(user);
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+
+//        List<User> result = jdbcTemplate.query("select * from users where id = ?", userRowMapper(), id);
+//        return result.stream().findAny();
     }
 
     @Override
     public Optional<User> findByUserId(String userId) {
-        List<User> result = jdbcTemplate.query("select * from users where userId = ?", userRowMapper(), userId);
-        return result.stream().findAny();
+        String sql = "select * from users where userId = :userId";
+
+        try {
+            Map<String, Object> param = Map.of("userId", userId);
+            User user = template.queryForObject(sql, param, userRowMapper());
+            assert user != null;
+            return Optional.of(user);
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+
+//        List<User> result = jdbcTemplate.query("select * from users where userId = ?", userRowMapper(), userId);
+//        return result.stream().findAny();
     }
 
     @Override
     public List<User> findAll() {
-        return jdbcTemplate.query("select * from users", userRowMapper());
+        String sql = "select * from users";
+        return template.query(sql, userRowMapper());
+
+//        return jdbcTemplate.query("select * from users", userRowMapper());
     }
 
     @Override
     public void update(Long id, User updateUser) {
-        String sql = "update users set password=?, name=?, email=? where id=?";
-        jdbcTemplate.update(sql,
-                updateUser.getPassword(),
-                updateUser.getName(),
-                updateUser.getEmail(),
-                id);
+        String sql = "update users " +
+                "set password=:password, name=:name, email=:email " +
+                "where id=:id";
+
+        SqlParameterSource param = new MapSqlParameterSource()
+                .addValue("password", updateUser.getPassword())
+                .addValue("name", updateUser.getName())
+                .addValue("email", updateUser.getEmail())
+                .addValue("id", id);
+
+        template.update(sql, param);
+
+//        String sql = "update users set password=?, name=?, email=? where id=?";
+//        jdbcTemplate.update(sql,
+//                updateUser.getPassword(),
+//                updateUser.getName(),
+//                updateUser.getEmail(),
+//                id);
     }
 
     private RowMapper<User> userRowMapper() {
-        return (rs, rowNum) -> {
-            User user = new User();
-            user.setId(rs.getLong("id"));
-            user.setUserId(rs.getString("userId"));
-            user.setPassword(rs.getString("password"));
-            user.setName(rs.getString("name"));
-            user.setEmail(rs.getString("email"));
-            return user;
-        };
+        return BeanPropertyRowMapper.newInstance(User.class);
+//        return (rs, rowNum) -> {
+//            User user = new User();
+//            user.setId(rs.getLong("id"));
+//            user.setUserId(rs.getString("userId"));
+//            user.setPassword(rs.getString("password"));
+//            user.setName(rs.getString("name"));
+//            user.setEmail(rs.getString("email"));
+//            return user;
+//        };
     }
 }
