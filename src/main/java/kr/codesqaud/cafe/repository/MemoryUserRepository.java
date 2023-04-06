c776544c222c12d3a7f9b7b407cd2bcef15964b2
takealook <takealook97@naver.com>
2023-04-06 13:48:04 +0900

    MemoryUserRepository  메서드 수정
    UserRepository의 변경에 따라 메서드를 수정하였다.

--- src/main/java/kr/codesqaud/cafe/repository/MemoryUserRepository.java
+++ src/main/java/kr/codesqaud/cafe/repository/MemoryUserRepository.java
@@ -11,19 +11,19 @@ import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
 public class MemoryUserRepository implements UserRepository {
-    private final Map<Long, User> userRepository = new ConcurrentHashMap<>();
+    private final Map<String, User> userRepository = new ConcurrentHashMap<>();
     private static long sequence = 0L;
 
     @Override
     public User save(User user) {
         user.setId(++sequence);
-        userRepository.put(user.getId(), user);
+        userRepository.put(user.getUserId(), user);
         return user;
     }
 
     @Override
-    public Optional<User> findById(Long id) {
-        return Optional.ofNullable(userRepository.get(id));
+    public Optional<User> findByUserId(String userId) {
+        return Optional.ofNullable(userRepository.get(userId));
     }
 
     @Override
