107ee14369a4550e58e63a2cee209fee79186ba2
yeonise <cerealnum@gmail.com>
2023-04-06 22:38:14 +0900

    refactor : UserResponse DTO의 password 제거
    - UserResponse DTO가 불필요하게 비밀번호를 담고 있으므로 password를 제거합니다.

--- src/main/java/kr/codesqaud/cafe/dto/user/UserResponse.java
+++ src/main/java/kr/codesqaud/cafe/dto/user/UserResponse.java
@@ -4,23 +4,20 @@ import kr.codesqaud.cafe.domain.User;
 
 public class UserResponse {
     private String userId;
-    private String password;
     private String name;
     private String email;
 
-    public UserResponse(String userId, String password, String name, String email) {
+    public UserResponse(String userId, String name, String email) {
         this.userId = userId;
-        this.password = password;
         this.name = name;
         this.email = email;
     }
 
     public static UserResponse from(User user) { // Entity → DTO
-        return new UserResponse(user.getUserId(), user.getPassword(), user.getName(), user.getEmail());
+        return new UserResponse(user.getUserId(), user.getName(), user.getEmail());
     }
 
     public String getUserId() { return userId; }
-    public String getPassword() { return password; }
     public String getName() { return name; }
     public String getEmail() { return email; }
 }
