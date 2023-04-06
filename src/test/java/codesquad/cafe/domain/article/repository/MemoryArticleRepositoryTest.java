676baed40abeab8329b9fd734b47c944349fc8ad
chung <chunghye1998>
2023-04-07 03:07:12 +0900

    test: Repository 게시글 저장 테스트

--- src/test/java/codesquad/cafe/domain/article/repository/MemoryArticleRepositoryTest.java
+++ src/test/java/codesquad/cafe/domain/article/repository/MemoryArticleRepositoryTest.java
@@ -1,13 +1,29 @@
 package codesquad.cafe.domain.article.repository;
 
+import codesquad.cafe.domain.article.domain.Article;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemoryArticleRepositoryTest {
 
+    ArticleRepository articleRepository = new MemoryArticleRepository();
+
     @Test
+    @DisplayName("게시글 저장 테스트")
     void save() {
+        // given
+        Article article = createDummyArticle1();
+
+        // when
+        articleRepository.save(article);
+
+        // then
+        Assertions.assertThat(articleRepository.findById(1L)).usingRecursiveComparison().isEqualTo(article.createdWith(1L));
     }
 
     @Test
@@ -16,5 +32,9 @@ class MemoryArticleRepositoryTest {
 
     @Test
     void findById() {
+    }
+
+    private Article createDummyArticle1() {
+        return new Article("sio", "title", "contents", LocalDateTime.of(2023, 4, 7, 3, 3));
     }
 }
