7ffc840f73a55e90aa8d903434f6c48e7adb09dd
chung <chunghye1998>
2023-04-07 04:44:31 +0900

    fix: article의 id에 auto increment가 적용되지 않는 문제 해결

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,17 @@
-drop table Article;
 
-create table Article(
+create table article(
     id bigint not null auto_increment,
-    writer varchar(30),
-    title varchar(200),
-    contents varchar(500),
+    writer varchar(200) not null,
+    title varchar(200) not null,
+    contents text not null,
     date datetime,
-    primary key (id)
+    primary key(id)
 );
+
+create table cafe_user(
+    id varchar(200) not null,
+    password varchar(200) not null,
+    name varchar(200) not null,
+    email varchar(200) not null,
+    primary key (id)
+)
