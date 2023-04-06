99b7785776c1123fbab9cc4e4af160165c4984f0
CDBchan <kdjin1215@naver.com>
2023-04-06 22:51:14 +0900

    feat: 입력값에 대한 검증 및 중복 id입력시 회원가입을 제한하는 기능 구현

--- src/main/java/kr/codesqaud/cafe/controller/dto/UserDTO.java
+++ src/main/java/kr/codesqaud/cafe/controller/dto/UserDTO.java
@@ -1,11 +1,26 @@
 package kr.codesqaud.cafe.controller.dto;
 
 import kr.codesqaud.cafe.domain.User;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 public class UserDTO {
+    @NotBlank
+    @Length(min = 2,max = 12)
     private String nickName;
+
+    @NotBlank
+    @Pattern(regexp = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$")
     private String email;
+
+    @NotBlank
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$")
     private String password;
+
+    @NotBlank
+    @Length(min = 2,max = 12)
     private String id;
 
     public UserDTO(String nickName, String email, String password, String id) {
@@ -13,6 +28,10 @@ public class UserDTO {
         this.email = email;
         this.password = password;
         this.id = id;
+    }
+
+    public String getId() {
+        return id;
     }
 
     public String getNickName() {
--- src/main/java/kr/codesqaud/cafe/service/UserService.java
+++ src/main/java/kr/codesqaud/cafe/service/UserService.java
@@ -4,9 +4,11 @@ import kr.codesqaud.cafe.controller.dto.ProfileEditDTO;
 import kr.codesqaud.cafe.controller.dto.UserDTO;
 import kr.codesqaud.cafe.controller.dto.UserListDTO;
 import kr.codesqaud.cafe.domain.User;
+import kr.codesqaud.cafe.exception.IdDuplicatedException;
 import kr.codesqaud.cafe.exception.InvalidPasswordException;
 import kr.codesqaud.cafe.exception.UserNotFoundException;
 import kr.codesqaud.cafe.repository.UserRepository;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,7 +24,17 @@ public class UserService {
     }
 
     public void addUser(UserDTO userDTO){
+        validateId(userDTO.getId());
         userRepository.save(userDTO.toUser());
+    }
+
+    private void validateId(String id){
+        try {
+            if (userRepository.findUserById(id).isPresent()) {
+                throw new IdDuplicatedException();
+            }
+        } catch (EmptyResultDataAccessException e) {
+        }
     }
 
     public List<UserListDTO> getUserList(){
