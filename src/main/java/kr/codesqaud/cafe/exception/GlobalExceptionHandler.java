90fd142f2bbde548d9088cea4b08bce82304fcc9
othertkfka <57559288+othertkfka@users.noreply.github.com>
2023-04-06 20:59:05 +0900

    refactor: 400에러로 발생하도록 변경

--- src/main/java/kr/codesqaud/cafe/exception/GlobalExceptionHandler.java
+++ src/main/java/kr/codesqaud/cafe/exception/GlobalExceptionHandler.java
@@ -1,11 +1,14 @@
 package kr.codesqaud.cafe.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(DuplicateKeyException.class)
     public String duplicateKeyExceptionHandler() {
         return "/user/form_failed";
