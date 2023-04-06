ee828866ab5309300ed4fa555bacc95b1d512714
ALbertIM0427 <guang7226961@naver.com>
2023-04-06 09:53:47 +0900

    refactor: 메서드 분리 및 상수 처리

--- src/main/java/kr/codesqaud/cafe/exception/GlobalExceptionHandler.java
+++ src/main/java/kr/codesqaud/cafe/exception/GlobalExceptionHandler.java
@@ -13,20 +13,25 @@ public class GlobalExceptionHandler {
 
     public static final String ERROR_DETAILS_FORMAT = "[ ErrorStatus : %s ][ErrorCode : %s][ ErrorMessage : %s ][ RequestURL : %s ]";
     private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+    public static final String ERROR_CODE = "errorCode";
 
     @ExceptionHandler(CustomException.class)
     public ModelAndView defaultErrorHandler(HttpServletRequest request, CustomException e) {
         ErrorCode errorCode = e.getErrorCode();
+        loggerError(request, errorCode);
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setStatus(errorCode.getStatus());
+        modelAndView.addObject(ERROR_CODE, errorCode);
+        modelAndView.setViewName(errorCode.getViewName());
+        return modelAndView;
+    }
+
+    private static void loggerError(HttpServletRequest request, ErrorCode errorCode) {
         logger.error(
                 String.format(ERROR_DETAILS_FORMAT,
                         errorCode.getStatus().toString(),
                         errorCode.getCode(),
                         errorCode.getMessage(),
                         request.getRequestURI()));
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setStatus(errorCode.getStatus());
-        modelAndView.addObject("errorCode", errorCode);
-        modelAndView.setViewName(errorCode.getViewName());
-        return modelAndView;
     }
 }
