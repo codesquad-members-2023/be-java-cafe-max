44b7fec5630e701942550c27f63e9c607fe414eb
sudago <sudago22@gmail.com>
2023-04-07 17:29:51 +0900

    feat: h2 DB를 사용해서 User 저장하고 조회하기

---
+++ src/main/java/kr/codesqaud/cafe/repository/JdbcUserRepository.java
@@ -0,0 +1,86 @@
+package kr.codesqaud.cafe.repository;
+
+import kr.codesqaud.cafe.domain.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+/*
+    미션에서 요구한 것은 Articles의 Repository이므로 헷갈리지 말자!
+    이 코드들은 김영한님의 강의를 바탕으로 쓴 코드이니, 이해하고 쓰자!
+ */
+@Repository
+public class JdbcUserRepository implements UserRepository{
+    private final JdbcTemplate jdbcTemplate;
+//    public JdbcUserRepository(JdbcTemplate jdbcTemplate){
+//        this.jdbcTemplate = jdbcTemplate;
+//    }
+    public JdbcUserRepository(DataSource dataSource){
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public User save(User user) {
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+        jdbcInsert.withTableName("users_squad").usingGeneratedKeyColumns("customerId");
+
+        Map<String, Object> parameters = new ConcurrentHashMap<>();
+        parameters.put("userId", user.getUserId());
+        parameters.put("password", user.getPassword());
+        parameters.put("name", user.getName());
+        parameters.put("email", user.getEmail());
+
+        Number key = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(parameters));
+        user.setCustomerId(key.longValue());
+        return user;
+    }
+
+    @Override
+    public Optional<User> findByUserId(String userId) {
+        // 디자인 패턴 중, 템플릿 메서드 패턴의 요소가 많이 반영되어있기 때문이다.
+        List<User> result = jdbcTemplate.query("select * from users_squad where userId = ?", userRowMapper(), userId);
+        return result.stream().findAny();
+    }
+
+    @Override
+    public Optional<User> findByName(String name) {
+        List<User> result = jdbcTemplate.query("select * from users_squad where name = ?", userRowMapper(), name);
+        return result.stream().findAny();
+    }
+
+    @Override
+    public List<User> findAll() {
+        return jdbcTemplate.query("select * from users_squad", userRowMapper());
+    }
+
+    @Override
+    public void clearStore() {
+        jdbcTemplate.update("delete from users_squad");
+    }
+
+    private RowMapper<User> userRowMapper(){
+        return (rs, rowNum) -> {
+            User user = new User();
+            user.setCustomerId(rs.getLong("customerId"));
+            user.setUserId(rs.getString("userId"));
+            user.setPassword(rs.getString("password"));
+            user.setName(rs.getString("name"));
+            user.setEmail(rs.getString("email"));
+            return user;
+        };
+    }
+
+//    public RowMapper<User> userRowMapper() {
+//        return BeanPropertyRowMapper.newInstance(User.class);
+//    }
+
+}
