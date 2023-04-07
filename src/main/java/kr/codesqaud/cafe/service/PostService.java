89e4b9be3554e545c54625bc451ac38f2746cc39
tmdqja0705@naver.com <tmdqja0705@naver.com>
2023-04-07 11:21:41 +0900

    게시글 생성, 단일 혹은 모든 게시글 찾는 로직 구현

---
+++ src/main/java/kr/codesqaud/cafe/service/PostService.java
@@ -0,0 +1,33 @@
+package kr.codesqaud.cafe.service;
+
+import kr.codesqaud.cafe.domain.Post;
+import kr.codesqaud.cafe.repository.MemoryPostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostService {
+
+    private final MemoryPostRepository memoryPostRepository;
+
+    @Autowired
+    public PostService(MemoryPostRepository memoryPostRepository) {
+        this.memoryPostRepository = memoryPostRepository;
+    }
+
+    public Post create(Post post) {
+        memoryPostRepository.save(post);
+        return post;
+    }
+
+    public List<Post> finePosts() {
+        return memoryPostRepository.findAll();
+    }
+
+    public Post findById(long id) {
+        return memoryPostRepository.findById(id).get();
+    }
+}
+
