a3b6220d4c690713e150df664c6ef8b8d9e5fe1a
O_jeegu <lumirite0@gmail.com>
2023-04-07 10:35:57 +0900

    refactor : ArticleRepository NamedParameterJdbcTemplate로 변경
    기존 JdbcTemplate에서 NamedParameterJdbcTemplate로 변경. 이 방법이 유지보수에 용이하고(순서 상관x) 요즘 보편적으로 사용된다고 한다.

--- src/main/java/kr/codesqaud/cafe/repository/JdbcTemplateArticleRepository.java
+++ src/main/java/kr/codesqaud/cafe/repository/JdbcTemplateArticleRepository.java
@@ -1,14 +1,17 @@
 package kr.codesqaud.cafe.repository;
 
 import kr.codesqaud.cafe.domain.Article;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -17,51 +20,98 @@ import java.util.Optional;
 @Repository
 public class JdbcTemplateArticleRepository implements ArticleRepository {
 
-    private final JdbcTemplate jdbcTemplate;
+//    private final JdbcTemplate jdbcTemplate;
+
+//    public JdbcTemplateArticleRepository(DataSource dataSource) {
+//        jdbcTemplate = new JdbcTemplate(dataSource);
+//    }
+
+    private final NamedParameterJdbcTemplate template;
+    private final SimpleJdbcInsert simpleJdbcInsert;
 
     public JdbcTemplateArticleRepository(DataSource dataSource) {
-        jdbcTemplate = new JdbcTemplate(dataSource);
+        this.template = new NamedParameterJdbcTemplate(dataSource);
+        this.simpleJdbcInsert = new SimpleJdbcInsert(dataSource)
+                .withTableName("ARTICLE_TB")
+                .usingGeneratedKeyColumns("ID");
     }
 
     @Override
     public void save(Article article) {
-        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
-        jdbcInsert.withTableName("ARTICLE_TB").usingGeneratedKeyColumns("ID");
-
-        Map<String, Object> articleParameters = new HashMap<>();
-        articleParameters.put("TITLE", article.getTitle());
-        articleParameters.put("CONTENT", article.getContent());
-        articleParameters.put("AUTHOR", article.getAuthor());
-
-        Number key = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(articleParameters));
+        SqlParameterSource param = new BeanPropertySqlParameterSource(article);
+        Number key = simpleJdbcInsert.executeAndReturnKey(param);
         article.setId(key.longValue());
     }
 
+//        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+//        jdbcInsert.withTableName("ARTICLE_TB").usingGeneratedKeyColumns("ID");
+//
+//        Map<String, Object> articleParameters = new HashMap<>();
+//        articleParameters.put("TITLE", article.getTitle());
+//        articleParameters.put("CONTENT", article.getContent());
+//        articleParameters.put("AUTHOR", article.getAuthor());
+//
+//        Number key = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(articleParameters));
+//        article.setId(key.longValue());
+
+
+    //todo : sully 코드 보기
     @Override
     public void update(Article updateArticle) {
-        String sql = "UPDATE ARTICLE_TB SET TITLE = ?, CONTENT = ? WHERE ID = ?";
-        jdbcTemplate.update(sql, updateArticle.getTitle(), updateArticle.getContent(), updateArticle.getId());
+        String sql = "UPDATE ARTICLE_TB SET TITLE = :TITLE, CONTENT = :CONTENT WHERE ID = :ID";
+
+        SqlParameterSource param = new MapSqlParameterSource()
+                .addValue("TITLE", updateArticle.getTitle())
+                .addValue("CONTENT", updateArticle.getContent())
+                .addValue("ID", updateArticle.getId());
+
+        template.update(sql, param);
     }
+
+//        String sql = "UPDATE ARTICLE_TB SET TITLE = ?, CONTENT = ? WHERE ID = ?";
+//        jdbcTemplate.update(sql, updateArticle.getTitle(), updateArticle.getContent(), updateArticle.getId());
 
     @Override
     public List<Article> gatherAll() {
-        return jdbcTemplate.query("SELECT * FROM ARTICLE_TB", articleRowMapper());
+        String sql = "SELECT * FROM ARTICLE_TB";
+        return template.query(sql, articleRowMapper());
     }
+
+//        return jdbcTemplate.query("SELECT * FROM ARTICLE_TB", articleRowMapper());
 
     @Override
     public Optional<Article> findById(long id) {
-        List<Article> wantedPost = jdbcTemplate.query("SELECT * FROM ARTICLE_TB WHERE ID = ?", articleRowMapper(), id);
-        return wantedPost.stream().findAny();
+        String sql = "SELECT * FROM ARTICLE_TB WHERE ID = :ID";
+
+        try {
+            Map<String, Object> param = Map.of("ID", id);
+            Article article = template.queryForObject(sql, param, articleRowMapper());
+            assert article != null;
+            return Optional.of(article);
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
     }
 
+//    List<Article> wantedPost = jdbcTemplate.query("SELECT * FROM ARTICLE_TB WHERE ID = ?", articleRowMapper(), id);
+//        return wantedPost.stream().findAny();
+
     public RowMapper<Article> articleRowMapper() {
-        return (rs, rowNum) -> {
-            Article article = new Article();
-            article.setId(rs.getLong("ID"));
-            article.setTitle(rs.getString("TITLE"));
-            article.setContent(rs.getString("CONTENT"));
-            article.setAuthor(rs.getString("AUTHOR"));
-            return article;
-        };
+        return BeanPropertyRowMapper.newInstance(Article.class);
+        /*
+            Item item = new Item();
+            item.setId(rs.getLong("id"));
+            item.setPrice(rs.getInt("price"))
+         */
+
+
+//        return (rs, rowNum) -> {
+//            Article article = new Article();
+//            article.setId(rs.getLong("ID"));
+//            article.setTitle(rs.getString("TITLE"));
+//            article.setContent(rs.getString("CONTENT"));
+//            article.setAuthor(rs.getString("AUTHOR"));
+//            return article;
+//        };
     }
 }
