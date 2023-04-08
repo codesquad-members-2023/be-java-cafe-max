14b9ace7ee006083c9763fd7f2410476c3bf2585
chacha <peony@bluehole.net>
2023-04-09 01:23:11 +0900

    bugfix test패키지안에 schema.sql 추가

---
+++ src/test/resources/schema.sql
@@ -0,0 +1,23 @@
+
+DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS member;
+
+CREATE TABLE member (
+                        memberId BIGINT AUTO_INCREMENT,
+                        email VARCHAR(100) NOT NULL UNIQUE,
+                        password VARCHAR(100) NOT NULL,
+                        nickName VARCHAR(100),
+                        create_date TIMESTAMP NOT NULL,
+                        PRIMARY KEY (memberId)
+);
+
+CREATE TABLE post (
+                      postId BIGINT NOT NULL AUTO_INCREMENT,
+                      title VARCHAR(255) NOT NULL,
+                      content TEXT NOT NULL,
+                      writerId  BIGINT NOT NULL,
+                      write_date TIMESTAMP NOT NULL,
+                      views BIGINT DEFAULT 0,
+                      PRIMARY KEY (postId),
+                      FOREIGN KEY (writerId) REFERENCES member (memberId) ON DELETE CASCADE ON UPDATE CASCADE
+);
