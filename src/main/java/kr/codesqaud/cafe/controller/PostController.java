2a961429db1051ff637b13567a94ad5a2b4ae832
chacha <peony@bluehole.net>
2023-04-09 01:09:26 +0900

    feat PostController 추가

---
+++ src/main/java/kr/codesqaud/cafe/controller/PostController.java
@@ -0,0 +1,53 @@
+package kr.codesqaud.cafe.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+import kr.codesqaud.cafe.dto.post.PostWriteRequest;
+import kr.codesqaud.cafe.service.PostService;
+
+@Controller
+@RequestMapping("/posts")
+public class PostController {
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping
+    public String posts(Model model) {
+        model.addAttribute("postResponses", postService.findAll());
+        return "post/all";
+    }
+
+    @PostMapping("/write")
+    public String write(@Valid PostWriteRequest postWriteRequest, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "post/write";
+        }
+
+        postService.save(postWriteRequest);
+        return "redirect:/write";
+    }
+
+    @GetMapping("/{postId}")
+    public String post(@PathVariable Long postId, Model model) {
+        model.addAttribute("postResponse", postService.findById(postId));
+        return "post/post";
+    }
+
+    @GetMapping("/write")
+    public String writeForm(Model model) {
+        PostWriteRequest postWriteRequest = new PostWriteRequest("", "", null, null);
+        model.addAttribute("postWriteRequest", postWriteRequest);
+        return "post/write";
+    }
+}
