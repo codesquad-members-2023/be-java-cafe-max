355ef7fe515794362d9ad0906a0d70ee6f952d5d
chacha <peony@bluehole.net>
2023-04-09 01:22:37 +0900

    bugfix Trasactinal처리로 데이터베이스 연산의 원자성 보장,오류 발생시 롤백 처리를 자동으로 수행

--- src/test/java/kr/codesqaud/cafe/controller/MemberControllerTest.java
+++ src/test/java/kr/codesqaud/cafe/controller/MemberControllerTest.java
@@ -21,9 +21,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 class MemberControllerTest {
     private MockMvc mockMvc;
 
