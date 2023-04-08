b347df496d75a3f34be310d76ee17ca7dce398bb
chacha <peony@bluehole.net>
2023-04-09 01:12:05 +0900

    feat PostRepository 추가

---
+++ src/main/java/kr/codesqaud/cafe/repository/post/PostRepository.java
@@ -0,0 +1,22 @@
+package kr.codesqaud.cafe.repository.post;
+
+import java.util.List;
+import java.util.Optional;
+
+import kr.codesqaud.cafe.domain.Member;
+import kr.codesqaud.cafe.domain.Post;
+
+
+public interface PostRepository {
+    Long save(Post post, Member member);
+
+    Optional<Post> findById(Long postId);
+
+    List<Post> findPostIdByWriterId(String writerId);
+
+    List<Post> findAll();
+
+    void update(Post post);
+
+    void deleteAll();
+}
