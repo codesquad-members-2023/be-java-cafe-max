c07c252309379485638eabdae99427562b0efa9e
CDBchan <kdjin1215@naver.com>
2023-04-09 02:27:39 +0900

    refactor(리뷰반영): DateUtil 클래스 수정
    private 생성자를 통해 외부에서 객체를 만들수 없게 만들었다. 그후 static final 키워드를 사용해 메서드를 통해 원하는 값을 얻을수 있도록 했다.

--- src/main/java/kr/codesqaud/cafe/util/DateUtil.java
+++ src/main/java/kr/codesqaud/cafe/util/DateUtil.java
@@ -5,7 +5,10 @@ import java.time.format.DateTimeFormatter;
 
 public class DateUtil {
 
-    public static String getCurrentDate() {
+    private DateUtil(){
+    }
+
+    public static final String getCurrentDate() {
         LocalDate date = LocalDate.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         return date.format(formatter);
