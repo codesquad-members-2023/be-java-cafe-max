6e0a0fd857e1c1ffcb31afafb1236df4d80e29de
yeonise <cerealnum@gmail.com>
2023-04-09 18:19:07 +0900

    test : 불필요한 초기화 제거

--- src/test/java/kr/codesqaud/cafe/repository/MemoryArticleRepositoryTest.java
+++ src/test/java/kr/codesqaud/cafe/repository/MemoryArticleRepositoryTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class MemoryArticleRepositoryTest {
 
-    MemoryArticleRepository memoryArticleRepository = new MemoryArticleRepository();
+    MemoryArticleRepository memoryArticleRepository;
 
     @BeforeEach
     void afterEach() {
--- src/test/java/kr/codesqaud/cafe/repository/MemoryUserRepositoryTest.java
+++ src/test/java/kr/codesqaud/cafe/repository/MemoryUserRepositoryTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class MemoryUserRepositoryTest {
 
-    MemoryUserRepository memoryUserRepository = new MemoryUserRepository();
+    MemoryUserRepository memoryUserRepository;
 
     @BeforeEach
     void afterEach() {
