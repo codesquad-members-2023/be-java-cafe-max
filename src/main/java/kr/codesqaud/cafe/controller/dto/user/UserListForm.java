c951a8862b58643d558f7000ecfe843d9dc872f4
Sully <2dcoder@naver.com>
2023-04-09 21:58:18 +0900

    feat(User): list 출력을 위한 dto 생성
    password를 뺀 dto

---
+++ src/main/java/kr/codesqaud/cafe/controller/dto/user/UserListForm.java
@@ -0,0 +1,56 @@
+package kr.codesqaud.cafe.controller.dto.user;
+
+import kr.codesqaud.cafe.domain.User;
+
+public class UserListForm {
+    private Long id;
+    private String userId;
+    private String name;
+    private String email;
+
+    public UserListForm() {
+    }
+
+    private UserListForm(Long id, String userId, String name, String email) {
+        this.id = id;
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+    }
+
+    public static UserListForm from(User user) {
+        return new UserListForm(user.getId(), user.getUserId(), user.getName(), user.getEmail());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
