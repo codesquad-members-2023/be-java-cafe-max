4b3b7381d894371d4545a2184097c83a87f67431
Sully <2dcoder@naver.com>
2023-04-10 22:59:19 +0900

    feat: 아이디와 패스워드 정보를 저장하는 DTO 생성

---
+++ src/main/java/kr/codesqaud/cafe/controller/dto/user/UserLoginForm.java
@@ -0,0 +1,26 @@
+package kr.codesqaud.cafe.controller.dto.user;
+
+import javax.validation.constraints.NotBlank;
+
+public class UserLoginForm {
+    @NotBlank(message = "아이디를 입력해주세요.")
+    private String loginId;
+    @NotBlank(message = "패스워드를 입력해주세요.")
+    private String password;
+
+    public String getLoginId() {
+        return loginId;
+    }
+
+    public void setLoginId(String loginId) {
+        this.loginId = loginId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
