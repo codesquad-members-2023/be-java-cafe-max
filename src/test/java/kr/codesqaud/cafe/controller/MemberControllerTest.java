3ee7415d89101b5c68c7ebfe77d82c03009e0c11
road-jin <rjswmtk@gmail.com>
2023-04-10 16:47:50 +0900

    test: validator 빈등록으로 @WebMvcTest으로 테스트 코드 작성

--- src/test/java/kr/codesqaud/cafe/controller/MemberControllerTest.java
+++ src/test/java/kr/codesqaud/cafe/controller/MemberControllerTest.java
@@ -22,14 +22,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(MemberController.class)
+@ComponentScan(basePackages = "kr.codesqaud.cafe.validator")
 class MemberControllerTest {
 
     @Autowired
