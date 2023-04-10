c5ab6429d14b57c60936fa5280c0426815bdd02e
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-04-10 11:59:00 +0900

    fix: UserRepository 인터페이스 추가로 인한 인터페이스 구현

--- src/main/java/kr/codesqaud/cafe/domain/user/UserRepository.java
+++ src/main/java/kr/codesqaud/cafe/domain/user/UserRepository.java
@@ -1,52 +1,21 @@
 package kr.codesqaud.cafe.domain.user;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public class UserRepository {
+public interface UserRepository {
 
-    // key: 사용자 등록번호(Long), value: User 객체
-    private final Map<Long, User> store = new ConcurrentHashMap<>();
-    private static long sequence = 0;
+    List<User> findAll();
 
-    public List<User> findAll() {
-        return new ArrayList<>(store.values());
-    }
+    Optional<User> findById(Long id);
 
-    public Optional<User> findById(Long id) {
-        return Optional.ofNullable(store.get(id));
-    }
+    Optional<User> findByUserId(String userId);
 
-    public Optional<User> findByUserId(String userId) {
-        return store.values().stream()
-            .filter(user -> user.getUserId().equals(userId))
-            .findFirst();
-    }
+    Optional<User> findByEmail(String email);
 
-    public Optional<User> findByEmail(String email) {
-        return store.values().stream()
-            .filter(user -> user.getEmail().equals(email))
-            .findFirst();
-    }
+    User save(User user);
 
-    public User save(User user) {
-        store.put(user.getId(), user);
-        return user;
-    }
+    User modify(User user);
 
-    public synchronized int deleteAll() {
-        int deleteUserCount = store.keySet().size();
-        store.clear();
-        sequence = 0;
-        return deleteUserCount;
-    }
-
-    public synchronized Long nextId() {
-        return ++sequence;
-    }
+    int deleteAll();
 }
