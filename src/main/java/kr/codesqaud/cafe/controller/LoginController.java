70bbff76f2ec67fd9755111c45bf9fbe949bb3ef
Sully <2dcoder@naver.com>
2023-04-10 23:08:48 +0900

    feat: 로그인 기능을 처리하는 컨트롤러 구현

---
+++ src/main/java/kr/codesqaud/cafe/controller/LoginController.java
@@ -0,0 +1,49 @@
+package kr.codesqaud.cafe.controller;
+
+import kr.codesqaud.cafe.controller.dto.user.UserLoginForm;
+import kr.codesqaud.cafe.domain.User;
+import kr.codesqaud.cafe.service.LoginService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+@Controller
+public class LoginController {
+    private final LoginService loginService;
+
+    public LoginController(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
+    @GetMapping("/login")
+    public String loginForm(Model model, UserLoginForm form) {
+        model.addAttribute("login", form);
+        return "user/login";
+    }
+
+    @PostMapping("/login")
+    public String login(@Valid UserLoginForm form, BindingResult bindingResult, HttpServletRequest request) {
+        if (bindingResult.hasErrors()) {
+            return "redirect:/users";
+        }
+
+        User loginUser = loginService.login(form.getLoginId(), form.getPassword());
+
+        if (loginUser == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            return "redirect:/users";
+        }
+
+        // 로그인 성공 처리
+        HttpSession session = request.getSession();
+        session.setAttribute(SessionConst.LOGIN_USER, loginUser);
+
+        return "redirect:/";
+    }
+}
