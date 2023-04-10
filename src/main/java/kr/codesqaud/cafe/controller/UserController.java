61d48b8d84ee899f4fbc013fad619c4edff60f99
sudago <sudago22@gmail.com>
2023-04-10 17:48:02 +0900

    fix: p2) UserController의 url 통일

--- src/main/java/kr/codesqaud/cafe/controller/UserController.java
+++ src/main/java/kr/codesqaud/cafe/controller/UserController.java
@@ -7,36 +7,38 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
     private final UserService userService;
     public UserController(UserService userService){
         this.userService = userService;
     }
 
-    @GetMapping("/user/create")
+    @GetMapping("/create")
     public String createForm(){
         return "user/form";
     }
 
-    @PostMapping("/user/create")
+    @PostMapping("/create")
     public String create(UserForm form){
         User user = new User(form);
         userService.join(user);
         return "redirect:/users";
     }
 
-    @GetMapping("/users")
+    @GetMapping("")
     public String list(Model model){
         List<User> users = userService.findUsers();
         model.addAttribute("users", users);
         return "user/list";
     }
 
-    @GetMapping("/users/{userId}")
+    @GetMapping("/{userId}")
     public String profile(@PathVariable String userId, Model model){
         User user = userService.findOne(userId).get();
         model.addAttribute("user", user);
