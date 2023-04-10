8a7b76205f10947d16279f3d093808c1acef2989
ALbertIM0427 <guang7226961@naver.com>
2023-04-10 20:10:14 +0900

    style: 필요 없는 annotation을 제거

--- src/main/java/kr/codesqaud/cafe/account/UserRepository.java
+++ src/main/java/kr/codesqaud/cafe/account/UserRepository.java
@@ -1,12 +1,10 @@
 package kr.codesqaud.cafe.account;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
