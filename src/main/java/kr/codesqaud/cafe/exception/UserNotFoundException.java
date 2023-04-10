b8f61f1388c12f06066431964e7e0f1733006a6d
CDBchan <kdjin1215@naver.com>
2023-04-10 13:20:59 +0900

    refactor: 오류메세지 오타 수정

--- src/main/java/kr/codesqaud/cafe/exception/UserNotFoundException.java
+++ src/main/java/kr/codesqaud/cafe/exception/UserNotFoundException.java
@@ -2,7 +2,7 @@ package kr.codesqaud.cafe.exception;
 
 public class UserNotFoundException extends RuntimeException{
 
-    private static final String USER_NOT_FOUND_EXCEPTION = "비밀번호가 일치하지 않습니다.";
+    private static final String USER_NOT_FOUND_EXCEPTION = "해당 사용자가 없습니다.";
     public UserNotFoundException(){
         super(USER_NOT_FOUND_EXCEPTION);
     }
