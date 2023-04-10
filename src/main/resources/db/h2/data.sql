d85d23c7e8b82bd010ea6fef66131115c152a089
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-04-10 12:01:56 +0900

    feat: sql 스크립트 추가 및 datasource 설정

---
+++ src/main/resources/db/h2/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO users(userId, password, name, email)
+VALUES ('user1', 'user1user1', '김용일', 'user1@naver.com');
+INSERT INTO users(userId, password, name, email)
+VALUES ('user2', 'user2user2', '김용이', 'user2@naver.com');
+INSERT INTO users(userId, password, name, email)
+VALUES ('user3', 'user3user3', '김용삼', 'user3@naver.com');
+INSERT INTO users(userId, password, name, email)
+VALUES ('user4', 'user4user4', '김용사', 'user4@naver.com');
+INSERT INTO users(userId, password, name, email)
+VALUES ('user5', 'user5user5', '김용오', 'user5@naver.com');
