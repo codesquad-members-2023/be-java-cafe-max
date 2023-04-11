64c093f523dfc4891fb4e80d83b6b61e176d8600
n-aa-g <wlsdnjs0124@gmail.com>
2023-04-11 16:11:23 +0900

    Feat(JdbcUserRepository): save 메서드 구현
    db에 User를 저장하는 메서드를 만들었습니다.
    이 클래스를 사용하는 UserRepository의 코드를 변경했습니다.

--- src/main/java/kr/codesqaud/cafe/repository/JdbcUserRepository.java
+++ src/main/java/kr/codesqaud/cafe/repository/JdbcUserRepository.java
@@ -1,12 +1,25 @@
 package kr.codesqaud.cafe.repository;
 
 import kr.codesqaud.cafe.domain.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+import javax.sql.DataSource;
 import java.util.List;
 
+@Repository
 public class JdbcUserRepository implements UserRepository{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcUserRepository(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
     @Override
     public void save(User user) {
+        String sql = "insert into userTable(userId, password, userName, email) values(?, ?, ?, ?)";
+        jdbcTemplate.update(sql, user.getUserId(), user.getPassword(), user.getUserName(), user.getEmail());
     }
 
     @Override
--- src/main/java/kr/codesqaud/cafe/service/UserService.java
+++ src/main/java/kr/codesqaud/cafe/service/UserService.java
@@ -2,30 +2,32 @@ package kr.codesqaud.cafe.service;
 
 import kr.codesqaud.cafe.domain.User;
 import kr.codesqaud.cafe.domain.UserForm;
+import kr.codesqaud.cafe.repository.JdbcUserRepository;
 import kr.codesqaud.cafe.repository.MemoryUserRepository;
+import kr.codesqaud.cafe.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UserService {
-    private MemoryUserRepository memoryUserRepository;
+    private UserRepository userRepository;
 
     public UserService(MemoryUserRepository memoryUserRepository) {
-        this.memoryUserRepository = memoryUserRepository;
+        this.userRepository = memoryUserRepository;
     }
 
     public void saveUser(UserForm userForm) {
         User user = new User(userForm.getUserId(), userForm.getPassword(), userForm.getUserName(), userForm.getEmail());
 
-        memoryUserRepository.save(user);
+        userRepository.save(user);
     }
 
     public List<User> getUserList() {
-        return memoryUserRepository.getAllUsers();
+        return userRepository.getAllUsers();
     }
 
     public User findByUserId(String userId) {
-        return memoryUserRepository.getSpecificUser(userId);
+        return userRepository.getSpecificUser(userId);
     }
 }
