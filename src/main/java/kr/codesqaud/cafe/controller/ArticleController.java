e0e53603b1ce396f187c342a31d3bd7cee6841e2
takealook <takealook97@naver.com>
2023-04-11 12:14:34 +0900

    ArticleController 생성
    질문하기(포스팅) 및 글 조회 기능

---
+++ src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
@@ -0,0 +1,33 @@
+package kr.codesqaud.cafe.controller;
+
+import kr.codesqaud.cafe.controller.dto.request.PostRequest;
+import kr.codesqaud.cafe.service.ArticleService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class ArticleController {
+    private final ArticleService articleService;
+
+    public ArticleController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
+    @PostMapping("/question")
+    public String post(@ModelAttribute final PostRequest request) {
+        articleService.post(request);
+        return "redirect:/";
+    }
+
+    @GetMapping("/question")
+    public String showArticle(){
+        return "qna/form";
+    }
+
+    @GetMapping("/articles/{articleId}")
+    public String showArticleDetails(@PathVariable final Long articleId, final Model model) {
+        model.addAttribute("article", articleService.findById(articleId));
+        return "qna/show";
+    }
+}
