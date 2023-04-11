68ceddede594532e3d1795e4953e5798eca9d305
othertkfka <57559288+othertkfka@users.noreply.github.com>
2023-04-11 20:33:46 +0900

    feat: 로그인 기능 추가
    Service - userId에 해당하는 데이터가 없는지 체크한 후에 userId에 해당하는 데이터를 조회하여 반환합니다.
    Controller - Optional을 사용하여 null 유무에 따라 반환값을 다르게 해줬습니다.

--- src/main/java/kr/codesqaud/cafe/user/controller/UserController.java
+++ src/main/java/kr/codesqaud/cafe/user/controller/UserController.java
@@ -1,10 +1,14 @@
 package kr.codesqaud.cafe.user.controller;
 
+import kr.codesqaud.cafe.user.domain.User;
 import kr.codesqaud.cafe.user.dto.UserAddForm;
+import kr.codesqaud.cafe.user.dto.UserLoginForm;
 import kr.codesqaud.cafe.user.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 
 @Controller
@@ -21,6 +25,15 @@ public class UserController {
     public String addUser(@ModelAttribute UserAddForm userAddForm) {
         String userId = userService.addUser(userAddForm);
         return "redirect:/user/list";
+    }
+
+    @PostMapping("/login")
+    public String loginUser(@ModelAttribute UserLoginForm userLoginForm) {
+        Optional<User> loginResult = userService.loginCheck(userLoginForm);
+        if (!loginResult.isPresent()) {
+            return "user/login_failed";
+        }
+        return "redirect:/board/list";
     }
 
     @GetMapping("/list")
