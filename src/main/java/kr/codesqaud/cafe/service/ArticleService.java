4d72080dcd55a398e0089171128eb98089297799
takealook <takealook97@naver.com>
2023-04-11 12:18:20 +0900

    ArticleSerivce 생성
    서비스관련 클래스로 포스팅과 article 조회 및 id로 DTO를 리턴할 수 있게 한다.

---
+++ src/main/java/kr/codesqaud/cafe/service/ArticleService.java
@@ -0,0 +1,37 @@
+package kr.codesqaud.cafe.service;
+
+import kr.codesqaud.cafe.controller.dto.ArticleDto;
+import kr.codesqaud.cafe.controller.dto.request.PostRequest;
+import kr.codesqaud.cafe.domain.Article;
+import kr.codesqaud.cafe.repository.ArticleRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ArticleService {
+    private final ArticleRepository articleRepository;
+
+    public ArticleService(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
+    public void post(PostRequest request) {
+        Article article = Article.from(request);
+        articleRepository.save(article);
+    }
+
+    public List<ArticleDto> getArticles() {
+        return articleRepository.findAll()
+                .stream()
+                .map(ArticleDto::from)
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    public ArticleDto findById(final Long id) {
+        return articleRepository.findById(id)
+                .map(ArticleDto::from)
+                .get();
+    }
+}
