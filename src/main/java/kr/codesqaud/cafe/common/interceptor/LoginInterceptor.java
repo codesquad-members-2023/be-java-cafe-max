59f210362099481a850bdb3941964e9f6f8ac55b
Jeongyong Park <1239824@naver.com>
2023-04-11 12:24:38 +0900

    feat : 권한이 없는 사용자는 게시글 작성, 상세내용 조회를 못하도록 기능 구현
    인터셉터를 활용해 session이 존재하지 않을 경우 로그인 창으로 리다이렉트

---
+++ src/main/java/kr/codesqaud/cafe/common/interceptor/LoginInterceptor.java
@@ -0,0 +1,23 @@
+package kr.codesqaud.cafe.common.interceptor;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request,
+		HttpServletResponse response, Object handler) throws IOException {
+		HttpSession session = request.getSession();
+		if (session.getAttribute("sessionedUser") == null) {
+			response.sendRedirect("/user/login");
+			return false;
+		}
+		return true;
+	}
+}
--- src/main/java/kr/codesqaud/cafe/config/MvcConfig.java
+++ src/main/java/kr/codesqaud/cafe/config/MvcConfig.java
@@ -2,8 +2,11 @@ package kr.codesqaud.cafe.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import kr.codesqaud.cafe.common.interceptor.LoginInterceptor;
 
 @Configuration(proxyBeanMethods = false)
 public class MvcConfig implements WebMvcConfigurer {
@@ -14,5 +17,14 @@ public class MvcConfig implements WebMvcConfigurer {
 		registry.addViewController("/user/form").setViewName("user/form");
 		registry.addViewController("/user/login").setViewName("user/login");
 		registry.addViewController("/question/form").setViewName("qna/form");
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		String[] authorizePatterns = new String[] {"/articles/**", "/question/**"};
+
+		registry
+			.addInterceptor(new LoginInterceptor())
+			.addPathPatterns(authorizePatterns);
 	}
 }
