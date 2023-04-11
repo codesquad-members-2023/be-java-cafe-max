f4152222f4630bace5aeae5ebff3c770937e1019
ALbertIM0427 <guang7226961@naver.com>
2023-04-12 07:50:30 +0900

    feat: Custom ValidAnnotation 추가
    하나의 결과를 return 하고 싶어서 추가 했습니다.

---
+++ src/main/java/kr/codesqaud/cafe/account/annotation/ValidEmail.java
@@ -0,0 +1,32 @@
+package kr.codesqaud.cafe.account.annotation;
+
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import javax.validation.ReportAsSingleViolation;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@NotEmpty
+@Email
+@Target({FIELD})
+@ReportAsSingleViolation
+@Retention(RUNTIME)
+@Documented
+@Constraint(validatedBy = {})
+public @interface ValidEmail {
+
+    String message() default "{error.email.correct}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
