bba4669626d12eaf2ecdc379641cc0cf0d50c4b4
CDBchan <kdjin1215@naver.com>
2023-04-11 20:46:59 +0900

    feat: 로그인 성공페이지 구현

--- src/main/java/kr/codesqaud/cafe/controller/LoginController.java
+++ src/main/java/kr/codesqaud/cafe/controller/LoginController.java
@@ -4,7 +4,10 @@ import kr.codesqaud.cafe.config.Session;
 import kr.codesqaud.cafe.controller.dto.LoginDTO;
 import kr.codesqaud.cafe.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -20,9 +23,16 @@ public class LoginController {
 
     @PostMapping("/user/login-success")
     public String userLogin(@ModelAttribute LoginDTO loginDto,HttpServletRequest request){
+        String userId = loginDto.getUserId();
         userService.matchPassword(loginDto);
         HttpSession session = request.getSession();
-        session.setAttribute(Session.LOGIN_MEMBER,loginDto.getUserId());
+        session.setAttribute(Session.LOGIN_MEMBER,userId);
+        return "redirect:/user/login-success/"+userId;
+    }
+
+    @GetMapping("/user/login-success/{userId}")
+    public String showLoginSuccessForm(@PathVariable String userId,Model model){
+        model.addAttribute("user",userService.getUserById(userId));
         return "user/login_success";
     }
 }
