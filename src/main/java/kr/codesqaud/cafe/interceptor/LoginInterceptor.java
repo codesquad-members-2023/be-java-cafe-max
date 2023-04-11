ebbe2b106419bbfbdfa12ad06b7386f562c426ff
Sully <2dcoder@naver.com>
2023-04-12 00:14:27 +0900

    bug: 인터셉터 기능 추가 후 작동하지 않음
    아직은 미완성

---
+++ src/main/java/kr/codesqaud/cafe/interceptor/LoginInterceptor.java
@@ -0,0 +1,22 @@
+package kr.codesqaud.cafe.interceptor;
+
+import kr.codesqaud.cafe.SessionConst;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession(false);
+
+        if (session == null || session.getAttribute(SessionConst.LOGIN_USER) == null) {
+            response.sendRedirect("/login");
+            return false;
+        }
+
+        return true;
+    }
+}
