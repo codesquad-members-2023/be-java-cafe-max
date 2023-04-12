73742a4203d475b969ff87751d7e33bc277be675
ALbertIM0427 <guang7226961@naver.com>
2023-04-12 10:06:32 +0900

    style: 네이밍이 수정

--- src/main/java/kr/codesqaud/cafe/global/GlobalExceptionHandler.java
+++ src/main/java/kr/codesqaud/cafe/global/GlobalExceptionHandler.java
@@ -4,17 +4,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
-public class ExceptionHandler {
+public class GlobalExceptionHandler {
 
-    private static final Logger logger = LoggerFactory.getLogger(ExceptionHandler.class);
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    @org.springframework.web.bind.annotation.ExceptionHandler(RuntimeException.class)
+    @ExceptionHandler(RuntimeException.class)
     public String executeRuntimeErrorHandler(HttpServletRequest request) {
         logger.warn("request:url {}", request.getRequestURI());
         return "error/4xx";
