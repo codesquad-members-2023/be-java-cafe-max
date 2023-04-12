5f406df6b52d43bb28b7bf4b4be3f6ffa0ea4a9d
jinny-l <jinny.l.dev@gmail.com>
2023-04-13 00:45:39 +0900

    ✨feat: 로그인하지 않은 사용자가 게시글 작성 페이지에 접근할 시 로그인 페이지로 이동하는 기능 구현

---
+++ src/main/java/kr/codesqaud/cafe/interceptor/LoginCheckInterceptor.java
@@ -0,0 +1,21 @@
+package kr.codesqaud.cafe.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class LoginCheckInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        HttpSession session = request.getSession(false);
+        if (session == null || session.getAttribute("loginUser") == null) {
+            response.sendRedirect("/login");
+            return false;
+        }
+        return true;
+    }
+
+}
