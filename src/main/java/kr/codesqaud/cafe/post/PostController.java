9b7dcca0bf8d36bf21da90ad29a5857c70be84cf
ALbertIM0427 <guang7226961@naver.com>
2023-04-13 06:00:36 +0900

    refactor: PostController에 authConfig 적용은 HttpSession 정리

--- src/main/java/kr/codesqaud/cafe/post/PostController.java
+++ src/main/java/kr/codesqaud/cafe/post/PostController.java
@@ -5,12 +5,8 @@ import kr.codesqaud.cafe.post.dto.PostForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.util.Optional;
 
@@ -24,33 +20,21 @@ public class PostController {
     }
 
     @GetMapping(value = {"/posts/new", "/posts/form"})
-    public String viewPostForm(@ModelAttribute PostForm postForm, HttpSession httpSession) {
-        Object sessionAttribute = httpSession.getAttribute("user");
-        if (sessionAttribute == null) {
-            return "redirect:/users/login";
-        }
+    public String viewPostForm(@ModelAttribute PostForm postForm) {
         return "/post/form";
     }
 
     @PostMapping("/posts")
-    public String savePost(@Valid PostForm postForm, Errors errors, HttpSession httpSession) {
+    public String savePost(@Valid PostForm postForm, Errors errors, @SessionAttribute User user) {
         if (errors.hasErrors()) {
             return "/post/form";
-        }
-        User user = (User) httpSession.getAttribute("user");
-        if (user == null) {
-            return "redirect:/users/login";
         }
         Post post = postService.save(postForm, user);
         return "redirect:/posts/" + post.getId();
     }
 
     @GetMapping("/posts/{postId}")
-    public String viewPost(Model model, @PathVariable int postId, HttpSession httpSession) {
-        Object sessionAttribute = httpSession.getAttribute("user");
-        if (sessionAttribute == null) {
-            return "redirect:/users/login";
-        }
+    public String viewPost(Model model, @PathVariable int postId) {
         Optional<Post> postOptional = postService.findById(postId);
         if (postOptional.isEmpty()) {
             throw new RuntimeException();
