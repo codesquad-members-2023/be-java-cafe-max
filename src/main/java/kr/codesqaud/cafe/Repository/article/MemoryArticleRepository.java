a6e32267a335a6e818b92d4bda86f8bf7aff53a0
pinetree <eodnjs568@gmail.com>
2023-04-12 17:54:01 +0900

    feat "JdbcArticleRepository 추가 및 H2DB - article table 연동"
    1. JdbcArticleRepository 작성
    - jdbctemplate 이용하여 빈 객체 생성
    - ArticleRepository 메서드 오버라이딩
    - rowMapper 메서드 이용
    2. MemoryArticleRepository @Repository 어노테이션때문인지 에러가 발생해서 주석처리 진행

--- src/main/java/kr/codesqaud/cafe/Repository/article/MemoryArticleRepository.java
+++ src/main/java/kr/codesqaud/cafe/Repository/article/MemoryArticleRepository.java
@@ -1,30 +1,30 @@
-package kr.codesqaud.cafe.Repository.article;
-
-import kr.codesqaud.cafe.Repository.article.ArticleRepository;
-import kr.codesqaud.cafe.domain.Article;
-import org.springframework.stereotype.Repository;
-
-import java.util.*;
-import java.util.concurrent.atomic.AtomicLong;
-
-@Repository
-public class MemoryArticleRepository implements ArticleRepository {
-    private final List<Article> articleRepository = new ArrayList<>();
-    //    private final Map<Integer, Article> articleRepository = new HashMap<>();
-    private AtomicLong sequence = new AtomicLong();
-
-    public void saveArticle(final Article article) {
-        articleRepository.add(article);
-        article.setId(sequence.getAndIncrement());
-    }
-
-    public List<Article> findAllArticle() {
-        return Collections.unmodifiableList(new ArrayList<>(articleRepository));
-    }
-
-    public Optional<Article> findArticleById(final int articleId) {
-        return Optional.ofNullable(articleRepository.get(articleId ));
-    }
-
-
-}
+//package kr.codesqaud.cafe.Repository.article;
+//
+//import kr.codesqaud.cafe.Repository.article.ArticleRepository;
+//import kr.codesqaud.cafe.domain.Article;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.*;
+//import java.util.concurrent.atomic.AtomicLong;
+//
+//@Repository
+//public class MemoryArticleRepository implements ArticleRepository {
+//    private final List<Article> articleRepository = new ArrayList<>();
+//    //    private final Map<Integer, Article> articleRepository = new HashMap<>();
+//    private AtomicLong sequence = new AtomicLong();
+//
+//    public void saveArticle(final Article article) {
+//        articleRepository.add(article);
+//        article.setId(sequence.getAndIncrement());
+//    }
+//
+//    public List<Article> findAllArticle() {
+//        return Collections.unmodifiableList(new ArrayList<>(articleRepository));
+//    }
+//
+//    public Optional<Article> findArticleById(final int articleId) {
+//        return Optional.ofNullable(articleRepository.get(articleId ));
+//    }
+//
+//
+//}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,16 +1,18 @@
-CREATE TABLE IF NOT EXISTS member (
-    id varchar(50) NOT NULL ,
+CREATE TABLE IF NOT EXISTS member
+(
+    id       bigint       NOT NULL AUTO_INCREMENT,
     password varchar(255) NOT NULL,
-    name varchar(10) NOT NULL,
-    email varchar(50) NOT NULL,
+    name     varchar(10)  NOT NULL,
+    email    varchar(50)  NOT NULL,
     PRIMARY KEY (id)
-    );
+);
 
-CREATE TABLE IF NOT EXISTS article (
-    id bigint NOT NULL AUTO_INCREMENT,
-    writer varchar(50) NULL,
-    title	varchar(50) NOT NULL,
-    content	varchar(3000) NOT NULL,
-    writetime	datetime NOT NULL,
-    PRIMARY KEY (id),
-    );
+CREATE TABLE IF NOT EXISTS article
+(
+    id        bigint        NOT NULL AUTO_INCREMENT,
+    writer    varchar(50)   NULL,
+    title     varchar(50)   NOT NULL,
+    content   varchar(3000) NOT NULL,
+    writetime datetime      NOT NULL,
+    PRIMARY KEY (id)
+);
