7aa5c4e80a671a75a2ed164330d8dc9bdb7fdaa5
n-aa-g <wlsdnjs0124@gmail.com>
2023-04-12 17:34:19 +0900

    Feat(LoginController): login getmapping 메서드 구현
    로그인 페이지를 호출하는 메서드를 만들었습니다.

--- src/main/java/kr/codesqaud/cafe/controller/LoginController.java
+++ src/main/java/kr/codesqaud/cafe/controller/LoginController.java
@@ -2,13 +2,21 @@ package kr.codesqaud.cafe.controller;
 
 import kr.codesqaud.cafe.service.LoginService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/login")
 public class LoginController {
 
     private final LoginService loginService;
 
     public LoginController(LoginService loginService) {
         this.loginService = loginService;
+    }
+
+    @GetMapping
+    public String login(LoginForm loginForm) {
+        return "user/login"
     }
 }
