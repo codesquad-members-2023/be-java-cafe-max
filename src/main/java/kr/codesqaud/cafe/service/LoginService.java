2b26b876981f4ab91a05c5382077974fe914d5d5
n-aa-g <wlsdnjs0124@gmail.com>
2023-04-12 16:18:57 +0900

    Feat(LoginService); login 메서드 구현
    userId와 password를 입력받아 회원정보와 일치하면 User를 반환하는 메서드를 구현했습니다.

--- src/main/java/kr/codesqaud/cafe/service/LoginService.java
+++ src/main/java/kr/codesqaud/cafe/service/LoginService.java
@@ -1,5 +1,6 @@
 package kr.codesqaud.cafe.service;
 
+import kr.codesqaud.cafe.domain.User;
 import kr.codesqaud.cafe.repository.JdbcUserRepository;
 import kr.codesqaud.cafe.repository.UserRepository;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,15 @@ public class LoginService {
 
     public LoginService(JdbcUserRepository  jdbcUserRepository) {
         this.userRepository = jdbcUserRepository;
+    }
+
+    public User login(String userId, String password) {
+        User user = userRepository.getSpecificUser(userId);
+
+        if (user.getPassword().equals(password)) {
+            return user;
+        }
+        return null;
     }
 
 }
