889373f980e508e74060b927f80f09ae1c173583
n-aa-g <wlsdnjs0124@gmail.com>
2023-04-12 21:10:01 +0900

    Feat(login): 로그인 post 메서드 구현
    입력받은 id와 password를 통해 만들어진 loginForm을 이용하여 세션을 만드는 메서드를 만들었습니다.
    메서드가 동작할 수 있게 html문서도 변경했습니다.

--- src/main/java/kr/codesqaud/cafe/controller/LoginController.java
+++ src/main/java/kr/codesqaud/cafe/controller/LoginController.java
@@ -1,10 +1,15 @@
 package kr.codesqaud.cafe.controller;
 
 import kr.codesqaud.cafe.domain.LoginForm;
+import kr.codesqaud.cafe.domain.User;
 import kr.codesqaud.cafe.service.LoginService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/login")
@@ -19,5 +24,14 @@ public class LoginController {
     @GetMapping
     public String login(LoginForm loginForm) {
         return "user/login";
+    }
+
+    @PostMapping
+    public String login(LoginForm loginForm, HttpServletRequest request) {
+        User loginUser = loginService.login(loginForm.getUserId(), loginForm.getPassword());
+        HttpSession session = request.getSession();
+
+        session.setAttribute("user", loginUser);
+        return "redirect:/";
     }
 }
