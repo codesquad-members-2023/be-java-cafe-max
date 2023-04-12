2f4ccc98c45ef4b8220f90783c458ddea79027c9
Sully <2dcoder@naver.com>
2023-04-12 15:29:25 +0900

    feat: 글 수정하는 정보만을 담은 DTO 추가

---
+++ src/main/java/kr/codesqaud/cafe/controller/dto/article/ArticleUpdateForm.java
@@ -0,0 +1,50 @@
+package kr.codesqaud.cafe.controller.dto.article;
+
+import kr.codesqaud.cafe.domain.Article;
+
+import javax.validation.constraints.NotBlank;
+
+public class ArticleUpdateForm {
+    private Long id;
+    @NotBlank(message = "제목을 입력해 주세요")
+    private String title;
+    @NotBlank(message = "글을 입력해 주세요")
+    private String contents;
+
+    public ArticleUpdateForm() {
+    }
+
+    private ArticleUpdateForm(Long id, String title, String contents) {
+        this.id = id;
+        this.title = title;
+        this.contents = contents;
+    }
+
+    public static ArticleUpdateForm from(Article article) {
+        return new ArticleUpdateForm(article.getId(), article.getTitle(), article.getContents());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+}
