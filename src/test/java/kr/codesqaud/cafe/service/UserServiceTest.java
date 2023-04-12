13638761c3f5b8a9120fc0cb91890486fc57b805
jinny-l <jinny.l.dev@gmail.com>
2023-04-12 15:31:21 +0900

    ✅test: UserService 테스트 코드 작성
    * test case:
      - 회원 가입을 하면 회원 정보가 저장소에 저장된다.
      - 중복 ID로 회원 가입하면 예외가 발생한다.

---
+++ src/test/java/kr/codesqaud/cafe/service/UserServiceTest.java
@@ -0,0 +1,69 @@
+package kr.codesqaud.cafe.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import kr.codesqaud.cafe.domain.User;
+import kr.codesqaud.cafe.dto.UserDTO;
+import kr.codesqaud.cafe.exception.signUpException.InvalidUserIdException;
+import kr.codesqaud.cafe.repository.UserRepository;
+import kr.codesqaud.cafe.repository.UserRepositoryImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+@JdbcTest
+class UserServiceTest {
+
+    UserService userService;
+    UserRepository userRepository;
+
+    @Autowired
+    NamedParameterJdbcTemplate template;
+
+    @BeforeEach
+    void beforeEach() {
+        userRepository = new UserRepositoryImpl(template);
+        userService = new UserService(userRepository);
+    }
+
+    @Test
+    @DisplayName("회원 가입을 하면 회원 정보가 저장소에 저장된다.")
+    void join() {
+        // given
+        UserDTO userDTO = new UserDTO();
+        userDTO.setUserId("test2");
+        userDTO.setName("test2");
+        userDTO.setPassword("12345678");
+        userDTO.setEmail("test2@gmail.com");
+
+        // when
+        userService.join(userDTO);
+
+        // then
+        User findUser = userRepository.findById(userDTO.getUserId()).get();
+        assertThat(userDTO.getUserId()).isEqualTo(findUser.getUserId()); // TODO: 메모리DB에서 Map으로 저장했을 때는 객체로 비교했는데, Id로 비교하는 것이 맞는지 모르겠다.
+    }
+
+    @Test
+    @DisplayName("중복 ID로 회원 가입하면 예외가 발생한다.")
+    void joinByDuplicatedId() {
+        // given
+        UserDTO userDTO = new UserDTO();
+        userDTO.setUserId("test2");
+        userDTO.setName("test2");
+        userDTO.setPassword("12345678");
+        userDTO.setEmail("test2@gmail.com");
+
+        // when
+        userService.join(userDTO);
+
+        // then
+        InvalidUserIdException e = assertThrows(InvalidUserIdException.class, () -> userService.join(userDTO));
+        assertThat(e.getMessage()).isEqualTo("ID가 이미 존재합니다.");
+    }
+
+}
