ca9065ea842c19c8c72f83a73105bff81356457f
yhpark95 <98851575+yhpark95@users.noreply.github.com>
2023-04-13 15:40:24 +0900

    refactor: Jdbctemplate 대신NamedParameterJdbcTemplate을 활용하여 repository 재구성

--- src/main/java/kr/codesqaud/cafe/article/repository/JdbcArticleRepository.java
+++ src/main/java/kr/codesqaud/cafe/article/repository/JdbcArticleRepository.java
@@ -1,77 +1,81 @@
 package kr.codesqaud.cafe.article.repository;
 
 import kr.codesqaud.cafe.article.domain.Article;
-import kr.codesqaud.cafe.article.dto.ArticleFormDto;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
-import java.sql.*;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class JdbcArticleRepository implements ArticleRepository {
-    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     public JdbcArticleRepository(DataSource dataSource) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
+        this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
     }
 
+    @Override
     public Long save(Article article) {
-        String sql = "INSERT INTO articles (author, title, contents, time) VALUES (?, ?, ?, ?)";
+        String sql = "INSERT INTO articles (author, title, contents, time) VALUES (:author, :title, :contents, :time)";
+        SqlParameterSource sqlParameterSource = new BeanPropertySqlParameterSource(article);
         KeyHolder keyHolder = new GeneratedKeyHolder();
-        jdbcTemplate.update(connection -> {
-            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            ps.setString(1, article.getAuthor());
-            ps.setString(2, article.getTitle());
-            ps.setString(3, article.getContents());
-            ps.setTimestamp(4, Timestamp.valueOf(article.getTime()));
-            return ps;
-        }, keyHolder);
+        String[] keyColumnNames = {"id"}; // specify the name of the ID column
+        namedParameterJdbcTemplate.update(sql, sqlParameterSource, keyHolder, keyColumnNames);
         return keyHolder.getKey().longValue();
     }
 
 
+    @Override
     public List<Article> findAll() {
         String sql = "SELECT * FROM articles";
-        return jdbcTemplate.query(sql, articleRowMapper());
-    }
-
-    public Article findByID(Long index) {
-        String sql = "SELECT * FROM articles WHERE id = ?";
-        return jdbcTemplate.queryForObject(sql, articleRowMapper(), index);
+        return namedParameterJdbcTemplate.query(sql, articleRowMapper());
     }
 
     @Override
-    public Long modify(long id, ArticleFormDto articleFormDto) {
-        String sql = "UPDATE articles SET title=?, contents=?, time=? WHERE id=?";
-        jdbcTemplate.update(connection -> {
-            PreparedStatement ps = connection.prepareStatement(sql);
-            ps.setString(1, articleFormDto.getTitle());
-            ps.setString(2, articleFormDto.getContents());
-            ps.setTimestamp(3, Timestamp.valueOf(LocalDateTime.now())); //repository 역할이 아니다 -> service
-            ps.setLong(4, id);
-            return ps;
-        });
-        return id;
+    public Article findById(Long index) {
+        String sql = "SELECT * FROM articles WHERE id = :id";
+
+        try {
+            SqlParameterSource namedParameters = new MapSqlParameterSource("id", index);
+            return namedParameterJdbcTemplate.queryForObject(sql, namedParameters, articleRowMapper());
+        } catch (EmptyResultDataAccessException e){
+            return null;
+        }
+    }
+
+    @Override
+    public Long modify(Article article) {
+        String sql = "UPDATE articles SET title=:title, contents=:contents, time=:time WHERE id=:id";
+        SqlParameterSource sqlParameterSource = new BeanPropertySqlParameterSource(article);
+        namedParameterJdbcTemplate.update(sql, sqlParameterSource);
+        return article.getId();
     }
 
     @Override
     public void delete(long id) {
-        String sql = "DELETE FROM articles WHERE id = ?";
-        jdbcTemplate.update(sql, id);
+        String sql = "DELETE FROM articles WHERE id = :id";
+        SqlParameterSource namedParameters = new MapSqlParameterSource("id", id);
+        namedParameterJdbcTemplate.update(sql, namedParameters);
     }
 
     private RowMapper<Article> articleRowMapper() {
-        return (rs, rowNum) -> {
-            Article article = new Article(rs.getString("author"), rs.getString("title"),rs.getString("contents"),rs.getLong("id"),rs.getTimestamp("time"));
-            return article;
-        };
+        return (rs, rowNum) -> new Article(
+                rs.getString("author"),
+                rs.getString("title"),
+                rs.getString("contents"),
+                rs.getLong("id"),
+                rs.getTimestamp("time").toLocalDateTime()
+        );
     }
 }
 
