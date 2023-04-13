8ca4505ac2e40479903b144c2879043dfed14231
O_jeegu <lumirite0@gmail.com>
2023-04-14 06:36:28 +0900

    feat : 로그인 세션 및 쿠키 생성 구현
    로그인 성공 시 세션과 쿠키를 생성하여 nav 항목을 바꾸고(로그인, 회원가입 -> 로그아웃,정보수정) 로그아웃 시 세션과 쿠키가 만료되도록 구현.

--- src/main/java/kr/codesqaud/cafe/controller/LoginController.java
+++ src/main/java/kr/codesqaud/cafe/controller/LoginController.java
@@ -7,6 +7,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class LoginController {
 
@@ -21,14 +26,43 @@ public class LoginController {
         return "login/form";
     }
 
+    // todo : HttpServletResponse 뭔지 찾아보기
     @PostMapping("/login")
-    public String login(final LoginDTO loginDTO, final Model model) {
-        boolean isLoginFailed = !loginService.authenticate(loginDTO);
+    public String login(final LoginDTO loginDTO, final Model model, HttpServletRequest request, HttpServletResponse response) {
+        boolean isLoginFailed = !loginService.checkIdAndPw(loginDTO);
         if(isLoginFailed) {
-           model.addAttribute("loginFailed", true);
-           return "login/form";
+            model.addAttribute("loginFailed", true);
+            return "login/form";
         }
-//        loginService.signIn(loginDTO);
+        LoginDTO loginUser = loginService.findId(loginDTO.getUserId());
+
+        //세션 객체 얻어오기
+        HttpSession session = request.getSession();
+        //세션 객체에 id 저장
+        session.setAttribute("loginUser", loginUser);
+
+        //쿠키 생성. 시간 정보 설정 생략 시 세션 쿠키(브라우저 종료 시 삭제)
+        Cookie idCookie = new Cookie("idCookie", String.valueOf(loginUser.getId()));
+        response.addCookie(idCookie);
+
         return "redirect:/";
+
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        HttpSession session = request.getSession(false); // 현재 세션이 없으면 null 반환
+        if (session != null) {
+            session.invalidate(); // 현재 세션을 무효화
+        }
+        expireCookie(response, "id");
+        return "redirect:/";
+    }
+
+    //todo : 로그아웃 해도 application - cookie 안 사라짐
+    private void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
     }
 }
