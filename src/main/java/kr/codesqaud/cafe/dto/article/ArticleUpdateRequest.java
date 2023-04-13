5259eca2a1f089397c071458b8b8746f5c0f5123
yeonise <cerealnum@gmail.com>
2023-04-14 07:32:51 +0900

    feat : 글 수정 시 사용할 DTO 구현

--- src/main/java/kr/codesqaud/cafe/dto/article/ArticleUpdateRequest.java
+++ src/main/java/kr/codesqaud/cafe/dto/article/ArticleUpdateRequest.java
@@ -1,16 +1,27 @@
 package kr.codesqaud.cafe.dto.article;
 
+import kr.codesqaud.cafe.domain.Article;
+
 public class ArticleUpdateRequest {
-    private Long id;
-    private String title;
+    private final Long id;
+    private final String title;
     private String writer;
-    private String contents;
+    private final String contents;
 
     public ArticleUpdateRequest(Long id, String title, String writer, String contents) {
         this.id = id;
         this.title = title;
         this.writer = writer;
         this.contents = contents;
+    }
+
+    public Article toArticle() { // DTO → Entity
+        Article article = new Article();
+        article.setId(this.id);
+        article.setTitle(this.title);
+        article.setWriter(this.writer);
+        article.setContents(this.contents);
+        return article;
     }
 
     public Long getId() { return id; }
