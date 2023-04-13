309a70e4cf73fd6d4a20d5da0e0d4404a90167c4
takealook <takealook97@naver.com>
2023-04-13 14:54:40 +0900

    행 번호를 증가시켜주는 메서드 생성

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,9 +1,21 @@
-String.prototype.format = function() {
+String.prototype.format = function () {
   var args = arguments;
-  return this.replace(/{(\d+)}/g, function(match, number) {
+  return this.replace(/{(\d+)}/g, function (match, number) {
     return typeof args[number] != 'undefined'
         ? args[number]
         : match
         ;
   });
 };
+
+function setRowIdOfUser() {
+  let elements = document.getElementsByClassName("row-id");
+  let idx = 1;
+  for (elem of elements) {
+    elem.innerHTML = idx++;
+  }
+}
+
+$(function() {
+  setRowIdOfUser();
+});
