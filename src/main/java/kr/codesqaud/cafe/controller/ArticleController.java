645aa4c81fb86920cedad45828fc4b68e97891e6
Hana <dokkisan.wang@gmail.com>
2023-04-14 12:37:46 +0900

    refactor: ArticleController 클래스에서 사용하지 않는 logger 제거

--- src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
+++ src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
@@ -3,8 +3,6 @@ package kr.codesqaud.cafe.controller;
 import kr.codesqaud.cafe.article.Article;
 import kr.codesqaud.cafe.article.ArticleDTO;
 import kr.codesqaud.cafe.article.ArticleRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,13 +10,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.math.BigInteger;
-import java.util.List;
 
 @Controller
 public class ArticleController {
 
     private final ArticleRepository articleRepository;
-    private final Logger logger = LoggerFactory.getLogger(ArticleController.class);
 
     public ArticleController(ArticleRepository articleRepository) {
         this.articleRepository = articleRepository;
