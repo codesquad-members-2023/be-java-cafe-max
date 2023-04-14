6a237b3b196ec86c1d4f6086ec0cd459580cfab3
takealook <takealook97@naver.com>
2023-04-14 14:16:04 +0900

    jdbc User 레파지토리 생성

---
+++ src/main/java/kr/codesqaud/cafe/repository/JdbcArticleRepository.java
@@ -0,0 +1,52 @@
+package kr.codesqaud.cafe.repository;
+
+import kr.codesqaud.cafe.domain.Article;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class JdbcArticleRepository implements ArticleRepository{
+    private final JdbcTemplate jdbcTemplate;
+    private final RowMapper<Article> articleMapper = (rs, rowNum) -> new Article(
+            rs.getLong("id"),
+            rs.getString("writer"),
+            rs.getString("title"),
+            rs.getString("content"),
+            rs.getTimestamp("created_at").toLocalDateTime()
+    );
+
+    public JdbcArticleRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Optional<Article> save(Article article) {
+        jdbcTemplate.update("insert into articles(writer, title, content, created_at) values(?, ?, ?, ?)",
+                article.getWriter(),
+                article.getTitle(),
+                article.getContent(),
+                article.getCreatedAt()
+        );
+        return Optional.of(article);
+    }
+
+    @Override
+    public List<Article> findAll() {
+        return jdbcTemplate.query("SELECT * FROM articles", articleMapper);
+    }
+
+    @Override
+    public Optional<Article> findById(Long id) {
+        try {
+            return Optional.ofNullable(
+                    jdbcTemplate.queryForObject("SELECT * FROM articles WHERE id = ?", articleMapper, id));
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+}
---
+++ src/main/java/kr/codesqaud/cafe/repository/JdbcUserRepository.java
@@ -0,0 +1,58 @@
+package kr.codesqaud.cafe.repository;
+
+import kr.codesqaud.cafe.domain.User;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class JdbcUserRepository implements UserRepository {
+    private final JdbcTemplate jdbcTemplate;
+    private final RowMapper<User> userMapper = (rs, rowNum) -> new User(
+            rs.getString("user_id"),
+            rs.getString("password"),
+            rs.getString("user_name"),
+            rs.getString("user_email")
+    );
+
+    public JdbcUserRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Optional<User> save(User user) {
+        jdbcTemplate.update("insert into users(user_id, password, user_name, user_email) values(?, ?, ?, ?)",
+                user.getUserId(),
+                user.getPassword(),
+                user.getUserName(),
+                user.getUserEmail()
+        );
+        return Optional.of(user);
+    }
+
+    @Override
+    public Optional<User> findByUserId(String userId) {
+        try {
+            return Optional.ofNullable(
+                    jdbcTemplate.queryForObject("SELECT * FROM users WHERE user_id = ?", userMapper, userId));
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public List<User> findAll() {
+        return jdbcTemplate.query("SELECT * FROM users", userMapper);
+    }
+
+    @Override
+    public void update(User user) {
+        jdbcTemplate.update("UPDATE users SET password = ?, user_name = ?, user_email = ? WHERE user_id = ?",
+                user.getPassword(), user.getUserName(), user.getUserEmail(), user.getUserId());
+
+    }
+}
