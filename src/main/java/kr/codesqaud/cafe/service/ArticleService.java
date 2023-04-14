082565471046e263e3767822f091d0eb9b215038
sejeong00 <buttermelt@daum.net>
2023-04-14 20:18:25 +0900

    feat id로 게시글 조회하는 메서드 구현

--- src/main/java/kr/codesqaud/cafe/service/ArticleService.java
+++ src/main/java/kr/codesqaud/cafe/service/ArticleService.java
@@ -1,14 +1,17 @@
 package kr.codesqaud.cafe.service;
 
+import kr.codesqaud.cafe.domain.entity.User;
 import kr.codesqaud.cafe.dto.ArticlePostRequest;
 import kr.codesqaud.cafe.domain.entity.Article;
 import kr.codesqaud.cafe.dto.ArticleResponse;
+import kr.codesqaud.cafe.dto.UserResponse;
 import kr.codesqaud.cafe.repository.ArticleH2Repository;
 import kr.codesqaud.cafe.repository.ArticleRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Transactional
@@ -32,6 +35,19 @@ public class ArticleService{
                 .collect(Collectors.toList());
 
     }
+
+    public ArticleResponse findByArticleId(long articleId){
+        Optional<Article> targetArticle = articleRepository.findByArticleId(articleId);
+
+        //TODO Entity -> Dto 메서드
+        return new ArticleResponse(
+                targetArticle.get().getArticleId(),
+                targetArticle.get().getWriter(),
+                targetArticle.get().getTitle(),
+                targetArticle.get().getContents()
+        );
+    }
+
     public void post(ArticlePostRequest articlePostRequest){
         Article article = new Article(
                 articlePostRequest.getWriter(),
