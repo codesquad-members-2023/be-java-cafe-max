1f75a8d633cdc2c61a2bc77de756be7eadbe5d60
CDBchan <kdjin1215@naver.com>
2023-04-14 12:08:26 +0900

    test: Mock 객체를 사용해 UserController 테스트 작성

--- src/test/java/kr/codesqaud/cafe/controller/UserControllerTest.java
+++ src/test/java/kr/codesqaud/cafe/controller/UserControllerTest.java
@@ -1,15 +1,21 @@
 package kr.codesqaud.cafe.controller;
 
+import kr.codesqaud.cafe.config.Session;
 import kr.codesqaud.cafe.controller.dto.user.UserDTO;
 import kr.codesqaud.cafe.controller.dto.user.UserListDTO;
 import kr.codesqaud.cafe.service.UserService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.ArrayList;
@@ -27,15 +33,25 @@ class UserControllerTest {
     @MockBean
     UserService userService;
 
+    private MockHttpSession httpSession;
+
+    @BeforeEach
+    public void setUp() {
+        httpSession = new MockHttpSession();
+        Session session = new Session("id", "testUser");
+        httpSession.setAttribute(Session.LOGIN_USER, session);
+    }
+
     @Test
     @DisplayName("회원가입시 db에 회원을 저장한후 user/list 로 리다이렉트한다.")
     void signUpTest() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.post("/user/sign-up")
+                        .session(httpSession)
                         .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                         .param("nickName", "nickName")
                         .param("email", "aaaa@naver.com")
-                        .param("password","password123")
-                        .param("id","charlie"))
+                        .param("password", "password123")
+                        .param("id", "charlie"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/user/list"));
     }
@@ -48,56 +64,54 @@ class UserControllerTest {
         given(userService.getUserList()).willReturn(userList);
 
         //when & then
-        mockMvc.perform(MockMvcRequestBuilders.get("/user/list"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/user/list")
+                        .session(httpSession))
                 .andExpect(status().isOk())
                 .andExpect(view().name("user/list"))
                 .andExpect(model().attributeExists("users"))//객체 검증
-                .andExpect(model().attribute("users",userList));
+                .andExpect(model().attribute("users", userList));
     }
 
     @Test
-    @DisplayName("유저의 정보를 db로부터 가져와 볼수있다.")
+    @DisplayName("유저의 id,nickName,email정보를 db로부터 가져와 user/profile에서 볼수있다.")
     void showUserProfileTest() throws Exception {
         //given
         UserDTO userDTO = new UserDTO("nickName", "aaa@naver.com", "password123", "testId");
         given(userService.getUserById("testId")).willReturn(userDTO);
 
         //when & then
-        mockMvc.perform(MockMvcRequestBuilders.get("/user/profile/testId"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/user/profile/testId")
+                        .session(httpSession))
                 .andExpect(status().isOk())
                 .andExpect(view().name("user/profile"))
                 .andExpect(model().attributeExists("user"))//객체 검증
-                .andExpect(model().attribute("user",userDTO));
+                .andExpect(model().attribute("user", userDTO));
 
     }
 
     @Test
-    @DisplayName("유저의 정보를 수정할수있는 id가 입력된상태의 form으로 이동한다.")
-    void userUpdateFormTest() throws Exception {
-        //given
-        UserDTO userDTO = new UserDTO("nickName", "aaa@naver.com", "password123", "testId");
-        given(userService.getUserById("testId")).willReturn(userDTO);
-
-        //when & then
-        mockMvc.perform(MockMvcRequestBuilders.get("/user/profile/testId/form"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("user/updateForm"))
-                .andExpect(model().attributeExists("user"))//객체 검증
-                .andExpect(model().attribute("user",userDTO));
-    }
-
-    @Test
-    @DisplayName("유저의 정보를 수정하면 db에 정보를 업데이트후 멤버 리스트로 리다이렉트 한다.")
+    @DisplayName("유저의 정보를 수정하면 db에 정보를 업데이트후 세션값을 바꾸고 멤버 리스트로 리다이렉트 한다.")
     void updateUserDataTest() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.put("/user/profile/testId")
-                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                        .param("nickName", "charlie")
-                        .param("email", "aaaa@naver.com")
-                        .param("oriPassword","password123")
-                        .param("newPassword","newpassword123")
-                        .param("id","testId"))
-                .andExpect(status().is3xxRedirection())
-                .andExpect(view().name("redirect:/users"));
 
+        //given & when
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.put("/user/profile/testId")
+                        .session(httpSession)
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .param("nickName", "nickName")
+                        .param("email", "aaa@naver.com")
+                        .param("newPassword", "password123")
+                        .param("oriPassword", "password1234")
+                        .param("id", "testId"))
+
+                //then
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/user/list"));
+
+        MvcResult mvcResult = resultActions.andReturn();
+        MockHttpSession resultSession = (MockHttpSession) mvcResult.getRequest().getSession();
+        Session session = (Session) resultSession.getAttribute(Session.LOGIN_USER);
+
+        Assertions.assertThat(session.getId().equals("testId")).isTrue();
+        Assertions.assertThat(session.getNickName().equals("nickName")).isTrue();
     }
 }
