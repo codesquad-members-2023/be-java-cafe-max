c5f7afcc915ad5cf1264aa27a49173cd8c270723
code-tree <dltjdqls1200@naver.com>
2023-04-15 19:15:54 +0900

    refactor: boolean 메서드 긍정형으로 수정

--- src/main/java/kr/codesqaud/cafe/common/auth/LoginInterceptor.java
+++ src/main/java/kr/codesqaud/cafe/common/auth/LoginInterceptor.java
@@ -1,5 +1,6 @@
 package kr.codesqaud.cafe.common.auth;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,13 +9,12 @@ import javax.servlet.http.HttpSession;
 
 public class LoginInterceptor implements HandlerInterceptor {
     private static final String LOGIN_URL = "/users/login";
-    private static final String POST_METHOD = "POST";
     private static final String USER_JOIN_URL = "/users";
     private static final String ARTICLE_CREATE_URL = "/articles";
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        if (POST_METHOD.equals(request.getMethod())) {
+        if (HttpMethod.POST.matches(request.getMethod())) {
             final String requestURI = request.getRequestURI();
             if (USER_JOIN_URL.equals(requestURI) || ARTICLE_CREATE_URL.equals(requestURI)) {
                 return true;
@@ -23,15 +23,16 @@ public class LoginInterceptor implements HandlerInterceptor {
 
         final HttpSession session = request.getSession(false);
 
-        if (hasNotLoginSession(session)) {
-            response.sendRedirect(LOGIN_URL);
-            return false;
+        if (hasLoginSession(session)) {
+            return true;
         }
 
-        return true;
+        response.sendRedirect(LOGIN_URL);
+
+        return false;
     }
 
-    private boolean hasNotLoginSession(HttpSession session) {
-        return session == null || session.getAttribute("loginUser") == null;
+    private boolean hasLoginSession(HttpSession session) {
+        return session != null && session.getAttribute("loginUser") != null;
     }
 }
