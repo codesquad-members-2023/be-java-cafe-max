d46be17e6f79978fb77e22fd8f85cb299898a2bc
chacha <peony@bluehole.net>
2023-04-15 21:16:19 +0900

    feat 회원정보 수정시 제한사항 추가

--- src/main/java/kr/codesqaud/cafe/dto/member/ProfileEditRequestDto.java
+++ src/main/java/kr/codesqaud/cafe/dto/member/ProfileEditRequestDto.java
@@ -1,24 +1,25 @@
 package kr.codesqaud.cafe.dto.member;
 
-import org.hibernate.validator.constraints.Length;
-
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 public class ProfileEditRequestDto {
     private final Long memberId;
 
-    @NotBlank
-    @Email
+    @NotBlank(message = "이메일을 입력하세요")
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
     private final String email;
 
-    @NotBlank
-    @Pattern(regexp = "^(?=.*\\d)(?=.*[A-Z])(?=.*[a-z])[a-zA-Z\\d]{8,32}$")
+    @NotBlank(message = "비밀번호를 입력하세요.")
+    @Size(min = 8, max = 12, message = "비밀번호는 {min} ~ {max} 길이로 입력하세요.")
+    @Pattern(regexp = "[a-zA-Z0-9!@#$%^&*()_+|~=`{}\\[\\]:\";'\\\\<>,.?/\\-]+", message = "비밀번호는 알파벳, 숫자, 특수문자만 가능합니다.")
     private final String password;
 
-    @NotBlank
-    @Length(min = 2, max = 10)
+    @NotBlank(message = "닉네임을 입력하세요")
+    @Size(min = 2, max = 10, message = "유효한 닉네임을 입력하세요")
+    @Pattern(regexp = "^[가-힣]{2,4}$", message = "닉네임 형식에 맞게 입력하세요.")
     private final String nickName;
 
     public ProfileEditRequestDto(Long memberId, String email, String password, String nickName) {
