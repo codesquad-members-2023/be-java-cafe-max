c0e82366043e96ae7fa3fed2ab03e96e3154eac9
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-04-15 20:18:31 +0900

    feat: 에러에 대한 정보를 담는 ErrorCode 인터페이스 정의 및 클래스 구현
    기존 예외처리타입의 인터페이스와 구현체 타입들을 다음과 같이 변경하였습니다.
    변경한 이유는 예외에 대한 정보가 아닌 에러에 대한 정보를 담고 있기 때문에 ErrorCode와 같이 변경하였습니다.
    * BaseExceptionType -> ErrorCode,
    UserExceptionType -> UserErrorCode,
    ArticleExceptionType -> QuestionErrorCode

---
+++ src/main/java/kr/codesqaud/cafe/errors/errorcode/CommonErrorCode.java
@@ -0,0 +1,31 @@
+package kr.codesqaud.cafe.errors.errorcode;
+
+import org.springframework.http.HttpStatus;
+
+public enum CommonErrorCode implements ErrorCode {
+    INVALID_INPUT_FORMAT(HttpStatus.BAD_REQUEST, "유효하지 않은 입력 형식입니다.");
+
+    private HttpStatus httpStatus;
+    private String message;
+
+    CommonErrorCode(HttpStatus httpStatus, String message) {
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
+
+
+    @Override
+    public String getName() {
+        return name();
+    }
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ src/main/java/kr/codesqaud/cafe/errors/errorcode/UserErrorCode.java
@@ -0,0 +1,35 @@
+package kr.codesqaud.cafe.errors.errorcode;
+
+import org.springframework.http.HttpStatus;
+
+public enum UserErrorCode implements ErrorCode {
+    ALREADY_EXIST_USERID(HttpStatus.CONFLICT, "이미 존재하는 아이디입니다."),
+    ALREADY_EXIST_EMAIL(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다."),
+    NOT_FOUND_USER(HttpStatus.CONFLICT, "회원을 찾을 수 없습니다."),
+    NOT_MATCH_LOGIN(HttpStatus.BAD_REQUEST, "아이디 또는 비밀번호가 일치하지 않습니다."),
+    NOT_MATCH_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다.");
+
+    private HttpStatus httpStatus;
+    private String message;
+
+    UserErrorCode(HttpStatus httpStatus, String message) {
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
+
+
+    @Override
+    public String getName() {
+        return name();
+    }
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
