d0f21dc6c10c1e63f3c90c0b79081ac320c79155
JJONSOO <pjs970616@naver.com>
2023-04-16 13:37:57 +0900

    [feat] Transactional(readOnly=true) Annotation 추가 (add,update 시 @Transactional )

--- src/main/java/kr/codesqaud/cafe/domain/user/repository/impl/JdbcUserRepositoryImpl.java
+++ src/main/java/kr/codesqaud/cafe/domain/user/repository/impl/JdbcUserRepositoryImpl.java
@@ -6,26 +6,31 @@ import java.util.Optional;
 
 import javax.sql.DataSource;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import kr.codesqaud.cafe.domain.user.entity.User;
 import kr.codesqaud.cafe.domain.user.repository.UserRepository;
 
 @Repository
+@Transactional(readOnly = true)
 public class JdbcUserRepositoryImpl implements UserRepository {
 
 	private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+	private final RowMapper<User> userRowMapper = BeanPropertyRowMapper.newInstance(User.class);
 
 	public JdbcUserRepositoryImpl(DataSource dataSource) {
 		this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
 	}
 
+	@Transactional
 	public void save(User user) {
-		final String SQL = "INSERT INTO users ( password, name, email, dateTime) VALUES (:password, :name, :email,:dateTime)";
+		final String SQL = "INSERT INTO users ( id, password, name, email, dateTime) VALUES (:id,:password, :name, :email,:dateTime)";
 		namedParameterJdbcTemplate.update(SQL, new BeanPropertySqlParameterSource(user));
 	}
 
@@ -34,17 +39,17 @@ public class JdbcUserRepositoryImpl implements UserRepository {
 		return namedParameterJdbcTemplate.query(SQL, BeanPropertyRowMapper.newInstance(User.class));
 	}
 
-	public Optional<User> findById(Long id) {
+	public Optional<User> findById(String id) {
 		final String SQL = "SELECT * FROM users WHERE id = :id";
-		User user = namedParameterJdbcTemplate.queryForObject(SQL,
-			new MapSqlParameterSource().addValue("id", id),
-			BeanPropertyRowMapper.newInstance(User.class));
-		if (user == null) {
+		try {
+			return namedParameterJdbcTemplate.queryForStream(SQL, Map.of("id", id), userRowMapper)
+				.findFirst();
+		} catch (DataAccessException e) {
 			return Optional.empty();
 		}
-		return Optional.of(user);
 	}
 
+	@Transactional
 	public void update(User user) {
 		final String SQL = "UPDATE users SET name = :name, email = :email, password = :password WHERE id = :id";
 		Map<String, Object> parameter = Map.of(
