f79c56df31452a155d7f1a663f3fa87963474ba9
ALbertIM0427 <guang7226961@naver.com>
2023-04-16 19:40:22 +0900

    refactor: 암호화 적용

--- src/main/java/kr/codesqaud/cafe/config/AppStartupRunner.java
+++ src/main/java/kr/codesqaud/cafe/config/AppStartupRunner.java
@@ -3,23 +3,25 @@ package kr.codesqaud.cafe.config;
 import kr.codesqaud.cafe.account.Role;
 import kr.codesqaud.cafe.account.User;
 import kr.codesqaud.cafe.account.UserRepository;
-import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.jasypt.encryption.StringEncryptor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Profile("cloud")
 @Component
 public class AppStartupRunner implements CommandLineRunner {
 
-    @Autowired
-    UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final StringEncryptor encryptor;
+
+    public AppStartupRunner(UserRepository userRepository, StringEncryptor encryptor) {
+        this.userRepository = userRepository;
+        this.encryptor = encryptor;
+    }
 
     @Override
     public void run(String... args) {
-        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
-        encryptor.setPassword("dip3tH7IJrLXRfKXCJfh4uR0rjnMjD8b");
-        encryptor.setAlgorithm("PBEWithMD5AndTripleDES");
-
         User build = new User.Builder()
                 .password(encryptor.decrypt("W5p8Dbc3f7sOjqaRc7WVp8inlukLB3LJ"))
                 .email(encryptor.decrypt("R9gvxFpZK6xYikAdPrdy1mDtd3O+ag2L"))
