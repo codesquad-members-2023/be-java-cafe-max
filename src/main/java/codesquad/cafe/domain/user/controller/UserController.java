43139b7556ca192ba414cf68bb638c170e2e2c22
chung <chunghye1998>
2023-04-18 00:10:28 +0900

    refactor: 개인정보 수정 기능 session 사용하도록 수정

--- src/main/java/codesquad/cafe/domain/user/controller/UserController.java
+++ src/main/java/codesquad/cafe/domain/user/controller/UserController.java
@@ -6,6 +6,7 @@ import codesquad.cafe.domain.user.dto.UserRequestDto;
 import codesquad.cafe.domain.user.dto.UserResponseDto;
 import codesquad.cafe.domain.user.dto.UserUpdateRequestDto;
 import codesquad.cafe.domain.user.service.UserService;
+import codesquad.cafe.global.exception.CustomException;
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
 import org.springframework.stereotype.Controller;
@@ -18,9 +19,12 @@ import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.util.List;
 
+import static codesquad.cafe.global.exception.ErrorCode.*;
+
 @Controller
 @RequestMapping("/users")
 public class UserController {
+    private final String LOGIN_USER = "loginUser";
     private Log log = LogFactory.getLog(UserController.class);
 
     private final UserService userService;
@@ -50,15 +54,18 @@ public class UserController {
     }
 
     @GetMapping("/{userId}/form")
-    public String showUpdateForm(@PathVariable String userId, final Model model) {
-        model.addAttribute("userId", userId);
+    public String showUpdateForm(@PathVariable String userId, HttpSession session) {
+        User sessionUser = findSessionUser(session);
+        userService.validateUpdateUser(sessionUser, userId);
         return "user/updateForm";
     }
 
     @PutMapping("/{userId}/update")
     public String updateUser(@PathVariable String userId,
-                             @ModelAttribute @Valid UserUpdateRequestDto userUpdateRequestDto) {
-        userService.updateUser(userId, userUpdateRequestDto);
+                             @ModelAttribute @Valid UserUpdateRequestDto userUpdateRequestDto,
+                             HttpSession session) {
+        User sessionUser = findSessionUser(session);
+        userService.updateUser(userId, userUpdateRequestDto, sessionUser);
         return "redirect:/users";
     }
 
@@ -73,7 +80,7 @@ public class UserController {
         User user = userService.login(userLoginRequestDto);
         log.info("로그인 성공");
         HttpSession session = request.getSession();
-        session.setAttribute("loginUser", user);
+        session.setAttribute(LOGIN_USER, user);
         return "redirect:/";
     }
 
@@ -83,5 +90,13 @@ public class UserController {
         session.invalidate();
         log.info("로그아웃 성공");
         return "redirect:/";
+    }
+
+    private User findSessionUser(final HttpSession session) {
+        Object value = session.getAttribute(LOGIN_USER);
+        if (value == null) {
+            throw new CustomException(NOT_FOUND_SESSION);
+        }
+        return (User) value;
     }
 }
