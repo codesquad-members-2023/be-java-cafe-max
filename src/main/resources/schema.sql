fe5fe3d06da4f5310526fb784042f87efa2a291a
chung <chunghye1998>
2023-04-17 16:58:18 +0900

    fix: article table에 user id foreign key로 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,17 +1,23 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS article;
 
-create table article(
-    id bigint auto_increment,
-    writer varchar(200) not null,
-    title varchar(200) not null,
-    contents text not null,
-    createdAt datetime,
-    primary key(id)
+CREATE TABLE users
+(
+    id       varchar(200) not null,
+    password varchar(200) not null,
+    name     varchar(200) not null,
+    email    varchar(200) not null,
+    PRIMARY KEY (id)
 );
 
-create table cafe_user(
-    id varchar(200) not null,
-    password varchar(200) not null,
-    name varchar(200) not null,
-    email varchar(200) not null,
-    primary key (id)
-)
+CREATE TABLE article
+(
+    id        bigint auto_increment,
+    writer    varchar(200) not null,
+    title     varchar(200) not null,
+    contents  text         not null,
+    createdAt datetime,
+    writer_id varchar(200) not null,
+    PRIMARY KEY (id),
+    FOREIGN KEY (writer_id) REFERENCES users (id) ON UPDATE CASCADE
+);
