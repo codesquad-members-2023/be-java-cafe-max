424a623a4b05f78e97162b3ad4853442c6ba9fc2
Jeongyong Park <1239824@naver.com>
2023-04-17 16:34:24 +0900

    docs : 댓글 테이블 스키마 작성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,6 +5,7 @@ CREATE TABLE IF NOT EXISTS article
     title      VARCHAR(64)  NOT NULL,
     content    VARCHAR(255) NOT NULL,
     created_at DATETIME     NOT NULL,
+    is_deleted BOOLEAN      NOT NULL DEFAULT FALSE,
     PRIMARY KEY (id)
 );
 
@@ -15,4 +16,15 @@ CREATE TABLE IF NOT EXISTS user_account
     name     VARCHAR(64)  NOT NULL,
     email    VARCHAR(64)  NOT NULL,
     PRIMARY KEY (user_id)
+);
+
+CREATE TABLE IF NOT EXISTS article_comment
+(
+    id         BIGINT       NOT NULL AUTO_INCREMENT,
+    content    VARCHAR(255) NOT NULL,
+    created_at DATETIME     NOT NULL,
+    is_deleted BOOLEAN      NOT NULL DEFAULT FALSE,
+    writer     VARCHAR(64)  NOT NULL,
+    article_id BIGINT       NOT NULL,
+    PRIMARY KEY (id)
 );
