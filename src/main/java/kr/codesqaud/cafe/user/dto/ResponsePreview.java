613ac88832281a159d572514ab082b7238dcd897
yhpark95 <98851575+yhpark95@users.noreply.github.com>
2023-04-17 15:44:13 +0900

    fix: Response User Dto에 setter -> 생성자로 교체

--- src/main/java/kr/codesqaud/cafe/user/dto/ResponsePreview.java
+++ src/main/java/kr/codesqaud/cafe/user/dto/ResponsePreview.java
@@ -2,33 +2,27 @@ package kr.codesqaud.cafe.user.dto;
 
 public class ResponsePreview {
     //회원 목록 조회영 사용자DTO
-    private String userId;
+    private final String userId;
 
-    private String name;
+    private final String name;
 
-    private String email;
+    private final String email;
+
+    public ResponsePreview(String userId, String name, String email) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+    }
 
     public String getUserId() {
         return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 }
--- src/main/java/kr/codesqaud/cafe/user/dto/ResponseProfile.java
+++ src/main/java/kr/codesqaud/cafe/user/dto/ResponseProfile.java
@@ -3,24 +3,21 @@ package kr.codesqaud.cafe.user.dto;
 public class ResponseProfile {
     //회원 프로필 내용 DTO
 
-    private String name;
+    private final String name;
 
-    private String email;
+    private final String email;
+
+    public ResponseProfile(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
 }
