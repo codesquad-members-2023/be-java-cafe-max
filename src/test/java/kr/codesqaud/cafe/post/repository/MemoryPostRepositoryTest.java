226f1e5aa713bb5746e57a51c2a39b8b097d7bc8
김수민(Sumin-Kim) <sumin.kim.develop@gmail.com>
2023-04-17 18:08:19 +0900

    게시글 관련 테스트 코드 작성
    - DB 버전과 HashMap 버전 분리
    - HashMap 버전은 매 테스트마다 저장소가 초기화 됨
    - DB 버전은 같은 테이블을 재활용 하므로 id가 초기화되지 않음

--- src/test/java/kr/codesqaud/cafe/post/repository/MemoryPostRepositoryTest.java
+++ src/test/java/kr/codesqaud/cafe/post/repository/MemoryPostRepositoryTest.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class PostRepositoryTest {
+class MemoryPostRepositoryTest {
 
     private PostRepository postRepository;
 
