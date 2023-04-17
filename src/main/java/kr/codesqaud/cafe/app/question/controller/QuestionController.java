f4da88d38f575b49f9bf5746597ba6f12ea3d74b
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-04-17 12:49:51 +0900

    fix: QuestionService에서 dto 객체를 반환하는 것이 아닌 엔티티 객체를 반환함에 따라 QuestionController에서 Dto 객체로 변환하는 것으로 변경
    * QuestionService에서 dto 객체를 반환하는 것에서 엔티티 객체로 반환한 이유는 어느 dto 객체 타입에 종속되는 것이 아닌 엔티티 객체를 반환하고 컨트롤러 레이어에서 각자 변환하여 외부로 전달하는 것이 적절하다고 생각하였습니다.

--- src/main/java/kr/codesqaud/cafe/app/question/controller/QuestionController.java
+++ src/main/java/kr/codesqaud/cafe/app/question/controller/QuestionController.java
@@ -1,10 +1,16 @@
 package kr.codesqaud.cafe.app.question.controller;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import kr.codesqaud.cafe.app.question.controller.dto.QuestionResponse;
 import kr.codesqaud.cafe.app.question.controller.dto.QuestionSavedRequest;
+import kr.codesqaud.cafe.app.question.entity.Question;
 import kr.codesqaud.cafe.app.question.service.QuestionService;
+import kr.codesqaud.cafe.app.user.entity.User;
+import kr.codesqaud.cafe.app.user.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -22,23 +28,34 @@ public class QuestionController {
 
     private static final Logger logger = LoggerFactory.getLogger(QuestionController.class);
     private final QuestionService questionService;
+    private final UserService userService;
 
-    public QuestionController(QuestionService questionService) {
+    public QuestionController(QuestionService questionService, UserService userService) {
         this.questionService = questionService;
+        this.userService = userService;
     }
 
     // 전체 게시글 조회
     @GetMapping({"/", "/qna"})
     public String home(Model model) {
-        model.addAttribute("articles", questionService.getAllArticles());
+        List<Question> questions = questionService.findAllQuestions();
+        List<QuestionResponse> questionResponses = new ArrayList<>();
+        for (Question question : questions) {
+            User user = userService.findUser(question.getUserId());
+            questionResponses.add(new QuestionResponse(question, user));
+        }
+        Collections.sort(questionResponses);
+        model.addAttribute("questions", questionResponses);
         return "index";
     }
-
 
     // 특정 게시글 조회
     @GetMapping("/qna/{id}")
     public String detail(@PathVariable(value = "id") Long id, Model model) {
-        model.addAttribute("article", questionService.findArticle(id));
+        Question question = questionService.findQuestion(id);
+        User user = userService.findUser(question.getUserId());
+        QuestionResponse questionResponse = new QuestionResponse(question, user);
+        model.addAttribute("question", questionResponse);
         return "qna/detail";
     }
 
@@ -47,7 +64,10 @@ public class QuestionController {
     @ResponseBody
     public QuestionResponse create(@Valid @RequestBody QuestionSavedRequest requestDto) {
         logger.info("create : " + requestDto.toString());
-        return questionService.write(requestDto);
+        User user = userService.findUser(requestDto.getUserId());
+        Question question = requestDto.toEntity(user.getId());
+        Question save = questionService.write(question);
+        return new QuestionResponse(save, user);
     }
 
     // 게시글 글쓰기 페이지
