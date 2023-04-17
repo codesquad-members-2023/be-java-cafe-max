bac7a52f855099c1513272c036a13216532fa928
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-04-17 12:58:04 +0900

    fix: isPasswordNotMatching -> isPasswordMatching으로 메소드명 변경

--- src/main/java/kr/codesqaud/cafe/app/user/validator/UserValidator.java
+++ src/main/java/kr/codesqaud/cafe/app/user/validator/UserValidator.java
@@ -10,20 +10,20 @@ public class UserValidator {
 
     // 두 회원의 비밀번호가 일치하는지 검증
     public void validateEqualConfirmPassword(String requestPassword, String password) {
-        if (isPasswordNotMatching(requestPassword, password)) {
+        if (!isPasswordMatching(requestPassword, password)) {
             throw new RestApiException(UserErrorCode.NOT_MATCH_PASSWORD);
         }
     }
 
     // 로그인 검증 (아이디 존재, 비밀번호 일치)
     public void validateLoginPassword(String requestPassword, String password) {
-        if (isPasswordNotMatching(requestPassword, password)) {
+        if (!isPasswordMatching(requestPassword, password)) {
             throw new RestApiException(UserErrorCode.NOT_MATCH_LOGIN);
         }
     }
 
     // 비밀번호가 일치하는지 않는지 검사
-    private boolean isPasswordNotMatching(String requestPassword, String password) {
+    private boolean isPasswordMatching(String requestPassword, String password) {
         return !Objects.equals(requestPassword, password);
     }
 
