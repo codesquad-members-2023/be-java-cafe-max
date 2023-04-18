85756e01acbd64feb7eae7963fa3287cdc5f1dbb
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-04-19 08:45:46 +0900

    feat: ErrorController 추가
    * ErrorController는 클라이언트, 서버 에러를 매핑하여 에러 페이지를 응답하는 컨트롤러입니다.

---
+++ src/main/java/kr/codesqaud/cafe/app/error/ErrorController.java
@@ -0,0 +1,22 @@
+package kr.codesqaud.cafe.app.error;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ErrorController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ErrorController.class);
+
+    @GetMapping("/error/client-error")
+    public String errorClient() {
+        return "error/4xx";
+    }
+
+    @GetMapping("/error/server-error")
+    public String errorServer() {
+        return "error/5xx";
+    }
+}
