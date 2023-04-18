9a373edb1b2fd1f44b2c7bc7392e8c7eb0d620db
jinny-l <jinny.l.dev@gmail.com>
2023-04-18 17:07:37 +0900

    🔧chore: MySQL로 DB 마이그레이션을 위한 환경 설정

--- src/main/resources/scheme.sql
+++ src/main/resources/scheme.sql
@@ -1,19 +1,19 @@
 CREATE TABLE IF NOT EXISTS users
 (
     sequence bigint NOT NULL AUTO_INCREMENT,
-    userId varchar(10),
-    password varchar(10),
-    name varchar(10),
-    email varchar(320),
-    primary key (userId)
+    userId varchar(10) NOT NULL,
+    password varchar(10) NOT NULL,
+    name varchar(10) NOT NULL,
+    email varchar(320) NOT NULL,
+    primary key (sequence, userId)
 );
 
 CREATE TABLE IF NOT EXISTS article
 (
     sequence bigint NOT NULL AUTO_INCREMENT,
-    writer varchar(10),
-    title varchar(10),
-    contents text(1000),
+    writer varchar(10) NOT NULL,
+    title varchar(10) NOT NULL,
+    contents text(1000) NOT NULL,
     PRIMARY KEY (sequence),
     FOREIGN KEY (writer)
     REFERENCES users (userId) ON UPDATE CASCADE
