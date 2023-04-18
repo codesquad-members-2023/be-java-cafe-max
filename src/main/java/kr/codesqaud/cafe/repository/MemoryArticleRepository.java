40d00cb65243c04194d3b9ed8d170ab8b6a0871e
n-aa-g <wlsdnjs0124@gmail.com>
2023-04-18 15:49:12 +0900

    Refactor(MemoryArticleRepository): 클래스 리팩토링
    MemeoryUserRepository와 동일한 기준으로 클래스를 리팩토링했습니다.
    변경 사항은 다음과 같습니다.
    - 필드 변수로 id 추가
    - findAll 메서드에서 unmodifiableList 변환
    - repository를 ConcurrentHashMap으로 변경

--- src/main/java/kr/codesqaud/cafe/repository/MemoryArticleRepository.java
+++ src/main/java/kr/codesqaud/cafe/repository/MemoryArticleRepository.java
@@ -3,25 +3,37 @@ package kr.codesqaud.cafe.repository;
 import kr.codesqaud.cafe.domain.Article;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 @Repository
 public class MemoryArticleRepository implements ArticleRepository{
-    List<Article> repository = new ArrayList<>();
+
+    private static Long id = 1L;
+    private final Map<Long, Article> repository = new ConcurrentHashMap();
 
     @Override
-    public void save(Article article) {
-        repository.add(0, article);
+    public void save(Article input) {
+        Article article = Article.builder()
+                .id(id)
+                .writer(input.getWriter())
+                .title(input.getTitle())
+                .contents(input.getContents())
+                .build();
+
+        id += 1;
+        repository.put(article.getId(), article);
     }
 
     @Override
     public List<Article> findAll() {
-        return repository;
+        return repository.values().stream().collect(Collectors.toUnmodifiableList());
     }
 
     @Override
-    public Article findById(int index) {
-        return repository.get(index);
+    public Article findById(long id) {
+        return repository.get(id);
     }
 }
