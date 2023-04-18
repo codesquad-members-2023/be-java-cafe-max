292c3840cc74d79de121f8dff74abddd012f147e
javavoja <javavoja@protonmail.com>
2023-04-18 17:54:30 +0900

    feat: 게시글 목록 조회 페이지 구현

--- src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
+++ src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
@@ -2,14 +2,18 @@ package kr.codesqaud.cafe.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
 
     @GetMapping("/question")
     public String introPageEqualToArticlePage(){
-
         return "/qna/form";
     }
 
+    @GetMapping("/articles")
+    public String showArticles(){
+        return "/qna/show";
+    }
 }
