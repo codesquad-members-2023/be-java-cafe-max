bdb303caf68855480e384881945ad5ec1f41a069
yhpark95 <98851575+yhpark95@users.noreply.github.com>
2023-04-18 20:46:02 +0900

    feat: comment Controller, Service 구현

---
+++ src/main/java/kr/codesqaud/cafe/comment/service/CommentService.java
@@ -0,0 +1,26 @@
+package kr.codesqaud.cafe.comment.service;
+
+import kr.codesqaud.cafe.comment.domain.Comment;
+import kr.codesqaud.cafe.comment.dto.RequestForm;
+import kr.codesqaud.cafe.comment.mapper.CommentDtoMapper;
+import kr.codesqaud.cafe.comment.repository.CommentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CommentService {
+    private final CommentRepository commentRepository;
+
+    public CommentService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    public void save(RequestForm requestForm, String userId) {
+        commentRepository.save(CommentDtoMapper.INSTANCE.toComment(requestForm, userId));
+    }
+
+    public List<Comment> getCommentsForArticle(long id){
+        return commentRepository.findByArticleId(id);
+    }
+}
