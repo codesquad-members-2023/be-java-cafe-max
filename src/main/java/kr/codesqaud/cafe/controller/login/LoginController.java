c6bbe2ea1047289cc8af0d773f2a7a2c4b81f520
sudago <sudago22@gmail.com>
2023-04-18 12:38:55 +0900

    feat: 로그인 하면 이전에 방문 시도한 페이지로 redirect

--- src/main/java/kr/codesqaud/cafe/controller/login/LoginController.java
+++ src/main/java/kr/codesqaud/cafe/controller/login/LoginController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -19,19 +20,21 @@ public class LoginController {
     }
 
     @GetMapping("/login")
-    public String login(){
+    public String login() {
         return "user/login";
     }
 
     @PostMapping("/login")
-    public String login(LoginForm loginForm, HttpSession session){
+    public String login(LoginForm loginForm,
+                        @RequestParam(required = false, defaultValue = "/", value = "redirectURL") String redirectURL,
+                        HttpSession session){
         User loginUser = loginService.login(loginForm.getUserId(), loginForm.getPassword());
         if (loginUser == null){
             return "user/login_failed";
         }
         // 세션 성공 시
         session.setAttribute("loginUser", loginUser);
-        return "redirect:/";
+        return "redirect:" + redirectURL;
     }
 
     @GetMapping("/logout")
