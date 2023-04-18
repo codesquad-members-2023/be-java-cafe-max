0b7a39d66eae2c6c2467380956eb882a3c91771c
sudago <sudago22@gmail.com>
2023-04-18 16:24:44 +0900

    fix: User 객체의 생성 역할 클래스 이동
    UserController에서 UserService로 이동
    ㅇ

--- src/main/java/kr/codesqaud/cafe/controller/user/UserForm.java
+++ src/main/java/kr/codesqaud/cafe/controller/user/UserForm.java
@@ -6,6 +6,15 @@ public class UserForm {
     private String name;
     private String email;
 
+    public UserForm(){}
+
+    public UserForm(String userId, String password, String name, String email) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
     public String getUserId() {
         return userId;
     }
