24a982d68f37c6bfb3f698c330c1c9ef27b1cb2c
yeonise <cerealnum@gmail.com>
2023-04-19 20:15:28 +0900

    fix : name column의 Unique 인덱스 제거

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS User (
   userId    VARCHAR(50) PRIMARY KEY,
   password  VARCHAR(50) NOT NULL,
-  name      VARCHAR(50) UNIQUE NOT NULL,
+  name      VARCHAR(50) NOT NULL,
   email     VARCHAR(50) NOT NULL
 );
 
