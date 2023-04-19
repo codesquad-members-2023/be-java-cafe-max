ec01741fff114e212c189dc50443a142d88a8866
JJONSOO <pjs970616@naver.com>
2023-04-19 10:38:04 +0900

    [feat] Reply 테이블 생성 및 loginId 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,26 @@
+
 CREATE TABLE IF NOT EXISTS Users (
-    id        VARCHAR(50) PRIMARY KEY,
+    id        BIGINT AUTO_INCREMENT PRIMARY KEY,
+    loginId VARCHAR(50) UNIQUE ,
     password  VARCHAR(50) NOT NULL,
-    name      VARCHAR(50) NOT NULL,
+    name      VARCHAR(50) NOT NULL UNIQUE ,
     email     VARCHAR(50) NOT NULL,
     dateTime TIMESTAMP NOT NULL
     );
 
 CREATE TABLE IF NOT EXISTS Article (
     id        BIGINT AUTO_INCREMENT PRIMARY KEY,
+    writer    VARCHAR (50) NOT NULL,
     title     VARCHAR(50) NOT NULL,
+    content  TEXT NOT NULL,
+    dateTime TIMESTAMP NOT NULL
+    );
+
+
+CREATE TABLE IF NOT EXISTS Reply (
+    id        BIGINT AUTO_INCREMENT PRIMARY KEY,
+    articleId BIGINT NOT NULL,
+    writer    VARCHAR (50) NOT NULL,
     content  TEXT NOT NULL,
     dateTime TIMESTAMP NOT NULL
     );
