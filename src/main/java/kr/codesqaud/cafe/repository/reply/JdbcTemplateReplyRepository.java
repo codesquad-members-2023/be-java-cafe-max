7e59947813c0c3fea0d71f35e4d8a987f3f83f27
Sully <2dcoder@naver.com>
2023-04-19 10:12:36 +0900

    feat: 댓글 기능을 담고 있는 레포지토리 구현
    정보 저장 기능, 고유 Id에 따라 데이터 찾는 기능, 고유 Id와 articleId를 Join으로 찾는 기능

---
+++ src/main/java/kr/codesqaud/cafe/repository/reply/JdbcTemplateReplyRepository.java
@@ -0,0 +1,61 @@
+package kr.codesqaud.cafe.repository.reply;
+
+import kr.codesqaud.cafe.domain.Reply;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Repository
+public class JdbcTemplateReplyRepository implements ReplyRepository {
+    private final NamedParameterJdbcTemplate template;
+    private final SimpleJdbcInsert simpleJdbcInsert;
+    private final RowMapper<Reply> replyRowMapper = BeanPropertyRowMapper.newInstance(Reply.class);
+
+    public JdbcTemplateReplyRepository(DataSource dataSource) {
+        this.template = new NamedParameterJdbcTemplate(dataSource);
+        this.simpleJdbcInsert = new SimpleJdbcInsert(dataSource)
+                .withTableName("REPLIES")
+                .usingGeneratedKeyColumns("id");
+    }
+
+    @Override
+    public Reply save(Reply reply) {
+        SqlParameterSource param = new BeanPropertySqlParameterSource(reply);
+        Number key = simpleJdbcInsert.executeAndReturnKey(param);
+        reply.setId(key.longValue());
+        return reply;
+    }
+
+    @Override
+    public Optional<Reply> findById(Long id) {
+        String sql = "select id, article_id, user_id, reply_content, reply_time from REPLIES where id = :id";
+
+        try {
+            Map<String, Object> param = Map.of("id", id);
+            Reply reply = template.queryForObject(sql, param, replyRowMapper);
+            return Optional.ofNullable(reply);
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public List<Reply> findByArticleId(Long articleId) {
+        String sql = "select r.id, r.user_id, u.userId, r.reply_content, r.reply_time " +
+                "from REPLIES r join USERS u on r.user_id = U.id " +
+                "where r.article_id=:articleId";
+
+            Map<String, Object> param = Map.of("articleId", articleId);
+            return template.query(sql, param, replyRowMapper);
+        }
+}
