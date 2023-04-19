e151fec4d9f72d60aeabd6bae0489295240d7d12
JJONSOO <pjs970616@naver.com>
2023-04-19 10:20:52 +0900

    [feat] 댓글을 추가 및 삭제할 수 있는 ReplyController 구현

---
+++ src/main/java/kr/codesqaud/cafe/domain/reply/controller/ReplyController.java
@@ -0,0 +1,46 @@
+package kr.codesqaud.cafe.domain.reply.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import kr.codesqaud.cafe.domain.reply.dto.request.ReplySaveRequestDto;
+import kr.codesqaud.cafe.domain.reply.repository.ReplyRepository;
+import kr.codesqaud.cafe.global.common.constant.SessionAttributeNames;
+
+@Controller
+public class ReplyController {
+
+	private final ReplyRepository replyRepository;
+
+	ReplyController(ReplyRepository replyRepository) {
+		this.replyRepository = replyRepository;
+	}
+
+	@PostMapping("/reply/{id}")
+	public String saveReply(@PathVariable("id") Long id, ReplySaveRequestDto replySaveRequestDto,
+		HttpSession httpSession) {
+		replyRepository.save(replySaveRequestDto.toEntity(id,
+			(String)httpSession.getAttribute(SessionAttributeNames.LOGIN_USER_NAME.type())));
+		return "redirect:/articles/{id}";
+	}
+
+	@DeleteMapping("/articles/{id}/replies/{replyId}")
+	public String deleteReply(@PathVariable("id") Long id, @PathVariable("replyId") String replyId,
+		HttpSession httpSession, Model model) {
+		System.out.println(replyId);
+		System.out.println(httpSession.getAttribute(SessionAttributeNames.LOGIN_USER_NAME.type()));
+		if (replyRepository.findById(replyId).get().getWriter() != httpSession.getAttribute(
+			SessionAttributeNames.LOGIN_USER_NAME.type())) {
+			model.addAttribute("error", "댓글작성자만  삭제할 수 있습니다.");
+			System.out.println("error!");
+			return "redirect:/articles/{id}";
+		}
+		replyRepository.delete(replyId);
+		return "redirect:/articles/{id}";
+	}
+}
