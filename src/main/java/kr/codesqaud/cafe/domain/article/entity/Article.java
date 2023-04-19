40e4f49a70747dec2055e7c37daa0414c92228a4
JJONSOO <pjs970616@naver.com>
2023-04-19 10:23:45 +0900

    [feat] 글 작성자 column 추가

--- src/main/java/kr/codesqaud/cafe/domain/article/entity/Article.java
+++ src/main/java/kr/codesqaud/cafe/domain/article/entity/Article.java
@@ -3,38 +3,20 @@ package kr.codesqaud.cafe.domain.article.entity;
 import java.time.LocalDateTime;
 
 public class Article {
-	Long id;
-	String content;
-	String title;
-	LocalDateTime dateTime;
+	private Long id;
+	private String content;
+	private String title;
+	private String writer;
+	private LocalDateTime dateTime;
 
-	public Article(String title, String content) {
+	public Article(String title, String content, String writer) {
 		this.title = title;
 		this.content = content;
+		this.writer = writer;
 		this.dateTime = LocalDateTime.now();
 	}
 
 	public Article() {
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
-
-	public void setDateTime(LocalDateTime dateTime) {
-		this.dateTime = dateTime;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
 	}
 
 	public Long getId() {
@@ -45,8 +27,36 @@ public class Article {
 		this.id = id;
 	}
 
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
 	public LocalDateTime getDateTime() {
 		return dateTime;
+	}
+
+	public void setDateTime(LocalDateTime dateTime) {
+		this.dateTime = dateTime;
 	}
 }
 
