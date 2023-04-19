5475cac8a1dbb81d7f8bd4a3c943392199343efa
pie <yejinh_h@naver.com>
2023-04-20 01:42:58 +0900

    refactor : 필요한 요소들 추가

--- src/main/java/kr/codesqaud/cafe/controller/dto/CommentDto.java
+++ src/main/java/kr/codesqaud/cafe/controller/dto/CommentDto.java
@@ -8,12 +8,14 @@ import kr.codesqaud.cafe.domain.Comment;
 public class CommentDto {
 	private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
+	private final Long id;
 	private final String userId;
 	private final String contents;
 	private final String createdAt;
 	private final Long articleId;
 
-	public CommentDto(String userId, String contents, LocalDateTime createdAt, Long articleId) {
+	private CommentDto(Long id, String userId, String contents, LocalDateTime createdAt, Long articleId) {
+		this.id = id;
 		this.userId = userId;
 		this.contents = contents;
 		this.createdAt = FORMATTER.format(createdAt);
@@ -22,6 +24,7 @@ public class CommentDto {
 
 	public static CommentDto fromEntity(Comment comment) {
 		return new CommentDto(
+			comment.getId(),
 			comment.getUserId(),
 			comment.getContents(),
 			comment.getCreatedAt(),
--- src/main/java/kr/codesqaud/cafe/controller/dto/CommentRequest.java
+++ src/main/java/kr/codesqaud/cafe/controller/dto/CommentRequest.java
@@ -1,10 +1,16 @@
 package kr.codesqaud.cafe.controller.dto;
 
 public class CommentRequest {
+	private final Long articleId;
 	private final String contents;
 
-	public CommentRequest(String contents) {
+	public CommentRequest(Long articleId, String contents) {
+		this.articleId = articleId;
 		this.contents = contents;
+	}
+
+	public Long getArticleId() {
+		return articleId;
 	}
 
 	public String getContents() {
