35f28f017449148ec6dab4f5db9e0b783fc65c0d
tmdqja0705@naver.com <tmdqja0705@naver.com>
2023-04-20 06:33:55 +0900

    loginForm DTO 구현

---
+++ src/main/java/kr/codesqaud/cafe/user/controller/loginForm.java
@@ -0,0 +1,22 @@
+package kr.codesqaud.cafe.user.controller;
+
+public class loginForm {
+    private String userId;
+    private String password;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
