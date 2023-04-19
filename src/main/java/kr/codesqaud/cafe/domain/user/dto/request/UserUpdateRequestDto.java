cfff09ea3b54cf2a63f7414d4b073f1ce147a9bb
JJONSOO <pjs970616@naver.com>
2023-04-19 10:39:59 +0900

    [fix] 변수 네이밍 수정 ( 변경할 닉네임 : changedName )

--- src/main/java/kr/codesqaud/cafe/domain/user/dto/request/UserUpdateRequestDto.java
+++ src/main/java/kr/codesqaud/cafe/domain/user/dto/request/UserUpdateRequestDto.java
@@ -5,24 +5,33 @@ import kr.codesqaud.cafe.domain.user.entity.User;
 public class UserUpdateRequestDto {
 
 	private String id;
-	private String name;
-	private String email;
 	private String password;
+	private String changedName;
+	private String changedEmail;
 
-	public UserUpdateRequestDto(String name, String email, String password) {
-		this.name = name;
-		this.email = email;
+	public UserUpdateRequestDto(String password, String changedName, String changedEmail) {
 		this.password = password;
+		this.changedName = changedName;
+		this.changedEmail = changedEmail;
 	}
 
-	public String getName() {
-		return name;
+	public User toEntity(String loginId) {
+		return new User(loginId, this.changedName, this.password, this.changedEmail);
 	}
 
-	public User toEntity(String id) {
-		User convertedUser = new User(id, name, password, email);
-		convertedUser.setId(id);
-		return convertedUser;
+	public String getId() {
+		return id;
 	}
 
+	public String getPassword() {
+		return password;
+	}
+
+	public String getChangedName() {
+		return changedName;
+	}
+
+	public String getChangedEmail() {
+		return changedEmail;
+	}
 }
