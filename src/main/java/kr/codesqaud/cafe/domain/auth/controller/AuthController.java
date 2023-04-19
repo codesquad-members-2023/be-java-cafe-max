d7c1a6a8991cbafc6fb13ecb515e6dc88cc807c6
JJONSOO <pjs970616@naver.com>
2023-04-19 10:32:04 +0900

    [fix] Custom 오류 처리

--- src/main/java/kr/codesqaud/cafe/domain/auth/controller/AuthController.java
+++ src/main/java/kr/codesqaud/cafe/domain/auth/controller/AuthController.java
@@ -1,17 +1,18 @@
 package kr.codesqaud.cafe.domain.auth.controller;
 
-import java.util.NoSuchElementException;
 import java.util.Objects;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import kr.codesqaud.cafe.domain.auth.dto.request.AuthLoginRequestDto;
 import kr.codesqaud.cafe.domain.user.entity.User;
 import kr.codesqaud.cafe.domain.user.repository.UserRepository;
 import kr.codesqaud.cafe.global.common.constant.SessionAttributeNames;
+import kr.codesqaud.cafe.global.exception.user.InvalidLoginInfoException;
 
 @Controller
 public class AuthController {
@@ -24,21 +25,21 @@ public class AuthController {
 
 	@PostMapping("/login")
 	public String login(AuthLoginRequestDto authLoginRequestDto, HttpSession session) {
-		User loginUser = userRepository.findById(authLoginRequestDto.getId())
-			.filter(user -> user.isCorrectPassword(authLoginRequestDto.getPassword()))
-			.orElseThrow(NoSuchElementException::new);
-		session.setAttribute(SessionAttributeNames.LOGIN_USER_ID, loginUser.getId());
-		session.setAttribute(SessionAttributeNames.LOGIN_USER_NAME, loginUser.getName());
+		User loginUser = userRepository.findById(authLoginRequestDto.getLoginId())
+			.filter(user -> user.matchPassword(authLoginRequestDto.getPassword()))
+			.orElseThrow(InvalidLoginInfoException::new);
+		session.setAttribute(SessionAttributeNames.LOGIN_USER_ID.type(), loginUser.getLoginId());
+		session.setAttribute(SessionAttributeNames.LOGIN_USER_NAME.type(), loginUser.getName());
 		return "redirect:/";
 	}
 
-	@PostMapping("/logout")
+	@GetMapping("/logout")
 	public String logout(HttpSession session) {
 		try {
 			Objects.requireNonNull(session).invalidate();
 		} catch (NullPointerException e) {
 			throw new RuntimeException(e);
 		}
-		return "rediretc:/";
+		return "redirect:/";
 	}
 }
