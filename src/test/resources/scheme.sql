edb996ad6d186df3f99f6eed105b10d86a88deb7
jinny-l <jinny.l.dev@gmail.com>
2023-04-20 14:40:23 +0900

    🔧test: 테스트 환경을 H2에서 MySQL로 마이그레이션

--- src/test/resources/scheme.sql
+++ src/test/resources/scheme.sql
@@ -1,22 +1,40 @@
-DROP TABLE IF EXISTS users;
-CREATE table users
-(
-    sequence bigint NOT NULL AUTO_INCREMENT,
-    userId varchar(10),
-    password varchar(10),
-    name varchar(10),
-    email varchar(320),
-    primary key (userId)
-);
-
+DROP TABLE IF EXISTS reply;
 DROP TABLE IF EXISTS article;
-CREATE table article
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users
 (
     sequence bigint NOT NULL AUTO_INCREMENT,
-    writer varchar(10),
-    title varchar(10),
-    contents text(1000),
-    PRIMARY KEY (sequence),
+    userId varchar(10) NOT NULL,
+    password varchar(10) NOT NULL,
+    name varchar(10) NOT NULL,
+    email varchar(320) NOT NULL,
+    primary key (sequence, userId)
+    );
+
+ALTER TABLE users ADD INDEX(userId);
+
+CREATE TABLE article
+(
+    id bigint NOT NULL AUTO_INCREMENT,
+    writer varchar(10) NOT NULL,
+    title varchar(10) NOT NULL,
+    contents text(1000) NOT NULL,
+    PRIMARY KEY (id),
     FOREIGN KEY (writer)
     REFERENCES users (userId) ON UPDATE CASCADE
-);
+    );
+
+CREATE TABLE reply
+(
+    id bigint NOT NULL AUTO_INCREMENT,
+    article_id bigint NOT NULL,
+    users_id varchar(10) NOT NULL,
+    contents text(1000) NOT NULL,
+    create_dateTime DATETIME DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    FOREIGN KEY (article_id)
+    REFERENCES article (id) ON UPDATE CASCADE,
+    FOREIGN KEY (users_id)
+    REFERENCES users (userId) ON UPDATE CASCADE
+    );
