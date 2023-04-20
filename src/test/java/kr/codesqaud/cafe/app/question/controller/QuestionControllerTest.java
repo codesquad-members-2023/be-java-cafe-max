c81a8a003a2b79fbb282666690ace24699253d2f
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-04-20 11:04:35 +0900

    test: 회원정보 수정 성공 테스트 추가

--- src/test/java/kr/codesqaud/cafe/app/question/controller/QuestionControllerTest.java
+++ src/test/java/kr/codesqaud/cafe/app/question/controller/QuestionControllerTest.java
@@ -3,6 +3,7 @@ package kr.codesqaud.cafe.app.question.controller;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -16,15 +17,17 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
+import kr.codesqaud.cafe.app.question.controller.dto.QuestionResponse;
 import kr.codesqaud.cafe.app.question.controller.dto.QuestionSavedRequest;
-import kr.codesqaud.cafe.app.question.repository.QuestionRepository;
+import kr.codesqaud.cafe.app.question.service.QuestionService;
+import kr.codesqaud.cafe.app.user.controller.dto.UserLoginRequest;
 import kr.codesqaud.cafe.app.user.controller.dto.UserSavedRequest;
 import kr.codesqaud.cafe.app.user.entity.User;
-import kr.codesqaud.cafe.app.user.repository.UserRepository;
+import kr.codesqaud.cafe.app.user.service.UserService;
 import kr.codesqaud.cafe.errors.errorcode.CommonErrorCode;
 import kr.codesqaud.cafe.errors.response.ErrorResponse;
 import kr.codesqaud.cafe.errors.response.ErrorResponse.ValidationError;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -33,51 +36,65 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
+@Transactional
 @AutoConfigureMockMvc
+@SpringBootTest
 class QuestionControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
-    private QuestionRepository repository;
+    private QuestionService questionService;
 
     @Autowired
-    private UserRepository userRepository;
-
+    private UserService userService;
+    @Autowired
     private ObjectMapper objectMapper;
+
+    private MockHttpSession httpSession;
+
+    private Long userId;
+
+    private Long questionId;
 
     @BeforeEach
     public void setup() {
-        objectMapper = new ObjectMapper().registerModule(new JavaTimeModule());
+        objectMapper.registerModule(new JavaTimeModule());
+        httpSession = new MockHttpSession();
+        userId = signUp();
+        questionId = writeQuestion();
     }
 
-    @BeforeEach
-    public void createSampleUser() throws Exception {
+    public Long signUp() {
         String userId = "yonghwan1107";
         String password = "yonghwan1107";
         String name = "김용환";
         String email = "yonghwan1107@gmail.com";
-        String url = "/users";
-        UserSavedRequest dto = new UserSavedRequest(userId, password, name, email);
-        mockMvc.perform(post(url).contentType(MediaType.APPLICATION_JSON).content(toJSON(dto)));
+        return userService.signUp(new UserSavedRequest(userId, password, name, email)).getId();
     }
 
-    @AfterEach
-    public void clean() {
-        repository.deleteAll();
-        userRepository.deleteAll();
+    public Long writeQuestion() {
+        User user = userService.findUser("yonghwan1107");
+        String title = "제목1";
+        String content = "내용1";
+        LocalDateTime writeDate = LocalDateTime.now();
+        QuestionSavedRequest dto = new QuestionSavedRequest(title, content, writeDate,
+            user.getUserId());
+        return questionService.write(dto.toEntity(user.getId())).getId();
     }
 
     @Test
     @DisplayName("회원 객체와 제목, 내용이 주어지고 글쓰기 요청시 글쓰기가 되는지 테스트")
     public void write_success() throws Exception {
         //given
+        login("yonghwan1107", "yonghwan1107");
         String userId = "yonghwan1107";
-        User user = userRepository.findByUserId(userId).orElseThrow();
+        User user = userService.findUser(userId);
         String writer = user.getName();
         String title = "제목1";
         String content = "내용1";
@@ -88,7 +105,8 @@ class QuestionControllerTest {
         String jsonArticle =
             mockMvc.perform(post(url)
                     .contentType(MediaType.APPLICATION_JSON)
-                    .content(toJSON(dto)))
+                    .content(toJSON(dto))
+                    .session(httpSession))
                 .andExpect(status().isOk())
                 .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
         //then
@@ -104,17 +122,20 @@ class QuestionControllerTest {
     @DisplayName("부적절한 입력 형식의 제목이 주어지고 글쓰기 요청시 에러 응답을 받는지 테스트")
     public void write_fail1() throws Exception {
         //given
+        login("yonghwan1107", "yonghwan1107");
+        User user = userService.findUser(userId);
         String title = "";
         String content = "내용1";
         LocalDateTime writeDate = LocalDateTime.now();
-        String userId = "user1";
+        String userId = user.getUserId();
         QuestionSavedRequest dto = new QuestionSavedRequest(title, content, writeDate, userId);
         String url = "/qna";
         //when
         String jsonErrors =
             mockMvc.perform(post(url)
                     .contentType(MediaType.APPLICATION_JSON)
-                    .content(toJSON(dto)))
+                    .content(toJSON(dto))
+                    .session(httpSession))
                 .andExpect(status().isBadRequest()).andReturn().getResponse()
                 .getContentAsString(StandardCharsets.UTF_8);
         //then
@@ -134,10 +155,71 @@ class QuestionControllerTest {
     @DisplayName("비 로그인 상태에서 글쓰기 페이지 진입시 로그인 페이지로 리다이렉트 되는지 테스트")
     public void form_fail() throws Exception {
         //given
-        String url = "/qna/form";
+        String url = "/qna/new";
         //when & then
         mockMvc.perform(get(url)).andExpect(status().is3xxRedirection())
-            .andExpect(redirectedUrl("/user/login"));
+            .andExpect(redirectedUrl("/login"));
+    }
+
+    @Test
+    @DisplayName("로그인한 회원이 특정 질문 조회 페이지로 접근했을때 페이지로 리다이렉션 되는지 테스트")
+    public void detail_success() throws Exception {
+        //given
+        login("yonghwan1107", "yonghwan1107");
+        String url = "/qna/" + questionId;
+        //when
+        QuestionResponse question = (QuestionResponse) Objects.requireNonNull(
+            mockMvc.perform(get(url)
+                    .session(httpSession))
+                .andExpect(status().isOk())
+                .andReturn().getModelAndView()).getModelMap().get("question");
+        //then
+        assertThat(question.getTitle()).isEqualTo("제목1");
+        assertThat(question.getContent()).isEqualTo("내용1");
+        assertThat(question.getWriter()).isEqualTo("김용환");
+    }
+
+    @Test
+    @DisplayName("비 로그인 상태에서 특정 게시물을 접속하려고 할때 로그인 페이지로 이동되는지 테스트")
+    public void detail_fail1() throws Exception {
+        //given
+        String url = "/qna/1";
+        //when & then
+        mockMvc.perform(get(url))
+            .andExpect(redirectedUrl("/login"));
+    }
+
+    @Test
+    @DisplayName("수정된 제목과 내용이 주어질때 질문 게시글 수정 요청시 수정이 되는지 테스트")
+    public void modify_success() throws Exception {
+        //given
+        login("yonghwan1107", "yonghwan1107");
+        String str_userId = userService.findUser(userId).getUserId();
+        String modifiedTitle = "변경된 제목1";
+        String modifiedContent = "변경된 내용1";
+        QuestionSavedRequest dto = new QuestionSavedRequest(modifiedTitle,
+            modifiedContent, LocalDateTime.now(), str_userId);
+        String url = "/qna/" + questionId;
+        //when
+        String json = mockMvc.perform(put(url)
+                .content(toJSON(dto))
+                .contentType(MediaType.APPLICATION_JSON)
+                .session(httpSession))
+            .andExpect(status().isOk())
+            .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+        //then
+        TypeReference<HashMap<String, Object>> typeReference = new TypeReference<>() {
+        };
+        HashMap<String, Object> questionMap = objectMapper.readValue(json, typeReference);
+        assertThat(questionMap.get("title")).isEqualTo("변경된 제목1");
+        assertThat(questionMap.get("content")).isEqualTo("변경된 내용1");
+    }
+
+    private void login(String userId, String password) throws Exception {
+        mockMvc.perform(post("/login")
+            .content(toJSON(new UserLoginRequest(userId, password)))
+            .contentType(MediaType.APPLICATION_JSON)
+            .session(httpSession));
     }
 
     private <T> String toJSON(T data) throws JsonProcessingException {
