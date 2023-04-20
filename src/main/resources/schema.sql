640fc2336bc1ab50a47727637a59e3342ceafec4
chung <chunghye1998>
2023-04-20 14:39:16 +0900

    feat: db에 reply 테이블 생성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -20,3 +20,15 @@ CREATE TABLE article
     PRIMARY KEY (id),
     FOREIGN KEY (writer_id) REFERENCES users (id) ON UPDATE CASCADE
 );
+
+CREATE TABLE reply
+(
+    id bigint auto_increment,
+    contents text not null,
+    createdAt datetime,
+    user_id varchar(200) not null,
+    article_id varchar(200) not null,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON UPDATE CASCADE,
+    FOREIGN KEY (article_id) REFERENCES article (id) ON UPDATE CASCADE
+);
