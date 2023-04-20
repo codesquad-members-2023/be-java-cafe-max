ce0dbd994641a8684fe7c6e3d5fe910291cef03c
othertkfka <57559288+othertkfka@users.noreply.github.com>
2023-04-20 16:44:15 +0900

    feat: Comment 도메인 객체와 DTO 생성
    Entity로 사용하는 Comment와 댓글 작성시 요청으로 받는 dto인 CommentWriteForm을 생성했습니다.

---
+++ src/main/java/kr/codesqaud/cafe/board/domain/Comment.java
@@ -0,0 +1,80 @@
+package kr.codesqaud.cafe.board.domain;
+
+import java.time.LocalDateTime;
+
+public class Comment {
+    private final Long commentId;
+    private final Long postId;
+    private final String writer;
+    private final String contents;
+    private final LocalDateTime writeDateTime;
+
+    public Comment(Builder builder) {
+        this.commentId = builder.commentId;
+        this.postId = builder.postId;
+        this.writer = builder.writer;
+        this.contents = builder.contents;
+        this.writeDateTime = builder.writeDateTime;
+    }
+
+    public Long getCommentId() {
+        return commentId;
+    }
+
+    public Long getPostId() {
+        return postId;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public LocalDateTime getWriteDateTime() {
+        return writeDateTime;
+    }
+
+    public static class Builder {
+        private Long commentId;
+        private Long postId;
+        private String writer;
+        private String contents;
+        private LocalDateTime writeDateTime;
+
+        public Builder() {
+
+        }
+
+        public Builder commentId(Long commentId) {
+            this.commentId = commentId;
+            return this;
+        }
+
+        public Builder postId(Long postId) {
+            this.postId = postId;
+            return this;
+        }
+
+        public Builder writer(String writer) {
+            this.writer = writer;
+            return this;
+        }
+
+        public Builder contents(String contents) {
+            this.contents = contents;
+            return this;
+        }
+
+        public Builder writeDateTime(LocalDateTime writeDateTime) {
+            this.writeDateTime = writeDateTime;
+            return this;
+        }
+
+        public Comment build() {
+            return new Comment(this);
+        }
+    }
+}
