a232efb8cb7b07b3bec18b7c307693652c664dc8
othertkfka <57559288+othertkfka@users.noreply.github.com>
2023-04-20 12:16:11 +0900

    refactor: schema.sql 수정
    post 테이블에 FK를 추가했습니다.
    comment 테이블을 생성했습니다.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,12 @@
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
+    user_id VARCHAR,
+    password VARCHAR NOT NULL,
+    user_name VARCHAR NOT NULL UNIQUE,
+    email VARCHAR NOT NULL,
+    PRIMARY KEY (user_id)
+);
+
 DROP TABLE IF EXISTS post;
 CREATE TABLE post(
     post_id BIGINT AUTO_INCREMENT,
@@ -5,14 +14,18 @@ CREATE TABLE post(
     title VARCHAR(255) NOT NULL,
     contents VARCHAR(255) NOT NULL,
     write_date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (post_id)
+    PRIMARY KEY (post_id),
+    FOREIGN KEY (writer) REFERENCES users(user_name)
 );
 
-DROP TABLE IF EXISTS users;
-CREATE TABLE users(
-    user_id VARCHAR,
-    password VARCHAR NOT NULL,
-    user_name VARCHAR NOT NULL,
-    email VARCHAR NOT NULL,
-    PRIMARY KEY (user_id)
+DROP TABLE IF EXISTS comment;
+CREATE TABLE comment(
+    comment_id BIGINT AUTO_INCREMENT,
+    post_id BIGINT NOT NULL,
+    writer VARCHAR(50) NOT NULL,
+    contents VARCHAR(255) NOT NULL,
+    write_date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (comment_id),
+    FOREIGN KEY (post_id) REFERENCES post(post_id),
+    FOREIGN KEY (writer) REFERENCES users(user_name)
 );
