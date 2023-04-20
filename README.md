bd551a0c09db4581008917d8a5613c2b43aedf79
javavoja <javavoja@protonmail.com>
2023-04-20 17:57:51 +0900

    feature2: fix: MvcConfig 문제 해결
    미루려고 했는데 다음 단계에 바로 스포일러가 나오는 듯해 어쩔 수 없이 바로 해결..

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-
 # be-java-cafe
 마스터즈 2023 스프링 카페<br>4차 리뷰 대비 다시 작성
 
@@ -6,12 +5,12 @@
 - 수단과 방법을 가리지 말고 진도 따라잡기....!!!!!!  :fire::fire:
 > [ㅇ] 2단계 -> 이번주 목표는 2단계까지<br>
 > [] 2단계 선택<회원정보 수정하기> -> (미루기)<br>
-> [] 시간 남으면 3단계 직행<br>
-> WebMVCConfigurer 적용 -> (미루기)
+> [] 3단계 직행<br>
+> [] WebMVCConfigurer 적용
 
 ### 경과
-- 2단계: 통과
-- 2단계-선택미션: 하려다가 뒤로 미루기
+- 2단계: 완료
+- 2단계-선택미션: 하려다가 미루고 3단계 시작
 - 3단계 시작(목요일)
 
 #### TODO 리스트
@@ -20,9 +19,8 @@
 - [] Get과 POST의 차이 + query parameter + MESSEAGE BODY
 - [] @ComponentScan 알아보기 -> (@Component + @Bean까지...)
 - [] @RequestBody
-- [] @Repository -> @Service -> @Controller 관계(매우 명쾌하면 좋겠다)
+- [] @Repository -> @Service -> @Controller 관계(매우 명쾌하면 좋겠다!)
 - [] Ioc Container
-
 
 #### <학습 주제 목록>
 - bean:
@@ -34,8 +32,8 @@
 - Ioc 컨테이너:
 - @ComponentScan:
 - @Repository -> @Service -> @Controller (흐름)
-- @Configuration:     (새로움)
+- @Configuration:  -> '스프링 MVC 자동구성 설정' java파일 만드는데 사용(?)
 - @ModelAttribute:
-- long과 Long(wrapper 클래스)
-- 자료구조 선택
-- WebMVCConfigurer
+- wrapper 클래스!: '원시 타입 자체를 객체로 만들어주는' 것인데(아직 정확히 이해하지 못함 -> 몇 번 예제로 연습을...)
+- 자료구조 선택기준!: 성능 스펙이 다 있으므로 생각을 해서 선택(!)
+- WebMVCConfigurer(인터페이스): ~를 구현한 클래스로 ....
--- src/main/java/kr/codesqaud/cafe/config/MvcConfig.java
+++ src/main/java/kr/codesqaud/cafe/config/MvcConfig.java
@@ -3,17 +3,16 @@ package kr.codesqaud.cafe.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 //요구사항: 아래의 Adapter가 deprecated되었는데 이유와 해결책에 대해 생각해 보고 해결하자....!!!
-public class MvcConfig extends WebMvcConfigurerAdapter {
+//  ===> 쓰임새가 사라진 WebMvcConfigurerAdapter 클래스는 스프링 부트 2.0에서 제외(Deprecated)되었다. -> (....!!!!!??)
+public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry){
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);  // ( = Integer.MIN_VALUE  ....)
-
-        registry.addViewController("/users/form").setViewName("user/form");
-        registry.addViewController("/users/login").setViewName("user/login");
-        registry.addViewController("/questions/form").setViewName("qna/form");
+//        registry.addViewController("/questions/form").setViewName("qna/form");
+//        일종의 GetMapping 구현
     }
 }
