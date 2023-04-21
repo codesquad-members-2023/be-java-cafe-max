d93d529a597b0c113c46bb3211dbce9fa767ee66
ALbertIM0427 <guang7226961@naver.com>
2023-04-21 09:40:54 +0900

    feat: default 게시글 추가

--- src/main/java/kr/codesquad/cafe/global/AppStartupRunner.java
+++ src/main/java/kr/codesquad/cafe/global/AppStartupRunner.java
@@ -10,12 +10,22 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.LocalDateTime;
 
 @Profile("cloud")
 @Component
 public class AppStartupRunner implements CommandLineRunner {
 
+    private static final String ADMIN = "admin";
+    private static final String PASSWORD = "W5p8Dbc3f7sOjqaRc7WVp8inlukLB3LJ";
+    private static final String EMAIL = "R9gvxFpZK6xYikAdPrdy1mDtd3O+ag2L";
+    private static final String TEST_TITLE = "명언";
+    private static final int START_NUMBER = 0;
+    private static final String CSV_FILE_NAME = "/goodword.csv";
+    private static final String USER_DIR = "user.dir";
     private final UserRepository userRepository;
     private final StringEncryptor encryptor;
 
@@ -30,22 +40,32 @@ public class AppStartupRunner implements CommandLineRunner {
     @Override
     public void run(String... args) {
         User manager = new User.Builder()
-                .password("W5p8Dbc3f7sOjqaRc7WVp8inlukLB3LJ")
-                .email(encryptor.decrypt("R9gvxFpZK6xYikAdPrdy1mDtd3O+ag2L"))
-                .nickname("admin")
+                .password(PASSWORD)
+                .email(encryptor.decrypt(EMAIL))
+                .nickname(ADMIN)
                 .role(Role.MANAGER).build();
         User savedManager = userRepository.save(manager);
 
 
-        for (int i = 0; i < 300; i++) {
-            Post test = new Post.Builder()
-                    .createdDateTime(LocalDateTime.now())
-                    .title("test" + i)
-                    .user(savedManager)
-                    .textContent("testContent"+i)
-                    .nickname(manager.getNickname())
-                    .build();
-            postRepository.save(test);
+        String path = System.getProperty(USER_DIR) + CSV_FILE_NAME;
+        System.out.println(path);
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line;
+            int index = START_NUMBER;
+            while ((line = br.readLine()) != null) {
+                Post test = new Post.Builder()
+                        .createdDateTime(LocalDateTime.now())
+                        .title(TEST_TITLE + (++index))
+                        .user(savedManager)
+                        .textContent(line)
+                        .nickname(manager.getNickname())
+                        .build();
+                postRepository.save(test);
+
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
