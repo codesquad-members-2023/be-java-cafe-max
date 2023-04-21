54895c166bf19a24677afa18b1543951e62a50e3
sudago <sudago22@gmail.com>
2023-04-21 16:18:27 +0900

    feat: sql 문에 reply 테이블 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,14 @@
-INSERT INTO users(userId, password, name, email)
-VALUES ('jianId', '1234', 'jian', 'jian@gmail.com');
-INSERT INTO users(userId, password, name, email)
-VALUES ('yukiId', '1234', 'yuki', 'yuki@gmail.com');
-
-INSERT INTO articles(writer, title, contents, createdAt, points)
+INSERT INTO article(writer, title, contents, created_at, points)
 VALUES ('jianId', '안뇽! 첫 게시글 이에요!', '헤헤 게시글 성공!', '20230413', '2');
-INSERT INTO articles(writer, title, contents, createdAt, points)
+INSERT INTO article(writer, title, contents, created_at, points)
 VALUES ('yukiId', '나능!! 유키당!!', '에오🐱', '20230413', '30');
+
+INSERT INTO reply(writer, contents, created_at, article_id)
+VALUES ('jian', '댓글도 성공!', '20230413', '1');
+INSERT INTO article(writer, contents, created_at, article_id)
+VALUES ('yuki', '🐟냠냠', '20230413', '2');
+
+INSERT INTO `user`(user_id, password, name, email)
+VALUES ('jianId', '1234', 'jian', 'jian@gmail.com');
+INSERT INTO `user`(user_id, password, name, email)
+VALUES ('yukiId', '1234', 'yuki', 'yuki@gmail.com');
