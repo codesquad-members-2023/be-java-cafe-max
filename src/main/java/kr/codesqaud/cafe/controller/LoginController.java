51ad8032454a3c647da9a087b2bd594d0c84cfa6
O_jeegu <lumirite0@gmail.com>
2023-04-21 17:31:48 +0900

    refactor : 필요 없는 메서드 삭제
    세션 생성만으로도 충분하므로 쿠키 생성하고 삭제하는 메서드를 삭제함

--- src/main/java/kr/codesqaud/cafe/controller/LoginController.java
+++ src/main/java/kr/codesqaud/cafe/controller/LoginController.java
@@ -4,13 +4,10 @@ import kr.codesqaud.cafe.controller.dto.LoginDTO;
 import kr.codesqaud.cafe.service.LoginService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -22,15 +19,10 @@ public class LoginController {
         this.loginService = loginService;
     }
 
-    @GetMapping("/login")
-    public String loginForm() {
-        return "login/form";
-    }
-
     //todo : MvcConfigure 활성화 하려면 get, post url 달라야 함. 변경하기
-    @PostMapping("/login")
-    public String login(@ModelAttribute final LoginDTO loginDTO, final Model model, HttpServletRequest request, HttpServletResponse response) {
-        boolean isLoginFailed = !loginService.checkIdAndPw(loginDTO);
+    @PostMapping("/login-try")
+    public String login(@ModelAttribute final LoginDTO loginDTO, final Model model, HttpServletRequest request) {
+        boolean isLoginFailed = !loginService.checkLoginUser(loginDTO);
         if (isLoginFailed) {
             model.addAttribute("loginFailed", true);
             return "login/form";
@@ -42,28 +34,17 @@ public class LoginController {
         HttpSession session = request.getSession();
         session.setAttribute("loginUser", loginUser);
 
-        //쿠키 생성. 시간 정보 설정 생략 시 세션 쿠키(브라우저 종료 시 삭제)
-        Cookie idCookie = new Cookie("idCookie", String.valueOf(loginUser.getId()));
-        response.addCookie(idCookie);
-
         return "redirect:/";
     }
 
-    @PostMapping("/logout")
-    public String logout(HttpServletRequest request, HttpServletResponse response) {
+    @PostMapping("/logout-try")
+    public String logout(HttpServletRequest request) {
         HttpSession session = request.getSession(false); // 현재 세션이 없으면 null 반환
         if (session != null) {
             session.invalidate(); // 현재 세션을 무효화
         }
-        expireCookie(response, "id");
 
         return "redirect:/";
     }
 
-    //todo : 로그아웃 해도 application - cookie 안 사라짐
-    private void expireCookie(HttpServletResponse response, String cookieName) {
-        Cookie cookie = new Cookie(cookieName, null);
-        cookie.setMaxAge(0);
-        response.addCookie(cookie);
-    }
 }
