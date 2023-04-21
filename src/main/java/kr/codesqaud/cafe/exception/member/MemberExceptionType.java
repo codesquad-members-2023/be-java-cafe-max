ad7e1c5aed60fc554e5a1e9db9c899e6172c2115
chacha <peony@bluehole.net>
2023-04-22 01:38:00 +0900

    refactor 잘못된 정보일때 관련 에러 추가

--- src/main/java/kr/codesqaud/cafe/exception/member/MemberExceptionType.java
+++ src/main/java/kr/codesqaud/cafe/exception/member/MemberExceptionType.java
@@ -1,6 +1,7 @@
 package kr.codesqaud.cafe.exception.member;
 
 public enum MemberExceptionType {
+    NOT_FOUND("error","잘못된 정보입니다."),
     NOT_FOUND_USER("error", "해당 유저를 찾을 수 없습니다."),
     DO_NOT_HAVE_ACCESS("error", "접근 권한이 없습니다."),
     DUPLICATED_EMAIL("error-email", "이미 존재하는 이메일입니다."),
