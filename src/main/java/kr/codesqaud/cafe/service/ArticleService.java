c9ee1376344c11294d305d0a782e31e4bfa00273
takealook <takealook97@naver.com>
2023-04-21 17:54:23 +0900

    서비스 클래스에 세션에서 id값을 가져올 수 있도록 메서드 추가. 추가로 articleId를 통한 수정과 삭제기능을 하는 메서드 생성

--- src/main/java/kr/codesqaud/cafe/service/ArticleService.java
+++ src/main/java/kr/codesqaud/cafe/service/ArticleService.java
@@ -1,11 +1,14 @@
 package kr.codesqaud.cafe.service;
 
 import kr.codesqaud.cafe.controller.dto.ArticleDto;
+import kr.codesqaud.cafe.controller.dto.request.PostEditRequest;
 import kr.codesqaud.cafe.controller.dto.request.PostRequest;
 import kr.codesqaud.cafe.domain.Article;
 import kr.codesqaud.cafe.repository.ArticleRepository;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,8 +20,8 @@ public class ArticleService {
         this.articleRepository = articleRepository;
     }
 
-    public void post(PostRequest request) {
-        Article article = Article.from(request);
+    public void post(PostRequest postRequest, HttpServletRequest httpRequest) {
+        Article article = Article.from(getUserIdFromSession(httpRequest), postRequest);
         articleRepository.save(article);
     }
 
@@ -33,5 +36,23 @@ public class ArticleService {
         return articleRepository.findById(id)
                 .map(ArticleDto::from)
                 .orElseThrow(() -> new RuntimeException("Article not found"));
+    }
+
+    public String getUserIdFromSession(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            return (String) session.getAttribute("userId");
+        }
+        return null;
+    }
+
+    public void deleteArticle(Long articleId){
+        articleRepository.delete(articleId);
+    }
+
+    public void editArticle(final Long articleId, final PostEditRequest request){
+        Article savedArticle = articleRepository.findById(articleId).orElseThrow(() -> new RuntimeException("Article not found"));
+        savedArticle.editArticle(request.getNewTitle(), request.getNewContent());
+        articleRepository.update(savedArticle);
     }
 }
