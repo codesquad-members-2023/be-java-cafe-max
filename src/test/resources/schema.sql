13cb3325505723abb3c538bdf3a55010901bcc29
chacha <peony@bluehole.net>
2023-04-22 01:31:41 +0900

    refactor main resource복사해서 test로 이동

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,23 +1,24 @@
 
+
 DROP TABLE IF EXISTS post;
 DROP TABLE IF EXISTS member;
 
 CREATE TABLE member (
-                        memberId BIGINT AUTO_INCREMENT,
-                        email VARCHAR(100) NOT NULL UNIQUE,
-                        password VARCHAR(100) NOT NULL,
-                        nickName VARCHAR(100),
-                        create_date TIMESTAMP NOT NULL,
-                        PRIMARY KEY (memberId)
+memberId BIGINT AUTO_INCREMENT,
+email VARCHAR(100) NOT NULL UNIQUE,
+password VARCHAR(100) NOT NULL,
+nickname VARCHAR(100),
+create_date TIMESTAMP NOT NULL,
+PRIMARY KEY (memberId)
 );
 
 CREATE TABLE post (
-                      postId BIGINT NOT NULL AUTO_INCREMENT,
-                      title VARCHAR(255) NOT NULL,
-                      content TEXT NOT NULL,
-                      writerEmail  VARCHAR(100)  NOT NULL,
-                      write_date TIMESTAMP NOT NULL,
-                      views BIGINT DEFAULT 0,
-                      PRIMARY KEY (postId),
-                      FOREIGN KEY (writerEmail) REFERENCES member (email) ON DELETE CASCADE ON UPDATE CASCADE
+postId BIGINT NOT NULL AUTO_INCREMENT,
+title VARCHAR(255) NOT NULL,
+content TEXT NOT NULL,
+writerEmail  VARCHAR(100) NOT NULL,
+write_date TIMESTAMP NOT NULL,
+views BIGINT DEFAULT 0,
+PRIMARY KEY (postId),
+FOREIGN KEY (writerEmail) REFERENCES member (email) ON DELETE CASCADE ON UPDATE CASCADE
 );
