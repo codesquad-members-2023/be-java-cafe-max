66b93ed176a12233b1a913a241d009006fa053f0
O_jeegu <lumirite0@gmail.com>
2023-04-21 17:29:24 +0900

    refactor : 메서드 간결화
    cehckLoginUser 메서드 사용 시 User class에 추가된 matchPassword 메서드를 이용하여 코드 간결화

--- src/main/java/kr/codesqaud/cafe/service/LoginService.java
+++ src/main/java/kr/codesqaud/cafe/service/LoginService.java
@@ -16,24 +16,18 @@ public class LoginService {
         this.userRepository = userRepository;
     }
 
-    public boolean checkIdAndPw(LoginDTO loginDTO) {
+//    public boolean checkLoginUser(LoginDTO loginDTO) {
+//        Optional<User> loginUser = userRepository.findByUserId(loginDTO.getUserId());
+//        return loginUser.isPresent() && loginUser.get().getPassword().equals(loginDTO.getPassword());
+//    }
+
+    public boolean checkLoginUser(LoginDTO loginDTO) {
         Optional<User> loginUser = userRepository.findByUserId(loginDTO.getUserId());
-        return loginUser.isPresent() && loginUser.get().getPassword().equals(loginDTO.getPassword());
+        return loginUser.isPresent() && loginUser.get().matchPassword(loginDTO.getPassword());
     }
 
     public LoginDTO findId(final String userId) {
         Optional<User> wantedId = userRepository.findByUserId(userId);
         return wantedId.map(LoginDTO::from).orElse(null);
     }
-
-
-//    public boolean authenticate(HttpServletRequest request) {
-//        //session 있을 때만 생성 없을땐 생성x
-//        HttpSession session = request.getSession(false);
-//        if(session != null) {
-//            return session.getAttribute("id") != null;
-//        }
-//        return false;
-//    }
-
 }
