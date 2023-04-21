b99e1690c925edabad11c4c81f8d178bbd90a684
yhpark95 <98851575+yhpark95@users.noreply.github.com>
2023-04-21 11:27:08 +0900

    feat: 댓글 삭제 ajax로 구현

--- src/main/java/kr/codesqaud/cafe/comment/controller/CommentController.java
+++ src/main/java/kr/codesqaud/cafe/comment/controller/CommentController.java
@@ -1,14 +1,18 @@
 package kr.codesqaud.cafe.comment.controller;
 
+import kr.codesqaud.cafe.comment.domain.Comment;
 import kr.codesqaud.cafe.comment.dto.RequestForm;
 import kr.codesqaud.cafe.comment.service.CommentService;
 import kr.codesqaud.cafe.utils.Session;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/comments")
@@ -25,5 +29,18 @@ public class CommentController {
         return commentService.save(requestForm, Session.getUserId(session));
     }
 
+    @DeleteMapping
+    public ResponseEntity<Map<String, Object>> deleteComment(long commentId, String userId, HttpSession session) {
+        Map<String, Object> response = new HashMap<>();
+        try {
+            if (Session.getUserId(session).equals(userId)) {
+                commentService.delete(commentId);
+            }
+            response.put("success", true);
+        } catch (Exception e) {
+            response.put("success", false);
+            response.put("error", e.getMessage());
+        }
+        return ResponseEntity.ok(response);
     }
 }
