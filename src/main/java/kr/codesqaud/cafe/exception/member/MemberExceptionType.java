a73b524a21ecdd88c10e0d28fcd76c06db7e5cd9
chacha <peony@bluehole.net>
2023-04-22 01:36:49 +0900

    refactor 구체적인 틀린 이유 삭제

--- src/main/java/kr/codesqaud/cafe/exception/member/MemberExceptionType.java
+++ src/main/java/kr/codesqaud/cafe/exception/member/MemberExceptionType.java
@@ -4,9 +4,7 @@ public enum MemberExceptionType {
     NOT_FOUND_USER("error", "해당 유저를 찾을 수 없습니다."),
     DO_NOT_HAVE_ACCESS("error", "접근 권한이 없습니다."),
     DUPLICATED_EMAIL("error-email", "이미 존재하는 이메일입니다."),
-    DUPLICATED_MEMBER_NICKNAME("error-nickname", "이미 존재하는 닉네임입니다."),
-    NOT_MATCHED_PASSWORD("error-password", "비밀번호가 일치하지 않습니다."),
-    INVALID_USER_ID("error-userId", "아이디를 잘못 입력했습니다.");
+    DUPLICATED_MEMBER_NICKNAME("error-nickname", "이미 존재하는 닉네임입니다.");
 
     private final String category;
     private final String message;
