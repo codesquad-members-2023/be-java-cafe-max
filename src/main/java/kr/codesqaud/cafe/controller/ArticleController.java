2a14d9b77c5c89fa2f409c25232e1ea86321b73c
O_jeegu <lumirite0@gmail.com>
2023-04-21 17:32:53 +0900

    feat : 로그인하지 않은 사용자의 글보기 및 글쓰기 기능을 제한
    로그인하지 않은 채 글을 보려하거나 글을 쓰려고 할 경우 로그인 페이지로 이동하도록 기능 구현. 로그인 한 경우에만 해당 기능이 활성화 됨.

--- src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
+++ src/main/java/kr/codesqaud/cafe/controller/ArticleController.java
@@ -4,10 +4,9 @@ import kr.codesqaud.cafe.controller.dto.ArticleDTO;
 import kr.codesqaud.cafe.service.ArticleService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
 
 
 @Controller
@@ -19,37 +18,43 @@ public class ArticleController {
         this.articleService = articleService;
     }
 
-    @GetMapping("/post/write")
-    public String postForm() {
+
+    @GetMapping("/posts/new")
+    public String writeForm(HttpSession session) {
+        if(isAnonymous(session)) return "redirect:/login";
         return "post/form";
     }
 
-    //todo : MvcConfigure 활성화 하려면 get, post url 달라야 함. 변경하기
-    @PostMapping("/post/write")
-    public String writePost(@ModelAttribute final ArticleDTO articleDto) {
-        articleService.write(articleDto);
+    @PostMapping("/posts/new-try")
+    public String writePost(@ModelAttribute final ArticleDTO articleDto, HttpSession session) {
+        articleService.write(articleDto, session);
         return "redirect:/";
     }
 
-    @GetMapping("/posts/{id}/show")
-    public String showPost(@PathVariable final long id, final Model model) {
-        ArticleDTO wantedPost = articleService.clickOne(id);
+    @GetMapping("/posts/{id}")
+    public String showPost(@PathVariable final long id, final Model model, HttpSession session) {
+        if(isAnonymous(session)) return "redirect:/login";
+        ArticleDTO wantedPost = articleService.findById(id);
         model.addAttribute("wantedPost", wantedPost);
         return "post/show";
     }
 
-    @GetMapping("/posts/{id}/modify")
+    @GetMapping("/posts/{id}/revision")
     public String modifyForm(@PathVariable final long id, final Model model) {
-        ArticleDTO wantedPost = articleService.clickOne(id);
+        ArticleDTO wantedPost = articleService.findById(id);
         model.addAttribute("articleDTO", wantedPost);
         return "post/modifyForm";
     }
 
     //todo : @ModelAttribute 없어도 되는 원리 정리하기
-    @PostMapping("/posts/{id}/modify")
+    @PutMapping("/posts/{id}/revision-try")
     public String modifyPost(@PathVariable final long id, final ArticleDTO articleDTO) {
         articleService.modify(id, articleDTO);
-        return "redirect:/posts/{id}/show";
+        return "redirect:/posts/{id}";
+    }
+
+    public boolean isAnonymous(HttpSession session) {
+        return session.getAttribute("loginUser") == null;
     }
 }
 
