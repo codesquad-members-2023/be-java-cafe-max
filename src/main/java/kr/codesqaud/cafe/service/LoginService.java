f21b0aa92206a736ee726bca345e638273133046
O_jeegu <lumirite0@gmail.com>
2023-04-25 07:37:00 +0900

    refactor : DTO 변경
    checkLoginUser의 파라미터로 전달하는 DTO와 findByUserId의 파라미터로 전달하는 DTO를 각각 기능에 맞게 변경

--- src/main/java/kr/codesqaud/cafe/service/LoginService.java
+++ src/main/java/kr/codesqaud/cafe/service/LoginService.java
@@ -1,6 +1,7 @@
 package kr.codesqaud.cafe.service;
 
-import kr.codesqaud.cafe.controller.dto.LoginDTO;
+import kr.codesqaud.cafe.controller.dto.login.AuthenticationDTO;
+import kr.codesqaud.cafe.controller.dto.login.LoggedInDTO;
 import kr.codesqaud.cafe.domain.User;
 import kr.codesqaud.cafe.repository.UserRepository;
 import org.springframework.stereotype.Service;
@@ -16,13 +17,13 @@ public class LoginService {
         this.userRepository = userRepository;
     }
 
-    public boolean checkLoginUser(LoginDTO loginDTO) {
-        Optional<User> loginUser = userRepository.findByUserId(loginDTO.getUserId());
-        return loginUser.isPresent() && loginUser.get().matchPassword(loginDTO.getPassword());
+    public boolean checkLoginUser(AuthenticationDTO authenticationDTO) {
+        Optional<User> loginUser = userRepository.findByUserId(authenticationDTO.getUserId());
+        return loginUser.isPresent() && loginUser.get().matchPassword(authenticationDTO.getPassword());
     }
 
-    public LoginDTO findId(final String userId) {
+    public LoggedInDTO findByUserId(final String userId) {
         Optional<User> wantedId = userRepository.findByUserId(userId);
-        return wantedId.map(LoginDTO::from).orElse(null);
+        return wantedId.map(LoggedInDTO::from).orElse(null);
     }
 }
