847d8e34774642ebea3dfc01fb0f07dcadc0bcd3
Sully <2dcoder@naver.com>
2023-04-24 10:12:46 +0900

    feat: 삭제 결과를 활용한 Result 클래스 생성

---
+++ src/main/java/kr/codesqaud/cafe/domain/dto/Result.java
@@ -0,0 +1,17 @@
+package kr.codesqaud.cafe.domain.dto;
+
+public class Result {
+    private String message;
+
+    public Result(String message) {
+        this.message = message;
+    }
+
+    public static Result ok() {
+        return new Result("Okay");
+    }
+
+    public static Result fail(String message) {
+        return new Result(message);
+    }
+}
