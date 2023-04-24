64e5150dc72a32e96caf1002e630e3e179b0435a
crtEvent <crtevn@gmail.com>
2023-04-24 17:47:58 +0900

    [PR-04-개선-08] ControllerExceptionHandler 에서 예외 발생시 Logger 찍기

--- src/main/java/kr/codesqaud/cafe/common/aop/ControllerExceptionHandler.java
+++ src/main/java/kr/codesqaud/cafe/common/aop/ControllerExceptionHandler.java
@@ -2,6 +2,8 @@ package kr.codesqaud.cafe.common.aop;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -14,20 +16,21 @@ import kr.codesqaud.cafe.user.exception.UserNotExistException;
 @ControllerAdvice
 public class ControllerExceptionHandler {
 	private static final String ERROR_MESSAGE_NAME = "errorMessage";
-
-	private String getRedirectRequestURI(HttpServletRequest request) {
-		return "redirect:" + request.getRequestURI();
-	}
+	private static final Logger logger = LoggerFactory.getLogger(ControllerExceptionHandler.class);
 
 	@ExceptionHandler(NumberFormatException.class)
-	public String catchNumberFormatException(HttpServletRequest request, RedirectAttributes redirect) {
-		redirect.addFlashAttribute(ERROR_MESSAGE_NAME, "잘못된 값이 입력되었습니다.");
+	public String catchNumberFormatException(HttpServletRequest request, RedirectAttributes redirect,
+		NumberFormatException e) {
+		String errorMessage = "잘못된 값이 입력되었습니다.";
+		logger.warn("{} {}", errorMessage, e.getStackTrace()[3].toString());
+		redirect.addFlashAttribute(ERROR_MESSAGE_NAME, errorMessage);
 		return getRedirectRequestURI(request);
 	}
 
 	@ExceptionHandler(QuestionNotExistException.class)
 	public String catchNoSuchElementException(HttpServletRequest request, RedirectAttributes redirect,
 		QuestionNotExistException e) {
+		printExceptionLogger(e);
 		redirect.addFlashAttribute(ERROR_MESSAGE_NAME, e.getMessage());
 		return getRedirectRequestURI(request);
 	}
@@ -35,6 +38,7 @@ public class ControllerExceptionHandler {
 	@ExceptionHandler({UserIdDuplicateException.class, UserNotExistException.class, UserDoesNotMatchException.class})
 	public String catchIllegalArgumentException(HttpServletRequest request, RedirectAttributes redirect,
 		Exception e) {
+		printExceptionLogger(e);
 		redirect.addFlashAttribute(ERROR_MESSAGE_NAME, e.getMessage());
 
 		if (request.getRequestURI().equals("/users")) {
@@ -42,6 +46,14 @@ public class ControllerExceptionHandler {
 		}
 
 		return getRedirectRequestURI(request);
+	}
+
+	private String getRedirectRequestURI(HttpServletRequest request) {
+		return "redirect:" + request.getRequestURI();
+	}
+
+	private void printExceptionLogger(Exception e) {
+		logger.warn("{} {}", e.getMessage(), e.getStackTrace()[0].toString());
 	}
 
 }
