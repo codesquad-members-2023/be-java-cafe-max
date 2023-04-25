36bf7a66f267c50c308d7375531a44b48eb45bb9
Sully <2dcoder@naver.com>
2023-04-25 23:26:50 +0900

    fix: ajax 댓글 삭제 버그 수정
    on 함수와 preventDefault 함수로 해결
    특히 preventDefault 함수의 존재는 ajax와 뗄 수 없는 사이와 같음

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,3 +1,4 @@
+
 String.prototype.format = function () {
     var args = arguments;
     return this.replace(/{(\d+)}/g, function (match, number) {
@@ -25,15 +26,18 @@ $('#post-reply').click(() => {
 
         $(".qna-comment-slipp-articles").append(template);
         $("textarea[name=replyContent]").val("");
-        $(".delete-reply").last().on("click", deleteReply);
+
+        $(".qna-comment-slipp-articles").on("click", ".delete-reply", deleteReply);
 
     }).fail((err) => {
         alert(JSON.stringify(err));
     });
 });
 
-$('.delete-reply').click(deleteReply)
 function deleteReply(e) {
+    // 이벤트의 기본 기능을 막는 것 (우선 이벤트를 멈춘 후 js의 소스의 기능을 먼저 따름)
+    e.preventDefault();
+
     var replyId = e.target.dataset['replyid'];
     var url = window.location.pathname + '/replies/' + replyId;
 
