1956448d26884def43b39dfc86c50469913b6c07
road-jin <rjswmtk@gmail.com>
2023-04-25 19:45:23 +0900

    fix: api로 요청하였을 때도 로그인이 안 되었을 경우 json으로 응답하게 수정

--- src/main/java/kr/codesqaud/cafe/config/SignInInterceptor.java
+++ src/main/java/kr/codesqaud/cafe/config/SignInInterceptor.java
@@ -1,8 +1,10 @@
 package kr.codesqaud.cafe.config;
 
+import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import kr.codesqaud.cafe.exception.comment.ApiUnauthorizedException;
 import kr.codesqaud.cafe.util.SignInSessionUtil;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -16,10 +18,19 @@ public class SignInInterceptor implements HandlerInterceptor {
         HttpSession session = request.getSession(false);
 
         if (session == null || session.getAttribute(SignInSessionUtil.SIGN_IN_SESSION_NAME) == null) {
-            response.sendRedirect("/members/sign-in");
+            handleRedirectOrThrowException(request, response);
             return false;
         }
 
         return true;
+    }
+
+    private void handleRedirectOrThrowException(HttpServletRequest request, HttpServletResponse response)
+        throws IOException {
+        if (request.getRequestURI().contains("/api")) {
+            throw new ApiUnauthorizedException();
+        }
+
+        response.sendRedirect("/members/sign-in");
     }
 }
