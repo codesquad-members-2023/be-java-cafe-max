4bd568ab22406e9c52d30f23ff530259b40dc820
ALbertIM0427 <guang7226961@naver.com>
2023-04-25 21:48:28 +0900

    test: SimplePostFormTest 구현

--- src/main/java/kr/codesquad/cafe/post/dto/SimplePostForm.java
+++ src/main/java/kr/codesquad/cafe/post/dto/SimplePostForm.java
@@ -4,11 +4,12 @@ import kr.codesquad.cafe.post.Post;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class SimplePostForm {
-    public static final int MAX_LENGTH = 150;
-    public static final int BEGIN_INDEX = 0;
+    private static final int MAX_LENGTH = 150;
+    private static final int BEGIN_INDEX = 0;
     private final long id;
 
     private final long commentCount;
@@ -49,7 +50,9 @@ public class SimplePostForm {
     }
 
     private static String getSimpleTextContent(Post post) {
-        return (post.getTextContent().length() > MAX_LENGTH) ? post.getTextContent().substring(BEGIN_INDEX, MAX_LENGTH) : post.getTextContent();
+        return (post.getTextContent().length() > MAX_LENGTH)
+                ? post.getTextContent().substring(BEGIN_INDEX, MAX_LENGTH)
+                : post.getTextContent();
     }
 
     public Long getId() {
@@ -74,6 +77,19 @@ public class SimplePostForm {
 
     public long getCommentCount() {
         return commentCount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof SimplePostForm)) return false;
+        SimplePostForm that = (SimplePostForm) o;
+        return getId() == that.getId() && getCommentCount() == that.getCommentCount() && Objects.equals(getNickname(), that.getNickname()) && Objects.equals(getTitle(), that.getTitle()) && Objects.equals(getTextContent(), that.getTextContent()) && Objects.equals(getCreatedDateTime(), that.getCreatedDateTime());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getCommentCount(), getNickname(), getTitle(), getTextContent(), getCreatedDateTime());
     }
 
     public static class Builder {
---
+++ src/test/java/kr/codesquad/cafe/post/dto/SimplePostFormTest.java
@@ -0,0 +1,66 @@
+package kr.codesquad.cafe.post.dto;
+
+import kr.codesquad.cafe.post.Post;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SimplePostFormTest {
+
+    private static final long TEST_ID = 1L;
+    private static final String TEST_TITLE = "title";
+    private static final String TEST_NICKNAME = "jack";
+    private static final String SHORT_TEXT_CONTENT = "content";
+    private static final int MAX_LENGTH = 150;
+    private static final int BEGIN_INDEX = 0;
+    public static final int LONG_COUNT = 100;
+
+    @DisplayName("Content 길이가 Max 길이보다 작으면 Content를 그대로 담고 및 필요한 모든 정보를 post에서 가져온다")
+    @Test
+    void toSimplePostFormWithShortTextContent() {
+        LocalDateTime now = LocalDateTime.now();
+        Post post = getPost(now, SHORT_TEXT_CONTENT);
+
+        List<SimplePostForm> simplePostForms = SimplePostForm.toSimplePostForm(List.of(post));
+
+        SimplePostForm simplePostForm = getSimplePostForm(now, SHORT_TEXT_CONTENT);
+        assertThat(simplePostForms).contains(simplePostForm);
+    }
+
+    @DisplayName("Content 길이가 Max 길이보다 커면 Content를 잘라서 담고 및 필요한 모든 정보를 post에서 가져온다")
+    @Test
+    void toSimplePostFormWithLongTextContent() {
+        LocalDateTime now = LocalDateTime.now();
+        String longTextContent = "content".repeat(LONG_COUNT);
+        Post post = getPost(now, longTextContent);
+
+        List<SimplePostForm> simplePostForms = SimplePostForm.toSimplePostForm(List.of(post));
+
+        SimplePostForm simplePostForm = getSimplePostForm(now, longTextContent.substring(BEGIN_INDEX, MAX_LENGTH));
+        assertThat(simplePostForms).contains(simplePostForm);
+    }
+
+    private static SimplePostForm getSimplePostForm(LocalDateTime now, String textContent) {
+        return new SimplePostForm.Builder()
+                .id(TEST_ID)
+                .nickname(TEST_NICKNAME)
+                .title(TEST_TITLE)
+                .textContent(textContent)
+                .createdDateTime(now)
+                .build();
+    }
+
+    private static Post getPost(LocalDateTime now, String textContent) {
+        return Post.builder()
+                .id(TEST_ID)
+                .nickname(TEST_NICKNAME)
+                .title(TEST_TITLE)
+                .textContent(textContent)
+                .createdDateTime(now)
+                .build();
+    }
+}
