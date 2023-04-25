158b2f36d04aefcbfff4d371105a82e989028efd
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-04-25 12:58:13 +0900

    feat: 리소스 핸들러 설정 추가
    * spring.web.resources.add-mappings=false 설정으로 인한 정적 리소스를 탐색하지 않아서 특정한 정적 리소스 핸들러를 추가하였습니다.

--- src/main/java/kr/codesqaud/cafe/config/web/WebMvcConfig.java
+++ src/main/java/kr/codesqaud/cafe/config/web/WebMvcConfig.java
@@ -4,6 +4,7 @@ import kr.codesqaud.cafe.interceptor.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -15,5 +16,17 @@ public class WebMvcConfig implements WebMvcConfigurer {
             .order(Ordered.HIGHEST_PRECEDENCE)
             .addPathPatterns("/qna/**", "/users/**")
             .excludePathPatterns("/users");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**")
+            .addResourceLocations("classpath:/static/css/");
+        registry.addResourceHandler("/fonts/**")
+            .addResourceLocations("classpath:/static/fonts/");
+        registry.addResourceHandler("/images/**")
+            .addResourceLocations("classpath:/static/images/");
+        registry.addResourceHandler("/js/**")
+            .addResourceLocations("classpath:/static/js/");
     }
 }
