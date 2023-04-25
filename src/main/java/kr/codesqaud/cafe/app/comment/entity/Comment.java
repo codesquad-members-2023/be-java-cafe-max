df6148f06828207dc4b0c9969384cc67877778ab
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-04-25 21:15:28 +0900

    feat: Comment(댓글) 엔티티 객체 추가
    * 댓글 객체는 질문 게시글의 댓글에 대한 정보를 저장하는 엔티티 객체입니다.

---
+++ src/main/java/kr/codesqaud/cafe/app/comment/entity/Comment.java
@@ -0,0 +1,97 @@
+package kr.codesqaud.cafe.app.comment.entity;
+
+import java.time.LocalDateTime;
+import kr.codesqaud.cafe.app.question.entity.Question;
+import kr.codesqaud.cafe.app.user.entity.User;
+
+public class Comment {
+
+    private final Long id;
+    private final String content;
+    private final LocalDateTime createTime;
+    private final LocalDateTime modifyTime;
+    private final Question question;
+    private final User writer;
+
+    private Comment(Long id, String content, LocalDateTime createTime, LocalDateTime modifyTime,
+        Question question, User writer) {
+        this.id = id;
+        this.content = content;
+        this.createTime = createTime;
+        this.modifyTime = modifyTime;
+        this.question = question;
+        this.writer = writer;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public LocalDateTime getModifyTime() {
+        return modifyTime;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public User getWriter() {
+        return writer;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+
+        private Long id;
+        private String content;
+        private LocalDateTime createTime;
+        private LocalDateTime modifyTime;
+        private Question question;
+        private User writer;
+
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder content(String content) {
+            this.content = content;
+            return this;
+        }
+
+        public Builder createTime(LocalDateTime createTime) {
+            this.createTime = createTime;
+            return this;
+        }
+
+        public Builder modifyTime(LocalDateTime modifyTime) {
+            this.modifyTime = modifyTime;
+            return this;
+        }
+
+        public Builder question(Question question) {
+            this.question = question;
+            return this;
+        }
+
+        public Builder writer(User writer) {
+            this.writer = writer;
+            return this;
+        }
+
+        public Comment build() {
+            return new Comment(id, content, createTime, modifyTime, question, writer);
+        }
+    }
+}
