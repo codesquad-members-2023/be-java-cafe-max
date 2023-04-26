c75dbc50978ebf7c40372cd3326c7eaa095baa49
tmdqja0705@naver.com <tmdqja0705@naver.com>
2023-04-26 10:16:01 +0900

    예외 throw 에서 boolean 반환으로 로직 변경

--- src/main/java/kr/codesqaud/cafe/post/service/AuthService.java
+++ src/main/java/kr/codesqaud/cafe/post/service/AuthService.java
@@ -1,18 +1,23 @@
 package kr.codesqaud.cafe.post.service;
 
+import kr.codesqaud.cafe.post.domain.Post;
+import kr.codesqaud.cafe.user.domain.User;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
-import java.nio.file.AccessDeniedException;
 
 @Service
 public class AuthService {
 
-    public boolean checkLogin(HttpSession session) throws AccessDeniedException {
-        if(session.getAttribute("sessionId") == null) {
-            throw new AccessDeniedException("로그인 해주세요");
+    public boolean checkLogin(HttpSession session){
+        if(session.getAttribute("sessionUser") == null) {
+            return false;
         }
         return true;
     }
 
+    public boolean checkAuth(HttpSession session, Post post){
+        User user = (User) session.getAttribute("sessionUser");
+        return user.getName().equals(post.getWriter());
+    }
 }
