55cdb340c7530e8b7309f21883e68f334eca06a6
takealook <takealook97@naver.com>
2023-04-26 09:51:50 +0900

    original comment는 다루지 않으므로 삭제

--- src/main/java/kr/codesqaud/cafe/controller/dto/request/ReplyEditRequest.java
+++ src/main/java/kr/codesqaud/cafe/controller/dto/request/ReplyEditRequest.java
@@ -1,16 +1,10 @@
 package kr.codesqaud.cafe.controller.dto.request;
 
 public class ReplyEditRequest {
-    private final String originalComment;
     private final String newComment;
 
-    public ReplyEditRequest(String originalComment, String newComment) {
-        this.originalComment = originalComment;
+    public ReplyEditRequest(String newComment) {
         this.newComment = newComment;
-    }
-
-    public String getOriginalComment() {
-        return originalComment;
     }
 
     public String getNewComment() {
