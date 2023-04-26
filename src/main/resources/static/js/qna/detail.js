44b421219f04a7bfbdf0a0b9f49c5dc40dd63fc9
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-04-26 12:29:27 +0900

    feat: 댓글 추가 기능에 대한 ajax 요청 코드 추가

--- src/main/resources/static/js/qna/detail.js
+++ src/main/resources/static/js/qna/detail.js
@@ -15,4 +15,49 @@ $(document).ready(function () {
       alert(errorResponse.errorMessage)
     })
   })
+
+  $("#addCommentForm").submit(async function (e) {
+    e.preventDefault()
+
+    const id = $("#id").val()
+
+    const data = {
+      content: $("#addCommentForm #content").val(),
+      questionId: $("#questionId").val(),
+      userId: $("#userId").val()
+    }
+
+    $.ajax({
+      type: "POST",
+      url: `/qna/${id}/comments`,
+      data: JSON.stringify(data),
+      contentType: 'application/json; charset=utf-8'
+    }).done(function (resp) {
+      const commentTemplate = $("#answerTemplate").html()
+      const template = commentTemplate.format(
+          resp.writerName,
+          formatDateTime(resp.createTime),
+          resp.content,
+          resp.questionId,
+          resp.id)
+
+      $(".qna-comment-slipp-articles").append(template);
+      $("#content").val("");
+    }).fail(function (response) {
+      const errorResponse = response.responseJSON
+      alert(errorResponse.errorMessage)
+    })
+  })
+
+  function formatDateTime(dateTimeString) {
+    const date = new Date(dateTimeString);
+
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+
+    return `${year}-${month}-${day} ${hours}:${minutes}`
+  }
 })
