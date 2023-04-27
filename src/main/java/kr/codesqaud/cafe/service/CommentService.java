7e73fc6b6cde981d0cbfc0509a5d7a7cd98b82c3
O_jeegu <lumirite0@gmail.com>
2023-04-28 05:59:40 +0900

    feat : 댓글 작성 및 조회 기능 추가
    댓글을 작성하고 해당 페이지의 댓글을 모두 불러오는 기능 추가

---
+++ src/main/java/kr/codesqaud/cafe/service/CommentService.java
@@ -0,0 +1,34 @@
+package kr.codesqaud.cafe.service;
+
+import kr.codesqaud.cafe.controller.dto.comment.CommentDTO;
+import kr.codesqaud.cafe.domain.Comment;
+import kr.codesqaud.cafe.repository.CommentRepository;
+import kr.codesqaud.cafe.util.LoginSessionManager;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CommentService {
+    private final CommentRepository commentRepository;
+    private final LoginSessionManager loginSessionManager;
+
+    public CommentService(CommentRepository commentRepository, LoginSessionManager loginSessionManager) {
+        this.commentRepository = commentRepository;
+        this.loginSessionManager = loginSessionManager;
+    }
+
+    public void write(Long articleId, CommentDTO commentDto) {
+        Long userId = loginSessionManager.getLoginUser().getId();
+        String userName = loginSessionManager.getLoginUser().getName();
+        Comment comment = commentDto.toEntity(articleId, userId, userName);
+        commentRepository.save(comment);
+    }
+
+    public List<CommentDTO> gather(Long articleId) {
+        return commentRepository.gatherAllByArticleId(articleId).stream()
+                .map(CommentDTO::from)
+                .collect(Collectors.toList());
+    }
+}
