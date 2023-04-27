b285c41ca534f00264cc5433185fd1cfaf4c8901
sudago <sudago22@gmail.com>
2023-04-27 12:26:03 +0900

    fix: 문자열 상수로 변경

--- src/main/java/kr/codesqaud/cafe/util/LoginCheckFilter.java
+++ src/main/java/kr/codesqaud/cafe/util/LoginCheckFilter.java
@@ -1,5 +1,6 @@
 package kr.codesqaud.cafe.util;
 
+import com.mysql.cj.Session;
 import org.springframework.util.PatternMatchUtils;
 
 import javax.servlet.*;
@@ -19,12 +20,11 @@ public class LoginCheckFilter implements Filter {
         String requestURI = httpRequest.getRequestURI();
 
         HttpServletResponse httpResponse = (HttpServletResponse) response;
-        // TODO: 로그인 사용자 enum 만들기
         // 인증 체크 하기
         if (isLoginCheckPath(requestURI)) {
             HttpSession session = httpRequest.getSession(false);
             // 세션이 없거나 로그인한 사용자가 없으면
-            if (session == null || session.getAttribute("loginUser") == null) {
+            if (session == null || session.getAttribute(SessionConst.LOGIN_USER) == null) {
                 // 권한이 없으면 로그인 페이지를 넘겨주기 -> 로그인 하면 로그인 하기 전의 페이지를 다시 반환해주기.
                 httpResponse.sendRedirect("/login?redirectURL=" + requestURI);
                 return;
