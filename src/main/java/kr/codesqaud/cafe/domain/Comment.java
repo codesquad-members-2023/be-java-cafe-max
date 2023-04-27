eeeeb1b496b9d07c76e589573a2a382561f1818f
O_jeegu <lumirite0@gmail.com>
2023-04-28 05:57:26 +0900

    refactor : Comment class setter 생성
    추후 삭제 예정

--- src/main/java/kr/codesqaud/cafe/domain/Comment.java
+++ src/main/java/kr/codesqaud/cafe/domain/Comment.java
@@ -1,30 +1,30 @@
 package kr.codesqaud.cafe.domain;
 
-import java.sql.Timestamp;
-
 public class Comment {
     private Long id;
-    private Long reid;
     private Long articleId;
     private String content;
-    private String commenter;
-    private Timestamp createTime;
+    private Long userId;
+    private String userName;
+    private String createTime;
 
-    public Comment(Long id, Long reid, Long articleId, String content, String commenter, Timestamp createTime) {
+    public Comment() {}
+
+    public Comment(Long id, Long articleId, String content, Long userId, String userName, String createTime) {
         this.id = id;
-        this.reid = reid;
         this.articleId = articleId;
         this.content = content;
-        this.commenter = commenter;
+        this.userId = userId;
+        this.userName = userName;
         this.createTime = createTime;
     }
 
     public void create(Long id, Comment comment) {
         this.id = id;
-        this.reid = comment.reid;
         this.articleId = comment.articleId;
         this.content = comment.content;
-        this.commenter = comment.commenter;
+        this.userId = comment.userId;
+        this.userName = comment.userName;
         this.createTime = comment.createTime;
     }
 
@@ -32,23 +32,47 @@ public class Comment {
         return id;
     }
 
-    public Long getReid() {
-        return reid;
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public Long getArticleId() {
         return articleId;
     }
 
+    public void setArticleId(Long articleId) {
+        this.articleId = articleId;
+    }
+
     public String getContent() {
         return content;
     }
 
-    public String getCommenter() {
-        return commenter;
+    public void setContent(String content) {
+        this.content = content;
     }
 
-    public Timestamp getCreateTime() {
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getCreateTime() {
         return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
     }
 }
