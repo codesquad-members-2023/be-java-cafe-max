097d2b4aba03d9694aa52bc589f87533bf8a8cfe
CDBchan <kdjin1215@naver.com>
2023-04-27 13:54:05 +0900

    refactor: controller test 수정  및 추가
    추가된 기능에 대한 test를 만들고 수정할부분은 수정했다.

--- src/test/java/kr/codesqaud/cafe/controller/MainControllerTest.java
+++ src/test/java/kr/codesqaud/cafe/controller/MainControllerTest.java
@@ -1,5 +1,6 @@
 package kr.codesqaud.cafe.controller;
 
+import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.util.ArrayList;
@@ -16,6 +17,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import kr.codesqaud.cafe.article.ArticleService;
 import kr.codesqaud.cafe.article.dto.ArticleResponseForList;
 import kr.codesqaud.cafe.mainPage.MainController;
+import kr.codesqaud.cafe.mainPage.PaginationDto;
 
 @WebMvcTest(MainController.class)
 class MainControllerTest {
@@ -30,15 +32,19 @@ class MainControllerTest {
 	@DisplayName("mainPage메서드를 통해 article의 list를 가져온후 나열한다.")
 	void mainPageTest() throws Exception {
 		//given
+		Long countOfArticles = 10L;
 		List<ArticleResponseForList> articleList = new ArrayList<>();
-		// given(articleService.getArticleList()).willReturn(articleList);
+		given(articleService.getArticleList(any(PaginationDto.class))).willReturn(articleList);
+		given(articleService.getCountOfArticles()).willReturn(countOfArticles);
 
 		//when & then
 		mockMvc.perform(MockMvcRequestBuilders.get("/"))
 			.andExpect(status().isOk())
 			.andExpect(view().name("index"))
+			.andExpect(model().attributeExists("pagination"))
 			.andExpect(model().attributeExists("articles"))//객체 검증
-			.andExpect(model().attribute("articles", articleList));
+			.andExpect(model().attribute("articles", articleList))
+			.andExpect(model().attribute("countOfArticles", countOfArticles));
 	}
 
 }
--- src/test/java/kr/codesqaud/cafe/controller/ReplyControllerTest.java
+++ src/test/java/kr/codesqaud/cafe/controller/ReplyControllerTest.java
@@ -3,6 +3,10 @@ package kr.codesqaud.cafe.controller;
 import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,11 +16,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import kr.codesqaud.cafe.global.config.Session;
 import kr.codesqaud.cafe.reply.ReplyController;
 import kr.codesqaud.cafe.reply.ReplyService;
+import kr.codesqaud.cafe.reply.dto.LoadMoreReplyDto;
 import kr.codesqaud.cafe.reply.dto.ReplyRequest;
 import kr.codesqaud.cafe.reply.dto.ReplyResponse;
 import kr.codesqaud.cafe.reply.dto.Result;
@@ -31,6 +37,10 @@ class ReplyControllerTest {
 	ReplyService replyService;
 
 	private MockHttpSession httpSession;
+
+	private Long articleIdx = 1L;
+
+	private List<ReplyResponse> replies = new ArrayList<>();
 
 	@BeforeEach
 	public void setUp() {
@@ -71,4 +81,44 @@ class ReplyControllerTest {
 			.andExpect(status().isOk())
 			.andExpect(jsonPath("$.ok").value(true));
 	}
+
+	@Test
+	@DisplayName("더보기 버튼 클릭시 loadMoreReply 메서드 실행후 추가로 load될 댓글이 존재하면 댓글 list를 return한다.")
+	void loadMoreReplySuccessTest() throws Exception {
+		//given
+		given(replyService.getCountOfReplies(articleIdx)).willReturn(10);
+		given(replyService.getRepliesByIdx(any(LoadMoreReplyDto.class))).willReturn(replies);
+
+		//when
+		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/articles/reply/loadMoreReply")
+				.session(httpSession)
+				.param("articleIdx", String.valueOf(articleIdx))
+				.param("countOfRepliesInHtml", "5"))
+			//then
+			.andExpect(status().isOk())
+			.andReturn();
+
+		System.out.println(mvcResult.getResponse().getContentAsString());
+		Assertions.assertThat(mvcResult.getResponse().getContentAsString()).isNotBlank();
+	}
+
+	@Test
+	@DisplayName("더보기 버튼 클릭시 추가로 load될 댓글이 존재하지 않으면 null을 return한다.")
+	void loadMoreReplyFailTest() throws Exception {
+		//given
+		given(replyService.getCountOfReplies(articleIdx)).willReturn(5);
+		given(replyService.getRepliesByIdx(any(LoadMoreReplyDto.class))).willReturn(replies);
+
+		//when
+		MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/articles/reply/loadMoreReply")
+				.session(httpSession)
+				.param("articleIdx", String.valueOf(articleIdx))
+				.param("countOfRepliesInHtml", "5"))
+			//then
+			.andExpect(status().isOk())
+			.andReturn();
+
+		Assertions.assertThat(mvcResult.getResponse().getContentAsString()).isBlank();
+	}
+
 }
