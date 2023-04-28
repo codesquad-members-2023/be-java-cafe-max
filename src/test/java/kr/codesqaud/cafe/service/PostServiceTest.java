ca843d3c54feb1ffa0ff45a1be07620d6a7c2387
road-jin <rjswmtk@gmail.com>
2023-04-28 16:46:22 +0900

    fix: spy 어노테이션 제거

--- src/test/java/kr/codesqaud/cafe/service/PostServiceTest.java
+++ src/test/java/kr/codesqaud/cafe/service/PostServiceTest.java
@@ -26,13 +26,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class PostServiceTest {
 
-    @Spy
     @InjectMocks
     private PostService postService;
 
