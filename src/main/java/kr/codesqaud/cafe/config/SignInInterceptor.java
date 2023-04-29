265182349bea6a32b6486fa2882435a26876d2e9
road-jin <rjswmtk@gmail.com>
2023-04-30 01:22:03 +0900

    refactor: 재사용 하지 않는 메서드 제거

--- src/main/java/kr/codesqaud/cafe/config/SignInInterceptor.java
+++ src/main/java/kr/codesqaud/cafe/config/SignInInterceptor.java
@@ -1,6 +1,5 @@
 package kr.codesqaud.cafe.config;
 
-import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -18,19 +17,14 @@ public class SignInInterceptor implements HandlerInterceptor {
         HttpSession session = request.getSession(false);
 
         if (session == null || session.getAttribute(SignInSessionUtil.SIGN_IN_SESSION_NAME) == null) {
-            handleRedirectOrThrowException(request, response);
+            if (request.getRequestURI().contains("/api")) {
+                throw new ApiUnauthorizedException();
+            }
+
+            response.sendRedirect("/sign-in");
             return false;
         }
 
         return true;
-    }
-
-    private void handleRedirectOrThrowException(HttpServletRequest request, HttpServletResponse response)
-        throws IOException {
-        if (request.getRequestURI().contains("/api")) {
-            throw new ApiUnauthorizedException();
-        }
-
-        response.sendRedirect("/sign-in");
     }
 }
