1b32b140f0a41a8e6bcda84837189963cd67a763
ALbertIM0427 <guang7226961@naver.com>
2023-05-07 11:00:19 +0900

    refactor: NotEmpty를 NotBlank로 변경

--- src/main/java/kr/codesquad/cafe/post/annotation/ValidPostTitle.java
+++ src/main/java/kr/codesquad/cafe/post/annotation/ValidPostTitle.java
@@ -4,7 +4,7 @@ package kr.codesquad.cafe.post.annotation;
 import javax.validation.Constraint;
 import javax.validation.Payload;
 import javax.validation.ReportAsSingleViolation;
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
@@ -12,7 +12,7 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@NotEmpty
+@NotBlank
 @Size(max = 64, min = 2, message = "{error.title.size}")
 @Target({FIELD})
 @ReportAsSingleViolation
